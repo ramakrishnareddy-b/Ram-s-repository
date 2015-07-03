1c6047e95d3d93388c6505c5bdece4afceb42e80
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-03 08:52:01 +0530

    Scripts for Guinea+Conakry

--- Corporate+Press+Releases/Government/Guinea+Conakry+PRES_body.js
+++ Corporate+Press+Releases/Government/Guinea+Conakry+PRES_body.js
@@ -1,1 +1,13 @@
 // Guinea Conakry
+
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.contentEvaluate("http://www.presidence.gov.gn/index.php/15-actualites/498-declaration-des-chefs-detat-et-de-gouvernement-de-lunion-du-fleuve-mano-sur-les-modalites-de-preparation-de-la-conference-plaidoyer-new-york",
+    function(){
+        return {
+            title:jQuery("#artcle-heading > h2")[0].innerText,
+            body:jQuery("#content p").text(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Guinea+Conakry+PRES_list.js
+++ Corporate+Press+Releases/Government/Guinea+Conakry+PRES_list.js
@@ -1,1 +1,10 @@
-http://www.presidence.gov.gn
+//http://www.presidence.gov.gn/
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.linksEvaluate(["http://www.presidence.gov.gn/"],function(){
+    var urls =[];
+    jQuery("._PROFILE h4 a").each(function(idx,el){
+        urls.push(el.href);
+    });
+    return urls;
+});
