fa956bf948886356fac902da07ad24ce26db19f4
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-04 23:43:52 +0530

    Scripts for Peru MOE

--- Corporate+Press+Releases/Government/Peru+MOE_body.js
+++ Corporate+Press+Releases/Government/Peru+MOE_body.js
@@ -1,1 +1,14 @@
 // Peru
+
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.minem.gob.pe/_detallenoticia.php?idSector=8&idTitular=6880",
+    function () {
+        return {
+            title: $("#zonaContenido .titulo").text().trim(),
+            published_s: $("#zonaContenido .fecha").text().trim(),
+            body: $("#zonaContenido .detalle p").css("text-align", "justify").text(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Peru+MOE_list.js
+++ Corporate+Press+Releases/Government/Peru+MOE_list.js
@@ -1,1 +1,11 @@
-http://www.minem.gob.pe/_noticias.php
+//http://www.minem.gob.pe/_noticias.php
+
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.minem.gob.pe/_noticias.php"], function () {
+    var urls = [];
+    $("#listaNotas .item").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
