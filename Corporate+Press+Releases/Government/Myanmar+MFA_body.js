cde70b97a7f4761e8d6e3085cd83e70b513af2e2
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-04 23:41:56 +0530

    Scripts for Myanmar MFA

--- Corporate+Press+Releases/Government/Myanmar+MFA_body.js
+++ Corporate+Press+Releases/Government/Myanmar+MFA_body.js
@@ -1,1 +1,13 @@
 // Myanmar
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.mofa.gov.mm/?p=4553",
+    function () {
+        return {
+            title: jQuery(".post-content .post-title").text().trim(),
+            published_s: jQuery(".post-content .post-date").text().trim(),
+            body: jQuery(".post-content").css("text-align", "justify").text(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Myanmar+MFA_list.js
+++ Corporate+Press+Releases/Government/Myanmar+MFA_list.js
@@ -1,3 +1,13 @@
-http://www.mofa.gov.mm/?tag=press-release
-http://www.mofa.gov.mm/?cat=4
-http://www.mofa.gov.mm/?cat=1
+//http://www.mofa.gov.mm/?tag=press-release
+//http://www.mofa.gov.mm/?cat=4
+//http://www.mofa.gov.mm/?cat=1
+
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.mofa.gov.mm/?tag=press-release", "http://www.mofa.gov.mm/?cat=4", "http://www.mofa.gov.mm/?cat=1"], function () {
+    var urls = [];
+    jQuery("#column .post-title a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
