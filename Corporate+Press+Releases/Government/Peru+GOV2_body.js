5fef759857b221c93dd15191e19e7fa21af43a1c
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-04 23:44:41 +0530

    Scripts for Peru GOV2

--- Corporate+Press+Releases/Government/Peru+GOV2_body.js
+++ Corporate+Press+Releases/Government/Peru+GOV2_body.js
@@ -1,1 +1,13 @@
 // Peru
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.pcm.gob.pe/2015/04/comunicado/",
+    function () {
+        return {
+            title: jQuery(".postarea h1").text().trim(),
+            published_s: jQuery(".postarea .align-left").text().trim(),
+            body: jQuery(".postarea [style='text-align: justify;']").text(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Peru+GOV2_list.js
+++ Corporate+Press+Releases/Government/Peru+GOV2_list.js
@@ -1,1 +1,11 @@
-http://www.pcm.gob.pe/seccion/noticias/comunicados/
+//http://www.pcm.gob.pe/seccion/noticias/comunicados/
+
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.pcm.gob.pe/seccion/noticias/comunicados/"], function () {
+    var urls = [];
+    jQuery("#portfolio-list .title a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
