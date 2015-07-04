2ba2f1023e9bd31cacf13796047c002a423b9f46
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-04 23:42:54 +0530

    Scripts for Namibia MOE

--- Corporate+Press+Releases/Government/Namibia+MOE_body.js
+++ Corporate+Press+Releases/Government/Namibia+MOE_body.js
@@ -1,1 +1,13 @@
 // Namibia
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.mme.gov.na/news/37/Ohorongo-donates-cement-to-all-regions",
+    function () {
+        return {
+            title: jQuery("#id_passed #page_title").text().trim(),
+            published_s: jQuery("#id_passed .small2").text().trim(),
+            body: jQuery("#id_passed .center_content p").text(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Namibia+MOE_list.js
+++ Corporate+Press+Releases/Government/Namibia+MOE_list.js
@@ -1,1 +1,10 @@
-http://www.mme.gov.na/news/
+//http://www.mme.gov.na/news/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.mme.gov.na/news/"], function () {
+    var urls = [];
+    jQuery(".storybox .bluetext .small2 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
