672627dfb232ac2e5c29ac5ad309adc35908e57a
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-05 00:01:23 +0530

    Scripts for Mozambique MOE

--- Corporate+Press+Releases/Government/Mozambique+MOE_body.js
+++ Corporate+Press+Releases/Government/Mozambique+MOE_body.js
@@ -1,1 +1,12 @@
 // Mozambique
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.mireme.gov.mz/index.php/34-esperanca-bias-distinguida-na-italia",
+    function () {
+        return {
+            title: jQuery(".component-content .item-page h2")[0].innerText,
+            body: jQuery(".component-content .item-page p").text(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Mozambique+MOE_list.js
+++ Corporate+Press+Releases/Government/Mozambique+MOE_list.js
@@ -1,1 +1,10 @@
-http://www.mireme.gov.mz
+//http://www.mireme.gov.mz
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.mireme.gov.mz"], function () {
+    var urls = [];
+    jQuery(".component-content .items-row .item h2 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
