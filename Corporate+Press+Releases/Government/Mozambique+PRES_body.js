adb2cff33b558054a85531fb0ef5fe122b7efc66
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-04 23:40:24 +0530

    Scripts for Mozambique PRES

--- Corporate+Press+Releases/Government/Mozambique+PRES_body.js
+++ Corporate+Press+Releases/Government/Mozambique+PRES_body.js
@@ -1,1 +1,12 @@
 // Mozambique
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.presidencia.gov.mz/index.php/noticias/1147-presidente-filipe-nyusi-inicia-visita-presidencial-a-provincia-de-niassa",
+    function () {
+        return {
+            title: jQuery(".ja-content-main .item-page h2")[0].innerText,
+            body: jQuery(".ja-content-main .item-page p").css("text-align", "justify").text(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Mozambique+PRES_list.js
+++ Corporate+Press+Releases/Government/Mozambique+PRES_list.js
@@ -1,1 +1,10 @@
-http://www.presidencia.gov.mz/index.php/noticias
+//http://www.presidencia.gov.mz/index.php/noticias
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.presidencia.gov.mz/index.php/noticias"], function () {
+    var urls = [];
+    jQuery(".blog .items-row .contentheading a, .blog .items-more ol>li a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
