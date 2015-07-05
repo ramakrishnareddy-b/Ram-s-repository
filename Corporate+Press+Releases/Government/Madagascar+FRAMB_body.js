9846a2f0e32c70f563e5450259075d28d331bfd7
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-05 20:59:32 +0530

    Scripts for Madagascar+FRAMB

--- Corporate+Press+Releases/Government/Madagascar+FRAMB_body.js
+++ Corporate+Press+Releases/Government/Madagascar+FRAMB_body.js
@@ -1,1 +1,12 @@
 // Madagascar
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.ambafrance-mada.org/Documents-d-identite-et-de-voyage-1798",
+    function () {
+        return {
+            title: $(".span9 .title, .block-article .title")[0].innerText.trim(),
+            body: $(".span9 .texte, .block-article .texte")[0].innerText,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Madagascar+FRAMB_list.js
+++ Corporate+Press+Releases/Government/Madagascar+FRAMB_list.js
@@ -1,1 +1,10 @@
-http://www.ambafrance-mada.org/-Toutes-les-actualites-
+//http://www.ambafrance-mada.org/-Toutes-les-actualites-
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.ambafrance-mada.org/-Toutes-les-actualites-"], function () {
+    var urls = [];
+    $(".block-page .block-listing-articles .title a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
