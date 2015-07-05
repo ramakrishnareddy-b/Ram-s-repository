a67b971790658cf4ddcb08ef5af2bc0f0ffcdc60
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-05 14:48:39 +0530

    Scripts for Mexico+PRES

--- Corporate+Press+Releases/Government/Mexico+PRES_body.js
+++ Corporate+Press+Releases/Government/Mexico+PRES_body.js
@@ -1,1 +1,13 @@
 // Mexico
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://en.presidencia.gob.mx/articles-press/society-is-understanding-the-progress-of-our-economy-which-explains-the-majoritys-support-of-the-project-promoted-by-the-government-enrique-pena-nieto/",
+    function () {
+        return {
+            title: $(".blog-post .post-title")[0].innerText,
+            published_s: $(".blog-post .post-content .last-update-date")[0].innerText.split(":")[1].trim(),
+            body: $(".blog-post .post-content p").text(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Mexico+PRES_list.js
+++ Corporate+Press+Releases/Government/Mexico+PRES_list.js
@@ -1,1 +1,10 @@
-http://en.presidencia.gob.mx/press/press-releases/
+//http://en.presidencia.gob.mx/press/press-releases/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://en.presidencia.gob.mx/press/press-releases/"], function () {
+    var urls = [];
+    $(".match-children-height h1 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
