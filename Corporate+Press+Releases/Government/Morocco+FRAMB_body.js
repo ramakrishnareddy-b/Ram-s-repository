f52dae4ab23fbf0cef6f3f8b7c74b7ed1c89afce
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-05 14:29:11 +0530

    Scripts for Morocco+FRAME

--- Corporate+Press+Releases/Government/Morocco+FRAMB_body.js
+++ Corporate+Press+Releases/Government/Morocco+FRAMB_body.js
@@ -1,1 +1,13 @@
 // Morocco
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.ambafrance-ma.org/COP21-Forum-de-jeunes-franco-germano-marocains-Prends-ta-planete-en-main",
+    function () {
+        return {
+            title: $('#main .title')[0].innerText,
+            published_s: $("#main .timestamp")[0].innerText.split(":")[1].trim(),
+            body: cs.textUpto($('#main p').text(), "Dernière modification "),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Morocco+FRAMB_list.js
+++ Corporate+Press+Releases/Government/Morocco+FRAMB_list.js
@@ -1,1 +1,10 @@
-http://www.ambafrance-ma.org/-Actualites-politiques-
+//http://www.ambafrance-ma.org/-Actualites-politiques-
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.ambafrance-ma.org/-Actualites-politiques-"], function () {
+    var urls = [];
+    $(".block-listing .inner ul>li h2 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
