7e39799d54582a16cda1d3bdfb9aab7df3bdbf96
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-05 20:13:59 +0530

    Scripts for Madagascar+GOV3

--- Corporate+Press+Releases/Government/Madagascar+GOV3_body.js
+++ Corporate+Press+Releases/Government/Madagascar+GOV3_body.js
@@ -1,1 +1,13 @@
 // Madagascar
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://eiti-madagascar.org/news/visite-officielle-a-madagascar-de-mrs-clare-short-presidente-de-leiti-international/",
+    function () {
+        return {
+            title: jQuery(".content h2")[0].innerText,
+            published_s: jQuery(".content .breadcrumbs")[0].innerText.split(" > ")[1].replace(/\./g, '-'),
+            body: cs.textUpto(jQuery(".content .post p").text(), "Tanteliarimiza Rakotomalala"),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Madagascar+GOV3_list.js
+++ Corporate+Press+Releases/Government/Madagascar+GOV3_list.js
@@ -1,1 +1,10 @@
-http://eiti-madagascar.org/all-news/actualites/
+//http://eiti-madagascar.org/all-news/actualites/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://eiti-madagascar.org/all-news/actualites/"], function () {
+    var urls = [];
+    jQuery(".content .archive h4 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
