56574377efcc97b9a16091a7d7dead6946712353
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-05 15:19:56 +0530

    Scripts for Mexico+MFA

--- Corporate+Press+Releases/Government/Mexico+MFA_body.js
+++ Corporate+Press+Releases/Government/Mexico+MFA_body.js
@@ -1,1 +1,12 @@
 // Mexico
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://en.sre.gob.mx/index.php/archived-press-releases/3230-mexico-welcomes-the-restoration-of-diplomatic-relations-between-cuba-and-the-united-states",
+    function () {
+        return {
+            title: $(".contentpaneopen .contentheading")[0].innerText,
+            body: $(".contentpaneopen p").text(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Mexico+MFA_list.js
+++ Corporate+Press+Releases/Government/Mexico+MFA_list.js
@@ -1,1 +1,10 @@
-http://en.sre.gob.mx/index.php/archived-press-releases
+//http://en.sre.gob.mx/index.php/archived-press-releases
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://en.sre.gob.mx/index.php/archived-press-releases"], function () {
+    var urls = [];
+    $(".sectiontableentry1 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
