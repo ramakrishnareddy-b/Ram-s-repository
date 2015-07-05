acb5c983b649c516636f70e04995a152d4b6c668
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-05 15:36:30 +0530

    Scripts for Madagascar+PRESS

--- Corporate+Press+Releases/Government/Madagascar+PRES_body.js
+++ Corporate+Press+Releases/Government/Madagascar+PRES_body.js
@@ -1,1 +1,12 @@
 // Madagascar
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.presidence.gov.mg/appel-a-vigilance-face-aux-usurpateurs/",
+    function () {
+        return {
+            title: jQuery("#content .entry-title")[0].innerText,
+            body: jQuery("#content .entry-content p").text(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Madagascar+PRES_list.js
+++ Corporate+Press+Releases/Government/Madagascar+PRES_list.js
@@ -1,1 +1,10 @@
-http://www.presidence.gov.mg/category/documents/communiques/
+//http://www.presidence.gov.mg/category/documents/communiques/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.presidence.gov.mg/category/documents/communiques/"], function () {
+    var urls = [];
+    jQuery("#content .entry-title a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
