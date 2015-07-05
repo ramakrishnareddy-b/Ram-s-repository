f957ccee4d5c8bcdf94903e7d20e40c0645a45e5
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-05 13:55:55 +0530

    Scripts for Morocco+GOV

--- Corporate+Press+Releases/Government/Morocco+GOV_body.js
+++ Corporate+Press+Releases/Government/Morocco+GOV_body.js
@@ -1,1 +1,13 @@
 // Morocco
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.pm.gov.ma/fr/details.1.603.Signature+par+le+Maroc+du+programme+pays+avec+l%E2%80%99OCDE",
+    function () {
+        return {
+            title: $("#details_articles h2")[0].innerText,
+            published_s: $("#details_articles h5")[0].innerText,
+            body: $("#details_articles p").text(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Morocco+GOV_list.js
+++ Corporate+Press+Releases/Government/Morocco+GOV_list.js
@@ -1,1 +1,10 @@
-http://www.pm.gov.ma/fr/section.1.Nouveautés
+//http://www.pm.gov.ma/fr/section.1.Nouveautés
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.pm.gov.ma/fr/section.1.Nouveautés"], function () {
+    var urls = [];
+    $("#mybodydetails .body_news_activites_large h1 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
