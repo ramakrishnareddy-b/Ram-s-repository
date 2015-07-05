f22d0d4bfd8a2b3054e125d457e698d104120d2a
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-05 20:34:02 +0530

    Scripts for Madagascar+GOV2

--- Corporate+Press+Releases/Government/Madagascar+GOV2_body.js
+++ Corporate+Press+Releases/Government/Madagascar+GOV2_body.js
@@ -1,1 +1,13 @@
 // Madagascar
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://mae.gov.mg/prix-de-linternational-catering-cup-2015-de-lyon-le-trophee-revient-a-madagascar/",
+    function () {
+        return {
+            title: jQuery(".entry-content h1")[0].innerText,
+            published_s: jQuery(".entry-content .article-attributes ul>li:first")[0].innerText.split(" : ")[1],
+            body: jQuery(".entry-content .article")[0].innerText,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Madagascar+GOV2_list.js
+++ Corporate+Press+Releases/Government/Madagascar+GOV2_list.js
@@ -1,1 +1,10 @@
-http://mae.gov.mg/category/actualites/
+//http://mae.gov.mg/category/actualites/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://mae.gov.mg/category/actualites/"], function () {
+    var urls = [];
+    jQuery(".thumbnail.mae-see-also .post-title a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
