d701005cf8f3bf862819fbe9fbb8fbe876e839f6
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-12 16:01:35 +0530

    Scripts for Uganda+USAMB.

--- Corporate+Press+Releases/Government/Uganda+USAMB_body.js
+++ Corporate+Press+Releases/Government/Uganda+USAMB_body.js
@@ -1,1 +1,12 @@
 // Uganda
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://kampala.usembassy.gov/pr_12202014.html",
+    function () {
+        return {
+            title: $(".middle-article .page-title")[0].innerText,
+            body: $(".middle-article .middle-content-article")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
