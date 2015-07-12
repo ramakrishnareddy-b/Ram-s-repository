86d9b13dd1c8536566c45176305c9f970910e2a9
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-12 23:43:56 +0530

    Scripts for Trinidad+and+Tobago+MOE.

--- Corporate+Press+Releases/Government/Trinidad+and+Tobago+MOE_body.js
+++ Corporate+Press+Releases/Government/Trinidad+and+Tobago+MOE_body.js
@@ -1,1 +1,12 @@
 // Trinidad and Tobago
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.energy.gov.tt/energy-minister-meets-with-senior-officials-from-landocean-energy-services-and-range-resources-ltd/",
+    function () {
+        return {
+            title: $("#main .entry-title")[0].innerText.trim(),
+            links: $("#main .entry-content a").attr("href"),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Trinidad+and+Tobago+MOE_list.js
+++ Corporate+Press+Releases/Government/Trinidad+and+Tobago+MOE_list.js
@@ -1,1 +1,10 @@
-http://www.energy.gov.tt/media-gallery/media-releases/
+//http://www.energy.gov.tt/media-gallery/media-releases/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.energy.gov.tt/media-gallery/media-releases/"], function () {
+    var urls = [];
+    $(".news_con .item a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
