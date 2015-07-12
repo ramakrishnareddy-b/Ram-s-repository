587e7565db196ac982d01cec39fb94c78ae4c7fb
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-13 00:05:11 +0530

    Scripts for Trinidad+and+Tobago+MFA.

--- Corporate+Press+Releases/Government/Trinidad+and+Tobago+MFA_body.js
+++ Corporate+Press+Releases/Government/Trinidad+and+Tobago+MFA_body.js
@@ -1,1 +1,18 @@
 // Trinidad and Tobago
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.foreign.gov.tt/news/2015/jul/6/trinidad-tobago-and-south-africa-establish-memoran/",
+    function () {
+        var t = $(".entry .title")[0].innerText.trim();
+        var ps = $(".entry .news-details-date")[0].innerText.replace("Published: ", "").trim();
+        var l = window.location.protocol + "//" + window.location.host + $(".entry .news-detail a").attr("href");
+        $(".entry .title, .entry .details, .entry .news-detail").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $(".entry")[0].innerText.trim(),
+            links: l,
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Trinidad+and+Tobago+MFA_list.js
+++ Corporate+Press+Releases/Government/Trinidad+and+Tobago+MFA_list.js
@@ -1,1 +1,10 @@
-http://www.foreign.gov.tt/news/
+//http://www.foreign.gov.tt/news/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.foreign.gov.tt/news/"], function () {
+    var urls = [];
+    $("ul.news-list > li .news-title a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
