c3a2c01ea05ba9de707895898b7ee43481c7a876
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-12 23:05:20 +0530

    Scripts for Uganda+MOE2.

--- Corporate+Press+Releases/Government/Uganda+MOE2_body.js
+++ Corporate+Press+Releases/Government/Uganda+MOE2_body.js
@@ -1,1 +1,15 @@
 // Uganda
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.petroleum.go.ug/news/33/CLOSURE-OF-THE-APPLICATION-FOR-QUALIFICATION-OF-THE-LICENSING-ROUND-FOR-PETROLEUM-EXPLORATION-DEVELOPMENT-AND-PRODUCTION-BLOCKS-IN-UGANDA",
+    function () {
+        var t = jQuery(".media-body .blog-desc-container .media-heading")[0].innerText.trim();
+        jQuery(".media-body .blog-desc-container .media-heading").remove();
+        return {
+            title: t,
+            published_s: jQuery(".media-body .blog-date")[0].innerText.replace(/\n/g, " ").trim(),
+            body: jQuery(".media-body .blog-desc-container .overview")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Uganda+MOE2_list.js
+++ Corporate+Press+Releases/Government/Uganda+MOE2_list.js
@@ -1,1 +1,10 @@
-http://www.petroleum.go.ug/news
+//http://www.petroleum.go.ug/news
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.petroleum.go.ug/news"], function () {
+    var urls = [];
+    jQuery(".main-content .article-container h3 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
