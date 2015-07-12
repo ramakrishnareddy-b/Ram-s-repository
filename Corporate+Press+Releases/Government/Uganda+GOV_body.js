b4864130c2bc3644ac7c77ddbd4cc21cae2720bf
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-12 22:41:39 +0530

    Scripts for Uganda+GOV.

--- Corporate+Press+Releases/Government/Uganda+GOV_body.js
+++ Corporate+Press+Releases/Government/Uganda+GOV_body.js
@@ -1,1 +1,12 @@
 // Uganda
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://mediacentre.go.ug/press-release/false-and-misleading-broadcasts",
+    function () {
+        return {
+            title: jQuery("#main2 h1")[0].innerText.trim(),
+            body: jQuery("#main2 .field-name-body")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Uganda+GOV_list.js
+++ Corporate+Press+Releases/Government/Uganda+GOV_list.js
@@ -1,2 +1,11 @@
-http://mediacentre.go.ug/media-releases%20
-http://mediacentre.go.ug/news-updates%20
+//http://mediacentre.go.ug/media-releases%20
+//http://mediacentre.go.ug/news-updates%20
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://mediacentre.go.ug/media-releases%20", "http://mediacentre.go.ug/news-updates%20"], function () {
+    var urls = [];
+    jQuery("#block-system-main ul > li.views-row h2 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
