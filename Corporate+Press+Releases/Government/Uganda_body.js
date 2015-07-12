ec4c9f1c9e1703efa1071f345872cd0298a25c29
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-12 15:36:00 +0530

    Scripts for Uganda.

--- Corporate+Press+Releases/Government/Uganda_body.js
+++ Corporate+Press+Releases/Government/Uganda_body.js
@@ -1,1 +1,16 @@
 // Uganda
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.oilinuganda.org/features/companies/bunyoro-leaders-demand-for-more.html",
+    function () {
+        var t = $("article h2")[0].innerText;
+        var ps = $("article .post_date")[0].innerText.split(" | ")[0];
+        $("article h2, article .articleHeader, article .wp-caption").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $("article")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Uganda_list.js
+++ Corporate+Press+Releases/Government/Uganda_list.js
@@ -1,1 +1,10 @@
-http://www.oilinuganda.org/categories/features/companies
+//http://www.oilinuganda.org/categories/features/companies
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.oilinuganda.org/categories/features/companies"], function () {
+    var urls = [];
+    $("#posts > ul > li > h3 > a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
