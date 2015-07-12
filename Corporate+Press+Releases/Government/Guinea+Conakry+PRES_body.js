315d29fc98d9f0fc66ac9f37d131a80277000cfb
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-12 15:13:54 +0530

    Guinea+Conakry+PRES change for consistency

--- Corporate+Press+Releases/Government/Guinea+Conakry+PRES_body.js
+++ Corporate+Press+Releases/Government/Guinea+Conakry+PRES_body.js
@@ -4,13 +4,11 @@ var main=require(require('system').env['NL_HOME']+'main.js');
 
 main.contentEvaluate("http://www.presidence.gov.gn/index.php/15-actualites/498-declaration-des-chefs-detat-et-de-gouvernement-de-lunion-du-fleuve-mano-sur-les-modalites-de-preparation-de-la-conference-plaidoyer-new-york",
     function(){
-        var cont="";
-        jQuery("#content p").each(function(idx,el){
-            cont+=el.innerText.trim()+"\n";
-        });
+        var t = jQuery("#artcle-heading > h2")[0].innerText;
+        jQuery("#content .item-page #artcle-heading, #content .item-page .itp-social-buttons-box").remove();
         return {
-            title:jQuery("#artcle-heading > h2")[0].innerText,
-            body:cont,
+            title:t,
+            body:jQuery("#content .item-page")[0].innerText.trim(),
             url: document.location.href
         }; //returns object with title,body and url properties.
     }
