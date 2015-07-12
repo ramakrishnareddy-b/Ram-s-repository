108be8e6b1b7ed30656970c6630272c7b19cffb3
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-12 23:27:58 +0530

    Scripts for Trinidad+and+Tobago+USAMB.

--- Corporate+Press+Releases/Government/Trinidad+and+Tobago+USAMB_body.js
+++ Corporate+Press+Releases/Government/Trinidad+and+Tobago+USAMB_body.js
@@ -1,1 +1,14 @@
 // Trinidad and Tobago
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://trinidad.usembassy.gov/warner.html",
+    function () {
+        var t = $(".middle-content-article h1")[0].innerText.trim();
+        $(".middle-content-article h1, .middle-content-article .image-enlarge").remove();
+        return {
+            title: t,
+            body: $(".middle-content-article")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Trinidad+and+Tobago+USAMB_list.js
+++ Corporate+Press+Releases/Government/Trinidad+and+Tobago+USAMB_list.js
@@ -1,1 +1,10 @@
-http://trinidad.usembassy.gov/press_releases.html
+//http://trinidad.usembassy.gov/press_releases.html
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://trinidad.usembassy.gov/press_releases.html"], function () {
+    var urls = [];
+    $(".middle-content-article > ul > li > a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
