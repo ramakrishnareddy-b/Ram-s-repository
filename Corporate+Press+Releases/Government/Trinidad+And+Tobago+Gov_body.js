94ecee49cefd3f2c6bcc766c7be5f52becf4c0fe
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-13 00:20:52 +0530

    Scripts for Trinidad+and+Tobago+GOV.

--- Corporate+Press+Releases/Government/Trinidad+And+Tobago+Gov_body.js
+++ Corporate+Press+Releases/Government/Trinidad+And+Tobago+Gov_body.js
@@ -1,1 +1,16 @@
-// Trinidad And Tobago 
+// Trinidad And Tobago
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.opm.gov.tt/media_centre.php?mid=5&eid=691",
+    function () {
+        var t = $("#results_container h2")[0].innerText.trim();
+        var ps = $("#results_container #event_details .first")[0].innerText.replace("Date: ", "").replace(/,/g, "").trim();
+        $("#results_container h2, #results_container #event_details").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $("#results_container")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Trinidad+And+Tobago+Gov_list.js
+++ Corporate+Press+Releases/Government/Trinidad+And+Tobago+Gov_list.js
@@ -1,1 +1,10 @@
-http://www.opm.gov.tt/media_centre.php?mid=5
+//http://www.opm.gov.tt/media_centre.php?mid=5
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.opm.gov.tt/media_centre.php?mid=5"], function () {
+    var urls = [];
+    $(".page_num .news_item .news_title a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
