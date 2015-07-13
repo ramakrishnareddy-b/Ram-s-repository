87ca61d89a52d9ee56b25c678c6d21abcd66945e
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-13 22:06:45 +0530

    Scripts for Peru+GOV.

--- Corporate+Press+Releases/Government/Peru+GOV_body.js
+++ Corporate+Press+Releases/Government/Peru+GOV_body.js
@@ -1,1 +1,14 @@
 // Peru
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.rree.gob.pe/Noticias/Paginas/NP-089-15.aspx",
+    function () {
+        var t = $("#mrecontent h1")[0].innerText.trim();
+        $("#mrecontent h1").remove();
+        return {
+            title: t,
+            body: $("#mrecontent")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Peru+GOV_list.js
+++ Corporate+Press+Releases/Government/Peru+GOV_list.js
@@ -1,1 +1,10 @@
-http://www.rree.gob.pe/noticias/Paginas/Notas_de_Prensa.aspx
+//http://www.rree.gob.pe/noticias/Paginas/Notas_de_Prensa.aspx
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.rree.gob.pe/noticias/Paginas/Notas_de_Prensa.aspx"], function () {
+    var urls = [];
+    $(".WPU-Tablem2TD h3 > a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
