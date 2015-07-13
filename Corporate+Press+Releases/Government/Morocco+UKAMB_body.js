96970e5bf16f585962275fd998096cef52a2869f
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-14 00:57:44 +0530

    Scripts for Morocco+UKAMB.

--- Corporate+Press+Releases/Government/Morocco+UKAMB_body.js
+++ Corporate+Press+Releases/Government/Morocco+UKAMB_body.js
@@ -1,1 +1,16 @@
 // Morocco
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("https://www.gov.uk/government/news/joint-communique-on-libya--2",
+    function () {
+        var t = $(".newsarticle .headings-block h1")[0].innerText.trim();
+        var ps = $(".newsarticle .headings-block .date")[0].innerText.trim();
+        $(".newsarticle .headings-block, .newsarticle .document-share-links, .newsarticle .inner-block .lead-image-sidebar, .newsarticle .inner-block .js-footer").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $(".newsarticle")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
