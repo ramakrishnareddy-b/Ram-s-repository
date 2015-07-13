776c2073303180c2926fa78f5ae4a116137a1446
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-13 23:51:38 +0530

    Scripts for Myanmar+UKAMB.

--- Corporate+Press+Releases/Government/Myanmar+UKAMB_list.js
+++ Corporate+Press+Releases/Government/Myanmar+UKAMB_list.js
@@ -1,1 +1,10 @@
-https://www.gov.uk/government/announcements?keywords=&announcement_filter_option=all&topics%5B%5D=all&departments%5B%5D=all&world_locations%5B%5D=burma&from_date=&to_date=
+//https://www.gov.uk/government/announcements?keywords=&announcement_filter_option=all&topics%5B%5D=all&departments%5B%5D=all&world_locations%5B%5D=burma&from_date=&to_date=
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["https://www.gov.uk/government/announcements?keywords=&announcement_filter_option=all&topics%5B%5D=all&departments%5B%5D=all&world_locations%5B%5D=burma&from_date=&to_date="], function () {
+    var urls = [];
+    $(".filter-results > ol > li > h3 > a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
