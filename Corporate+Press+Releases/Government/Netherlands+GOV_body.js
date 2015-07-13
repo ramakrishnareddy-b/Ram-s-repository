6dbe931f9f6d5dcd8362dbf7eba2c640c77fdc04
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-13 23:23:08 +0530

    Scripts for Netherlands+GOV.

--- Corporate+Press+Releases/Government/Netherlands+GOV_body.js
+++ Corporate+Press+Releases/Government/Netherlands+GOV_body.js
@@ -1,1 +1,16 @@
 // Netherlands
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.government.nl/news/2015/07/10/no-extraction-of-shale-gas-during-the-next-five-years.html",
+    function () {
+        var t = $(".article h1")[0].innerText.trim();
+        var ps = $(".article .article-meta")[0].innerText.split(" | ")[1].trim();
+        $(".article h1, .article .article-meta, .article .docs-pubs").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $(".article")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Netherlands+GOV_list.js
+++ Corporate+Press+Releases/Government/Netherlands+GOV_list.js
@@ -1,3 +1,12 @@
-http://www.government.nl/news
-http://www.government.nl/ministries/bz/news
-http://www.government.nl/ministries/ienm/news
+//http://www.government.nl/news
+//http://www.government.nl/ministries/bz/news
+//http://www.government.nl/ministries/ienm/news
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.government.nl/news", "http://www.government.nl/ministries/bz/news", "http://www.government.nl/ministries/ienm/news"], function () {
+    var urls = [];
+    $(".article .headline .visual a, .article .regulars a, .news").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
