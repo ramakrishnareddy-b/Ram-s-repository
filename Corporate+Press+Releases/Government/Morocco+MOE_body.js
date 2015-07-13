ed3b5c3614bc632d46002b393f98c88c1d4a3ec9
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-14 01:16:58 +0530

    Scripts for Morocco+MOE.

--- Corporate+Press+Releases/Government/Morocco+MOE_body.js
+++ Corporate+Press+Releases/Government/Morocco+MOE_body.js
@@ -1,1 +1,16 @@
 // Morocco
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.onhym.com/en/news-feed/372-26-06-2015-results-of-lam-1-well-on-lalla-mimouna-permit.html",
+    function () {
+        var t_ps = jQuery(".art-post-inner h2")[0].innerText.split(":");
+        var t = t_ps[1].trim();
+        var ps = t_ps[0].trim();
+        return {
+            title: t,
+            published_s: ps,
+            body: jQuery(".art-post-inner .art-article #articlepxfontsize1")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Morocco+MOE_list.js
+++ Corporate+Press+Releases/Government/Morocco+MOE_list.js
@@ -1,1 +1,10 @@
-http://www.onhym.com/en/news-feed.html
+//http://www.onhym.com/en/news-feed.html
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.onhym.com/en/news-feed.html"], function () {
+    var urls = [];
+    jQuery(".blog .art-postcontent .readmore a, .blog .art-postcontent  ol > li a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
