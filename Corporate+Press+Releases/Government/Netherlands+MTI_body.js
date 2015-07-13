4dd98cc9488febf6a3bad784b571fe212bd5d8b0
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-13 22:39:25 +0530

    Scripts for Netherlands+MTI.

--- Corporate+Press+Releases/Government/Netherlands+MTI_body.js
+++ Corporate+Press+Releases/Government/Netherlands+MTI_body.js
@@ -1,1 +1,16 @@
 // Netherlands
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://mediacentre.go.ug/press-release/false-and-misleading-broadcasts",
+    function () {
+        var t = $(".js_equal_right h1")[0].innerText.trim();
+        var ps = $(".js_equal_right h2")[0].innerText.trim();
+        $(".js_equal_right h1, .js_equal_right h2, .js_equal_right section, .js_equal_right .type-tag, .js_equal_right .image").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $(".js_equal_right")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Netherlands+MTI_list.js
+++ Corporate+Press+Releases/Government/Netherlands+MTI_list.js
@@ -1,1 +1,10 @@
-https://www.tno.nl/en/about-tno/news/
+//https://www.tno.nl/en/about-tno/news/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["https://www.tno.nl/en/about-tno/news/"], function () {
+    var urls = [];
+    $(".list__block > .list__block--item > a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
