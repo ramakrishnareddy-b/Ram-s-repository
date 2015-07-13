976db38b7d06fdf15320cd83b284dc91d7a8db48
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-14 00:21:20 +0530

    Scripts for Mozambique+CNAMB.

--- Corporate+Press+Releases/Government/Mozambique+CNAMB_body.js
+++ Corporate+Press+Releases/Government/Mozambique+CNAMB_body.js
@@ -1,1 +1,16 @@
 // Mozambique
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://mz.china-embassy.org/por/xwdt/t1272485.htm",
+    function () {
+        var t = $("#article .bigtitlee")[0].innerText.trim();
+        var ps = $("#article font")[0].innerText.replace(/\//g,"-").trim();
+        $("#article .bigtitlee, #article font").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $("#article")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Mozambique+CNAMB_list.js
+++ Corporate+Press+Releases/Government/Mozambique+CNAMB_list.js
@@ -1,1 +1,10 @@
-http://mz.china-embassy.org/por/xwdt/
+//http://mz.china-embassy.org/por/xwdt/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://mz.china-embassy.org/por/xwdt/"], function () {
+    var urls = [];
+    $("#docMore > table > tbody > tr > td > a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
