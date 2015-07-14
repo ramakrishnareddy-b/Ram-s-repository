c41658ee68e4b5c9d5387f876a72883979a35bc7
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-14 23:19:34 +0530

    Scripts for Mozambique+GOV.

--- Corporate+Press+Releases/Government/Mozambique+GOV_body.js
+++ Corporate+Press+Releases/Government/Mozambique+GOV_body.js
@@ -1,1 +1,14 @@
 // Mozambique
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.minec.gov.mz/index.php?option=com_content&task=view&id=799&Itemid=1",
+    function () {
+        var t = $("#ja-content .contentheading")[0].innerText.trim();
+        $("#ja-content .pathway, #ja-content .contentheading, #ja-content .buttonheading, #ja-content .pagenav_prev, #ja-content .pagenav_next, #ja-content .back_button").remove();
+        return {
+            title: t,
+            body: $("#ja-content")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Mozambique+GOV_list.js
+++ Corporate+Press+Releases/Government/Mozambique+GOV_list.js
@@ -1,1 +1,10 @@
-http://www.minec.gov.mz
+//http://www.minec.gov.mz
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.minec.gov.mz"], function () {
+    var urls = [];
+    $(".blog a.readon, .blog_more > ul > li > a.blogsection").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
