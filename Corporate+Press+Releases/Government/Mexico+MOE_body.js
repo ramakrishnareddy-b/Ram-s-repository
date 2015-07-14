8f6f15170671648b351f1c00a4650ee566dc5b4e
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-14 22:25:57 +0530

    Scripts for Mexico+MOE.

--- Corporate+Press+Releases/Government/Mexico+MOE_body.js
+++ Corporate+Press+Releases/Government/Mexico+MOE_body.js
@@ -1,1 +1,16 @@
 // Mexico
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://sener.gob.mx/portal/Default_blt.aspx?id=3236",
+    function () {
+        var t = $(".text_cont > p > strong:first")[0].innerText.trim();
+        var ps = $(".text_cont .Fecha_listado")[0].innerText.trim();
+        $(".text_cont > p > strong:first, .text_cont .Fecha_listado, .text_cont img").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $(".text_cont .texto_registro")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Mexico+MOE_list.js
+++ Corporate+Press+Releases/Government/Mexico+MOE_list.js
@@ -1,1 +1,10 @@
-http://sener.gob.mx/portal/Default.aspx?id=901
+//http://sener.gob.mx/portal/Default.aspx?id=901
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://sener.gob.mx/portal/Default.aspx?id=901"], function () {
+    var urls = [];
+    $(".text_cont a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
