46fa02967966d53d862959855647d89f9b732e13
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-14 21:14:41 +0530

    Scripts for Guatemala+MFA.

--- Corporate+Press+Releases/Government/Guatemala+MFA_body.js
+++ Corporate+Press+Releases/Government/Guatemala+MFA_body.js
@@ -1,1 +1,16 @@
-// Guatemala 
+// Guatemala
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.minex.gob.gt/noticias/Noticia.aspx?ID=4634",
+    function () {
+        var t = $("#ctl00_ContentPlaceHolder1_WebNoticiaFull1_Panel1 .TituloNoticia")[0].innerText.trim();
+        var ps = $("#ctl00_ContentPlaceHolder1_WebNoticiaFull1_Panel1 .FechaNoticiaFull")[0].innerText.replace(/\//g, "-").trim();
+        $("#ctl00_ContentPlaceHolder1_WebNoticiaFull1_Panel1 .addthis_toolbox, #ctl00_ContentPlaceHolder1_WebNoticiaFull1_Panel1 .addthis_button_compact, #ctl00_ContentPlaceHolder1_WebNoticiaFull1_Panel1 .FechaNoticiaFull, #ctl00_ContentPlaceHolder1_WebNoticiaFull1_Panel1 .TemaNoticiaFull, #ctl00_ContentPlaceHolder1_WebNoticiaFull1_Panel1 .TituloNoticia, #ctl00_ContentPlaceHolder1_WebNoticiaFull1_Panel1 #tdImagen").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $("#ctl00_ContentPlaceHolder1_WebNoticiaFull1_Panel1")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Guatemala+MFA_list.js
+++ Corporate+Press+Releases/Government/Guatemala+MFA_list.js
@@ -1,1 +1,10 @@
-http://www.minex.gob.gt/noticias/NoticiasxTemas.aspx?cTema=36
+//http://www.minex.gob.gt/noticias/NoticiasxTemas.aspx?cTema=36
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.minex.gob.gt/noticias/NoticiasxTemas.aspx?cTema=36"], function () {
+    var urls = [];
+    $(".dxncContent .dxncItem .dxncItemHeader a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
