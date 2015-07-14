7047e5675295ce23bf682aebb87e0527811a3cc5
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-14 23:42:23 +0530

    Scripts for Mozambique+GOV3.

--- Corporate+Press+Releases/Government/Mozambique+GOV3_body.js
+++ Corporate+Press+Releases/Government/Mozambique+GOV3_body.js
@@ -1,1 +1,14 @@
 // Mozambique
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.portaldogoverno.gov.mz/noticias/news_folder_politica/julho-de-2015/presidente-da-republica-efectua-visita-de-estado-a-portugal/",
+    function () {
+        var t = $(".documentContent .documentFirstHeading")[0].innerText.trim();
+        $(".documentContent .documentFirstHeading, .documentContent .documentActions").remove();
+        return {
+            title: t,
+            body: $(".documentContent")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Mozambique+GOV3_list.js
+++ Corporate+Press+Releases/Government/Mozambique+GOV3_list.js
@@ -1,1 +1,10 @@
-http://www.portaldogoverno.gov.mz/news
+//http://www.portaldogoverno.gov.mz/news
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.portaldogoverno.gov.mz/news"], function () {
+    var urls = [];
+    $('.tileItem h2 a').each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
