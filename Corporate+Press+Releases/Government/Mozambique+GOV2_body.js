cde31520df04ad68f9aff119127ba8c3ef467dc4
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-14 22:58:08 +0530

    Scripts for Mozambique+GOV2.

--- Corporate+Press+Releases/Government/Mozambique+GOV2_body.js
+++ Corporate+Press+Releases/Government/Mozambique+GOV2_body.js
@@ -1,1 +1,16 @@
 // Mozambique
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.funae.co.mz/index.php?option=com_content&view=article&id=578%3A2015-07-06-13-24-13&catid=43%3Anoticias&Itemid=41&lang=en",
+    function () {
+        var t = $("#colunaesquerda .contentheading")[0].innerText.trim();
+        var ps = cs.scanForDate($("#colunaesquerda .createdate")[0].innerText.trim(), "dd MMMM yyyy HH:mm");
+        $("#colunaesquerda .contentheading, #colunaesquerda .buttonheading, #colunaesquerda .createdate, #colunaesquerda #pathway, #colunaesquerda img").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $("#colunaesquerda")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Mozambique+GOV2_list.js
+++ Corporate+Press+Releases/Government/Mozambique+GOV2_list.js
@@ -1,1 +1,10 @@
-http://www.funae.co.mz/index.php?option=com_content&view=section&layout=blog&id=9&Itemid=41&lang=en
+//http://www.funae.co.mz/index.php?option=com_content&view=section&layout=blog&id=9&Itemid=41&lang=en
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.funae.co.mz/index.php?option=com_content&view=section&layout=blog&id=9&Itemid=41&lang=en"], function () {
+    var urls = [];
+    $(".article_column .readon").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
