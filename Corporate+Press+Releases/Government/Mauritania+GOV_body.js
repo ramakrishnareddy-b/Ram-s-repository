46906630e2337d2600551ad92fed55432a7f9331
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-15 23:59:47 +0530

    Scripts for Mauritania+GOV

--- Corporate+Press+Releases/Government/Mauritania+GOV_body.js
+++ Corporate+Press+Releases/Government/Mauritania+GOV_body.js
@@ -1,1 +1,17 @@
 // Mauritania
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.primature.gov.mr/fr/index.php?link=1&id=1491",
+    function () {
+        var t_ps = $(".mytext2 strong").text().split("Nouakchott, ");
+        var t = t_ps[0].trim();
+        var ps = t_ps[1].replace(/\//g, "-").replace(" - ", "");
+        $(".mytext2 strong, .mytext2 img").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $(".mytext2")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Mauritania+GOV_list.js
+++ Corporate+Press+Releases/Government/Mauritania+GOV_list.js
@@ -1,1 +1,10 @@
-http://www.primature.gov.mr/fr/index.php?link=3&idcat=4
+//http://www.primature.gov.mr/fr/index.php?link=3&idcat=4
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.primature.gov.mr/fr/index.php?link=3&idcat=4"], function () {
+    var urls = [];
+    $(".mytext2 strong a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
