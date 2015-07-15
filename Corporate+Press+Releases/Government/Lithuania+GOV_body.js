e2855bb0aa38e87bf634aaee8b876f0cd0517f6f
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-16 00:50:33 +0530

    Scripts for Lithuania+GOV

--- Corporate+Press+Releases/Government/Lithuania+GOV_body.js
+++ Corporate+Press+Releases/Government/Lithuania+GOV_body.js
@@ -1,1 +1,13 @@
 // Lithuania
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("https://www.urm.lt/default/en/news/mfa-statement-on-the-agreement-on-the-iranian-nuclear-program",
+    function () {
+        return {
+            title: $(".widget3 h1")[0].innerText.trim(),
+            published_s: $(".widget3 .news_date")[0].innerText.split("Updated: ")[1].replace(/\./g, '-').trim(),
+            body: $(".widget3 .text")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Lithuania+GOV_list.js
+++ Corporate+Press+Releases/Government/Lithuania+GOV_list.js
@@ -1,1 +1,10 @@
-https://www.urm.lt/default/en/news
+//https://www.urm.lt/default/en/news
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["https://www.urm.lt/default/en/news"], function () {
+    var urls = [];
+    $("#news_list .site_1 a.link").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
