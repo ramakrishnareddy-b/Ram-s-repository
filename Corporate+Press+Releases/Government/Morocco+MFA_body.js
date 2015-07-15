45947b8c520f3216bc0ff4d2f912e99d8e312b14
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-15 22:39:44 +0530

    Scripts for Morocco+MFA

--- Corporate+Press+Releases/Government/Morocco+MFA_body.js
+++ Corporate+Press+Releases/Government/Morocco+MFA_body.js
@@ -1,1 +1,16 @@
 // Morocco
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("https://www.diplomatie.ma/LeMinist%C3%A8re/Lavieinterne/tabid/124/vw/1/ItemID/12270/language/en-US/Default.aspx",
+    function () {
+        var t = $(".ModAdvancedArticlesC .titre_artcile1")[0].innerText.trim();
+        var ps = $(".ModAdvancedArticlesC .titre_artcile2")[0].innerText.trim().replace(/\./g,'-');
+        $(".ModAdvancedArticlesC .titre_artcile1, .ModAdvancedArticlesC .titre_artcile2, .ModAdvancedArticlesC img").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $(".ModAdvancedArticlesC")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Morocco+MFA_list.js
+++ Corporate+Press+Releases/Government/Morocco+MFA_list.js
@@ -1,1 +1,10 @@
-https://www.diplomatie.ma/LeMinistère/M_ArchiveActualites.aspx
+//https://www.diplomatie.ma/LeMinistère/M_ArchiveActualites.aspx
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["https://www.diplomatie.ma/LeMinistère/M_ArchiveActualites.aspx"], function () {
+    var urls = [];
+    $(".ModAdvancedArticlesSatelliteC a.lireaussi").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
