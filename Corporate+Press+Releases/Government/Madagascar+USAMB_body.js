1f9fc49b0aadaee241709a2524ad696019f9746d
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-16 00:33:11 +0530

    Scripts for Madagascar+USAMB

--- Corporate+Press+Releases/Government/Madagascar+USAMB_body.js
+++ Corporate+Press+Releases/Government/Madagascar+USAMB_body.js
@@ -1,1 +1,14 @@
 // Madagascar
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.antananarivo.usembassy.gov/embassy_news/press-releases2/press-releases6.html",
+    function () {
+        var t = $(".middle-content-article h1")[0].innerText.trim();
+        $(".middle-content-article h1, .middle-content-article a").remove();
+        return {
+            title: t,
+            body: $(".middle-content-article")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Madagascar+USAMB_list.js
+++ Corporate+Press+Releases/Government/Madagascar+USAMB_list.js
@@ -1,1 +1,10 @@
-http://www.antananarivo.usembassy.gov/embassy_news/press-releases2.html
+//http://www.antananarivo.usembassy.gov/embassy_news/press-releases2.html
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.antananarivo.usembassy.gov/embassy_news/press-releases2.html"], function () {
+    var urls = [];
+    $(".main_listing .box-body > ul > li > h5 > a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
