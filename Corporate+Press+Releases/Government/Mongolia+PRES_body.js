c5d1a630540dc4aed8b57e7b10ed70c14ec51321
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-15 23:23:57 +0530

    Scripts for Mangolia+PRES

--- Corporate+Press+Releases/Government/Mongolia+PRES_body.js
+++ Corporate+Press+Releases/Government/Mongolia+PRES_body.js
@@ -1,1 +1,13 @@
 // Mongolia
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.contentEvaluate("http://www.president.mn/eng/newsCenter/viewNews.php?newsId=1631",
+    function(){
+        return {
+            title: $("#newsTitle")[0].innerText.trim(),
+            published_s: $("#newsDate")[0].innerText.trim(),
+            body: jQuery("#newsBody")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Mongolia+PRES_list.js
+++ Corporate+Press+Releases/Government/Mongolia+PRES_list.js
@@ -1,1 +1,10 @@
-http://www.president.mn/eng/newsCenter/index.php
+//http://www.president.mn/eng/newsCenter/index.php
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.linksEvaluate(["http://www.president.mn/eng/newsCenter/index.php"],function(){
+    var urls =[];
+    $("ul#latestNews li.title a").each(function(idx,el){
+        urls.push(el.href);
+    });
+    return urls;
+});
