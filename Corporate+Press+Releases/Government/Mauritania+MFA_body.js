4c894381d85530703c5026425a90c319164f24db
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-15 23:43:33 +0530

    Scripts for Mauritania+MFA

--- Corporate+Press+Releases/Government/Mauritania+MFA_body.js
+++ Corporate+Press+Releases/Government/Mauritania+MFA_body.js
@@ -1,1 +1,14 @@
 // Mauritania
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.contentEvaluate("http://www.diplomatie-mr.com/maec/site/index.php?option=com_content&view=article&id=925:notre-pays-participe-a-la-3eme-conference-internationale-sur-le-financement-du-developpement-a-addis-abeba&catid=78&Itemid=649&lang=fr",
+    function(){
+        jQuery(".item-page .art-post .art-postcontent img").remove();
+        return {
+            title: jQuery(".item-page .art-post h2")[0].innerText.trim(),
+            published_s: jQuery(".item-page .art-post .art-postdateicon")[0].innerText.trim().replace("Créé ",""),
+            body: jQuery(".item-page .art-post .art-postcontent")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Mauritania+MFA_list.js
+++ Corporate+Press+Releases/Government/Mauritania+MFA_list.js
@@ -1,1 +1,10 @@
-http://www.diplomatie-mr.com/maec/site/index.php?lang=fr
+//http://www.diplomatie-mr.com/maec/site/index.php?lang=fr
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.linksEvaluate(["http://www.diplomatie-mr.com/maec/site/index.php?lang=fr"],function(){
+    var urls =[];
+    jQuery(".blog .art-post h2 a").each(function(idx,el){
+        urls.push(el.href);
+    });
+    return urls;
+});
