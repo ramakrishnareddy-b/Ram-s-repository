8f7f7b6fbae93934dd16db011d805e4ca68ba99a
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-16 00:22:58 +0530

    Scripts for Lebanon+USAMB

--- Corporate+Press+Releases/Government/Lebanon+USAMB_list.js
+++ Corporate+Press+Releases/Government/Lebanon+USAMB_list.js
@@ -1,1 +1,10 @@
-http://lebanon.usembassy.gov/pressreleases_2015.html
+//http://lebanon.usembassy.gov/pressreleases_2015.html
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://lebanon.usembassy.gov/pressreleases_2015.html"], function () {
+    var urls = [];
+    $(".middle-content-article > ul > li > a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
