75ae8d44ea6b9e1fc9f5225cce5ea84c43d75f6a
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-15 23:10:03 +0530

    Scripts for Madagascar+GOV

--- Corporate+Press+Releases/Government/Madagascar+GOV_body.js
+++ Corporate+Press+Releases/Government/Madagascar+GOV_body.js
@@ -1,1 +1,14 @@
 // Madagascar
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.contentEvaluate("http://www.mprs.gov.mg/?p=549",
+    function(){
+        jQuery("#content .entry img").remove();
+        return {
+            title: jQuery("#content h2.title")[0].innerText.trim(),
+            published_s: jQuery("#content .meta_date")[0].innerText.trim(),
+            body: jQuery("#content .entry")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Madagascar+GOV_list.js
+++ Corporate+Press+Releases/Government/Madagascar+GOV_list.js
@@ -1,1 +1,10 @@
-http://www.mprs.gov.mg/?p=524
+//http://www.mprs.gov.mg/?p=524
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.linksEvaluate(["http://www.mprs.gov.mg/?p=524"],function(){
+    var urls =[];
+    jQuery("#recent-posts-3 > ul > li > a").each(function(idx,el){
+        urls.push(el.href);
+    });
+    return urls;
+});
