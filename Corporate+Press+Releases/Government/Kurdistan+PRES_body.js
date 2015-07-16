bc01d7c6410fa1a6dde8bb29a1001fa616581b2f
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-16 22:22:09 +0530

    Scripts for Scripts for Kurdistan+PRES

--- Corporate+Press+Releases/Government/Kurdistan+PRES_body.js
+++ Corporate+Press+Releases/Government/Kurdistan+PRES_body.js
@@ -1,1 +1,14 @@
 // Kurdistan
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://presidency.krd/english/articledisplay.aspx?id=GEgz/y/PYCQ=",
+    function () {
+        $("#ctl00_main_grid2 strong.red_text_small").remove();
+        return {
+            title: $("#ctl00_main_grid2 .text_boldh1")[0].innerText.trim(),
+            published_s: $("#ctl00_main_grid2 .red_text_small")[0].innerText.trim().replace(/\//g, '-'),
+            body: $("#ctl00_main_grid2 .content")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Kurdistan+PRES_list.js
+++ Corporate+Press+Releases/Government/Kurdistan+PRES_list.js
@@ -1,1 +1,10 @@
-http://presidency.krd/english/default.aspx
+//http://presidency.krd/english/default.aspx
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://presidency.krd/english/default.aspx"], function () {
+    var urls = [];
+    $("#ctl00_main_grid2 a, #ctl00_main_gridhome a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
