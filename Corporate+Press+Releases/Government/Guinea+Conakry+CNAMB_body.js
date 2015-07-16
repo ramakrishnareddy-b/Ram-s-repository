1933a0aa0ce8358343eed5b8cfc2bd80848f3945
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-16 21:18:48 +0530

    Scripts for Scripts for Guinea+Conakry+CNAMB

--- Corporate+Press+Releases/Government/Guinea+Conakry+CNAMB_body.js
+++ Corporate+Press+Releases/Government/Guinea+Conakry+CNAMB_body.js
@@ -1,1 +1,13 @@
 // Guinea Conakry
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://gn.chineseembassy.org/fra/zxxx/t1281227.htm",
+    function () {
+        return {
+            title: $("#article font")[0].innerText.trim(),
+            published_s: $("#article font")[2].innerText.trim(),
+            body: $("#article .defaultfont")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Guinea+Conakry+CNAMB_list.js
+++ Corporate+Press+Releases/Government/Guinea+Conakry+CNAMB_list.js
@@ -1,1 +1,10 @@
-http://gn.chineseembassy.org/fra/zxxx/
+//http://gn.chineseembassy.org/fra/zxxx/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://gn.chineseembassy.org/fra/zxxx/"], function () {
+    var urls = [];
+    $("#content a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
