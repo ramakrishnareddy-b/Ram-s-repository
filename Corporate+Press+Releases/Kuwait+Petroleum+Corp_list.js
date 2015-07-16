4c1586e76a1b1bbc3c14a0ce3cf27e7a9b299654
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-17 01:00:31 +0530

    Scripts for Kuwait+Petroleum+Corp

--- Corporate+Press+Releases/Kuwait+Petroleum+Corp_list.js
+++ Corporate+Press+Releases/Kuwait+Petroleum+Corp_list.js
@@ -1,1 +1,10 @@
-https://www.kpc.com.kw/press/press-release/Pages/default.aspx
+//https://www.kpc.com.kw/press/press-release/Pages/default.aspx
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["https://www.kpc.com.kw/press/press-release/Pages/default.aspx"], function () {
+    var urls = [];
+    $(".dfwp-list .dfwp-item .link-item a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
