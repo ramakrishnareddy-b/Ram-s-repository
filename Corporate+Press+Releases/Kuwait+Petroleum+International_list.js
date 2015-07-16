3d19b041e8373c395b28062a7f4b3c8f6acdda86
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-17 01:13:47 +0530

    Scripts for Kuwait+Petroleum+International

--- Corporate+Press+Releases/Kuwait+Petroleum+International_list.js
+++ Corporate+Press+Releases/Kuwait+Petroleum+International_list.js
@@ -1,1 +1,10 @@
-http://www.q8.com/content/our-news/Pages/Default.aspx
+//http://www.q8.com/content/our-news/Pages/Default.aspx
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.linksEvaluate(["http://www.q8.com/content/our-news/Pages/Default.aspx"],function(){
+    var urls =[];
+    $("#column .item .link-item a").each(function(idx,el){
+        urls.push(el.href);
+    });
+    return urls;
+});
