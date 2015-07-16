bb518265127119382dcdfa04c1d698fdb5e3a4f8
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-17 00:04:25 +0530

    Scripts for Kuwait+Gulf+Oil+Co

--- Corporate+Press+Releases/Kuwait+Gulf+Oil+Co_list.js
+++ Corporate+Press+Releases/Kuwait+Gulf+Oil+Co_list.js
@@ -1,1 +1,10 @@
-http://www.kgoc.com/Pages/NewsListing.aspx
+//http://www.kgoc.com/Pages/NewsListing.aspx
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.kgoc.com/Pages/NewsListing.aspx"], function () {
+    var urls = [];
+    $(".cbq-layout-main > a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
