73d8758e8efccb1c7c137ff82a6c97301662227f
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-16 23:03:46 +0530

    Scripts for Scripts for Mongolia+PRES

--- Corporate+Press+Releases/Government/Mongolia+USAMB_body.js
+++ Corporate+Press+Releases/Government/Mongolia+USAMB_body.js
@@ -1,1 +1,16 @@
 // Mongolia
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://mongolia.usembassy.gov/news_071115.html",
+    function () {
+        var t = $("#middle-content-article h1")[0].innerText.trim();
+        var ps = cs.scanForDate($('#middle-content-article h6').text(), "dd MMMM yyyy");
+        $("#middle-content-article h1, #middle-content-article h6, #middle-content-article a").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $("#middle-content-article")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Mongolia+USAMB_list.js
+++ Corporate+Press+Releases/Government/Mongolia+USAMB_list.js
@@ -1,2 +1,12 @@
-http://mongolia.usembassy.gov/news.html
-http://mongolia.usembassy.gov/press-releases.html
+//http://mongolia.usembassy.gov/news.html
+//http://mongolia.usembassy.gov/press-releases.html
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://mongolia.usembassy.gov/news.html", "http://mongolia.usembassy.gov/press-releases.html"], function () {
+    var urls = [];
+    $(".main_listing .ipagination").remove();
+    $(".main_listing .box-body h5 > a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
