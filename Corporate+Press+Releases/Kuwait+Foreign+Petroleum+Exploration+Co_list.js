3de3334ab87d6c08e3c0fb61cc0e17e73f764283
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-16 23:54:19 +0530

    Scripts for Kuwait+Foreign+Petroleum+Exploration+Co

--- Corporate+Press+Releases/Kuwait+Foreign+Petroleum+Exploration+Co_list.js
+++ Corporate+Press+Releases/Kuwait+Foreign+Petroleum+Exploration+Co_list.js
@@ -1,1 +1,10 @@
-http://www.kufpec.com/AboutKUFPEC/Pages/NewsRoom.aspx#myAnchor
+//http://www.kufpec.com/AboutKUFPEC/Pages/NewsRoom.aspx#myAnchor
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.kufpec.com/AboutKUFPEC/Pages/NewsRoom.aspx#myAnchor"], function () {
+    var urls = [];
+    $("#WebPartWPQ1 .newsByline a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
