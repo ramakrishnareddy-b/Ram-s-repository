c755bc45ca1afd534396563c590bc9497dca6d48
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-16 23:25:51 +0530

    Scripts for Scripts for Uganda+MFA

--- Corporate+Press+Releases/Government/Uganda+MFA_body.js
+++ Corporate+Press+Releases/Government/Uganda+MFA_body.js
@@ -1,1 +1,16 @@
 // Uganda
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.mofa.go.ug/data/dnews/168/Uganda%20Signs%20an%20Inter%20Governmental%20Commission%20on%20Economic,%20Scientific%20and%20Technical%20Cooperation%20With%20Russia.html",
+    function () {
+        var t = $(".publications_holder > h2")[0].innerText.trim();
+        var ps = $(".publications_holder .caption")[0].innerText.trim();
+        $(".publications_holder h2, .publications_holder .caption, .publications_holder img").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $(".publications_holder")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Uganda+MFA_list.js
+++ Corporate+Press+Releases/Government/Uganda+MFA_list.js
@@ -1,1 +1,10 @@
-http://www.mofa.go.ug/data/dnews/168/Uganda%20Signs%20an%20Inter%20Governmental%20Commission%20on%20Economic,%20Scientific%20and%20Technical%20Cooperation%20With%20Russia.html
+//http://www.mofa.go.ug/data/dnews/168/Uganda%20Signs%20an%20Inter%20Governmental%20Commission%20on%20Economic,%20Scientific%20and%20Technical%20Cooperation%20With%20Russia.html
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.linksEvaluate(["http://www.mofa.go.ug/data/dnews/168/Uganda%20Signs%20an%20Inter%20Governmental%20Commission%20on%20Economic,%20Scientific%20and%20Technical%20Cooperation%20With%20Russia.html"],function(){
+    var urls =[];
+    $(".publications_holder > h2 > a, #sub_news .pubdiscription > h2 > a").each(function(idx,el){
+        urls.push(el.href);
+    });
+    return urls;
+});
