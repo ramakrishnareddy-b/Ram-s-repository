c151214b33322d8b874241b5ead7d0c1b36ede1e
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-17 00:22:32 +0530

    Scripts for Kuwait+National+Petroleum+Co

--- Corporate+Press+Releases/Kuwait+National+Petroleum+Co_list.js
+++ Corporate+Press+Releases/Kuwait+National+Petroleum+Co_list.js
@@ -1,1 +1,10 @@
-http://www.knpc.com.kw/en/MediaCentre/News/Pages/default.aspx
+//http://www.knpc.com.kw/en/MediaCentre/News/Pages/default.aspx
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.knpc.com.kw/en/MediaCentre/News/Pages/default.aspx"], function () {
+    var urls = [];
+    $(".cbq-layout-main .dfwp-item .moreBtn").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
