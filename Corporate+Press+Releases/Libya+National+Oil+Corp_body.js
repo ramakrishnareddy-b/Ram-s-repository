de7cd1b77b2231999dd74903394135bb9817bc30
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-17 18:59:57 +0530

    Scripts for Libya+National+Oil+Corp

--- Corporate+Press+Releases/Libya+National+Oil+Corp_body.js
+++ Corporate+Press+Releases/Libya+National+Oil+Corp_body.js
@@ -1,1 +1,13 @@
 // Libya
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://noc.ly/index.php/en/new-4/953-the-statement-by-the-national-oil-corporation-,-the-national-general-company-for-maritime-transport-and-the-general-company-for-electricity-concerning-the-incident-of-shelling-the-tanker-%E2%80%9C-anwar-africa%E2%80%9D-at-the-gulf-steam-station-in-sirt-city-2",
+    function () {
+        jQuery(".item-page img").remove();
+        return {
+            title: jQuery(".item-page .article-title")[0].innerText.trim(),
+            body: jQuery(".item-page .article-content")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Libya+National+Oil+Corp_list.js
+++ Corporate+Press+Releases/Libya+National+Oil+Corp_list.js
@@ -1,1 +1,10 @@
-http://noc.ly/index.php/en/new-4
+//http://noc.ly/index.php/en/new-4
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://noc.ly/index.php/en/new-4"], function () {
+    var urls = [];
+    jQuery(".table td.list-title a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
