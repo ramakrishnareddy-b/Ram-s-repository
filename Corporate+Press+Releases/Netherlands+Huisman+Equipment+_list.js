18811b13c694bbc6a6c102aae326ff8640053ddf
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-17 21:49:25 +0530

    Scripts for Netherlands+Huisman+Equipment

--- Corporate+Press+Releases/Netherlands+Huisman+Equipment+_list.js
+++ Corporate+Press+Releases/Netherlands+Huisman+Equipment+_list.js
@@ -1,1 +1,11 @@
-http://www.huismanequipment.com/en/news
+//http://www.huismanequipment.com/en/news
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.huismanequipment.com/en/news"], function () {
+    var urls = [];
+    $(".nieuwsJaarOverzicht a.arrow").remove();
+    $(".nieuwsJaarOverzicht a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
