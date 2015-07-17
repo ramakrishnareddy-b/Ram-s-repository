7c94f0e093907b5323e8c41993dc195ad675c85c
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-17 19:55:18 +0530

    Scripts for Netherlands+ARCADIS

--- Corporate+Press+Releases/Netherlands+ARCADIS_list.js
+++ Corporate+Press+Releases/Netherlands+ARCADIS_list.js
@@ -1,1 +1,10 @@
-http://www.arcadis.com/press_releases.aspx
+//http://www.arcadis.com/press_releases.aspx
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.arcadis.com/press_releases.aspx"], function () {
+    var urls = [];
+    $(".rgMasterTable #ListItemLink a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
