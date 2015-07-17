8f2da91dcca48b48016602d25395ded6b0f9742b
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-17 20:57:39 +0530

    Scripts for Netherlands+Fairmount+Marine

--- Corporate+Press+Releases/Netherlands+Fairmount+Marine_body.js
+++ Corporate+Press+Releases/Netherlands+Fairmount+Marine_body.js
@@ -1,1 +1,16 @@
 // Netherlands
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.fairmount.nl/index.php/news/detail/285",
+    function () {
+        var t = $(".content_main h2")[0].innerText.trim();
+        var ps = cs.scanForDate($(".content_main h4.news_date")[0].innerText.trim(), "yyyy-MM-dd");
+        $(".content_main h2, .content_main h4.news_date, .content_main img").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $(".content_main")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Netherlands+Fairmount+Marine_list.js
+++ Corporate+Press+Releases/Netherlands+Fairmount+Marine_list.js
@@ -1,1 +1,10 @@
-http://www.fairmount.nl/index.php/news_archive
+//http://www.fairmount.nl/index.php/news_archive
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.fairmount.nl/index.php/news_archive"], function () {
+    var urls = [];
+    $(".content_main h2 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
