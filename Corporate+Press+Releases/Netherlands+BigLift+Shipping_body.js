eb55be8c6d4bd3a84627c860f678c69a1ee62b53
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-17 20:08:24 +0530

    Scripts for Netherlands+BigLift+Shipping

--- Corporate+Press+Releases/Netherlands+BigLift+Shipping_body.js
+++ Corporate+Press+Releases/Netherlands+BigLift+Shipping_body.js
@@ -1,1 +1,16 @@
 // Netherlands
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.bigliftshipping.com/news/latest/biglift-shipping-joins-trident-alliance-",
+    function () {
+        var t = $(".content h1")[0].innerText.trim();
+        var ps = cs.scanForDate($(".content .posted")[0].innerText.trim(), "dd MMMM yyyy");
+        $(".content h1, .content .posted, .content img, .content a").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $(".content")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Netherlands+BigLift+Shipping_list.js
+++ Corporate+Press+Releases/Netherlands+BigLift+Shipping_list.js
@@ -1,1 +1,10 @@
-http://www.bigliftshipping.com/news/latest
+//http://www.bigliftshipping.com/news/latest
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.bigliftshipping.com/news/latest"], function () {
+    var urls = [];
+    $(".bricks .brick a.button").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
