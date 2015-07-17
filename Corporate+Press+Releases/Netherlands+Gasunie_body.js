ac340bfde0767f24db6d95c1e5728170f3244511
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-17 21:09:06 +0530

    Scripts for Netherlands+Gasunie

--- Corporate+Press+Releases/Netherlands+Gasunie_body.js
+++ Corporate+Press+Releases/Netherlands+Gasunie_body.js
@@ -1,1 +1,13 @@
-// Netherlands 
+// Netherland
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.gasunie.nl/en/news/gasunie-en-gazprom-willen-samen-inzet-van-lng-als-schoner-brandst",
+    function () {
+        return {
+            title: jQuery("#main h1")[0].innerText.trim(),
+            published_s: cs.scanForDate(jQuery("#main .geel")[0].innerText.trim(), "dd-MM-yyyy"),
+            body: jQuery("#main .alinea")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
