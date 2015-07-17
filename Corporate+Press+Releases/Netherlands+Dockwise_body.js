974a19716284d98d3b7cf76e93cef8934dfaac24
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-17 20:28:02 +0530

    Scripts for Netherlands+Dockwise

--- Corporate+Press+Releases/Netherlands+Dockwise_body.js
+++ Corporate+Press+Releases/Netherlands+Dockwise_body.js
@@ -1,1 +1,16 @@
-// Netherlands 
+// Netherlands
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.dockwise.com/readmore/pressrelease-37.html",
+    function () {
+        var t = $(".pressrelease h3")[0].innerText.trim();
+        var ps = $(".pressrelease h5.publish-date")[0].innerText.trim();
+        $(".pressrelease h3, .pressrelease h5, .pressrelease .navigation-overview").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: cs.textUpto($(".pressrelease")[0].innerText.trim(), "This press release can also be found on our website"),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
