0df4ad3a79030a9f5d4c0506a8b617698bfa17fc
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-20 13:48:17 +0530

    Scripts for CNOOC+China

--- Corporate+Press+Releases/CNOOC+China_body.js
+++ Corporate+Press+Releases/CNOOC+China_body.js
@@ -1,1 +1,14 @@
 // CNOOC
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.cnoocltd.com/art/2015/6/30/art_14041_2035001.html",
+    function () {
+        var t = $(".Introduction .title").text().trim();
+        $(".Introduction .title").remove();
+        return {
+            title: t,
+            body: $(".Introduction")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/CNOOC+China_list.js
+++ Corporate+Press+Releases/CNOOC+China_list.js
@@ -1,1 +1,10 @@
-http://www.cnoocltd.com/col/col14041/index.html
+//http://www.cnoocltd.com/col/col14041/index.html
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.cnoocltd.com/col/col14041/index.html"], function () {
+    var urls = [];
+    $(".simple_pgContainer a.gszc").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
