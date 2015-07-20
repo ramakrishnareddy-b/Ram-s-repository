35e9dd15d79458bb9ef1b34bfba7d2fbf54858e0
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-20 15:34:55 +0530

    Scripts for ConocoPhillips+USA

--- Corporate+Press+Releases/ConocoPhillips+USA_list.js
+++ Corporate+Press+Releases/ConocoPhillips+USA_list.js
@@ -1,1 +1,10 @@
-http://www.conocophillips.com/newsroom/Pages/default.aspx
+//http://www.conocophillips.com/newsroom/Pages/default.aspx
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.conocophillips.com/newsroom/Pages/default.aspx"], function () {
+    var urls = [];
+    $(".news-pod li a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
