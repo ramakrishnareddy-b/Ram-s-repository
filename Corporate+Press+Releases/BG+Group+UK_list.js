6d1a8a71b220dbd5bea3e4fbf20c1f3f877ce0bf
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-20 13:27:01 +0530

    Scripts for BG+Group+UK

--- Corporate+Press+Releases/BG+Group+UK_list.js
+++ Corporate+Press+Releases/BG+Group+UK_list.js
@@ -1,1 +1,10 @@
-http://www.bg-group.com/18/media/
+//http://www.bg-group.com/18/media/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.bg-group.com/18/media/"], function () {
+    var urls = [];
+    $(".mediatile h3 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
