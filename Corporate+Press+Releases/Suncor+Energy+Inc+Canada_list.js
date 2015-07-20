4534b8f4f1e1bc262c79aaaf00ff9ef56c63dd36
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-20 20:16:07 +0530

    Scripts for Suncor+Energy+Inc+Canada

--- Corporate+Press+Releases/Suncor+Energy+Inc+Canada_list.js
+++ Corporate+Press+Releases/Suncor+Energy+Inc+Canada_list.js
@@ -1,1 +1,10 @@
-http://www.suncor.com/en/newsroom/404.aspx
+//http://www.suncor.com/en/newsroom/404.aspx
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.suncor.com/en/newsroom/404.aspx"], function () {
+    var urls = [];
+    $(".rightColPageData #newsItem > a.bluetextbold").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
