485b1b966b3a9520229a212d042d6d6c0c49cd3b
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-23 21:29:13 +0530

    Scripts for UK+Petrofac+Ltd

--- Corporate+Press+Releases/UK+Petrofac+Ltd_list.js
+++ Corporate+Press+Releases/UK+Petrofac+Ltd_list.js
@@ -1,1 +1,10 @@
-http://www.petrofac.com/media/news.aspx?Section=PRESS%20RELEASES
+//http://www.petrofac.com/media/news.aspx?Section=PRESS%20RELEASES
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.petrofac.com/media/news.aspx?Section=PRESS%20RELEASES"], function () {
+    var urls = [];
+    $("article h1 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
