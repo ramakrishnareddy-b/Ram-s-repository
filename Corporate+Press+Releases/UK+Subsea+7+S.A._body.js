792daac19695d3261ffb2696e5ef5623de7c2ba2
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-23 22:04:02 +0530

    Scripts for UK+Subsea+7+S.A.

--- Corporate+Press+Releases/UK+Subsea+7+S.A._body.js
+++ Corporate+Press+Releases/UK+Subsea+7+S.A._body.js
@@ -1,1 +1,13 @@
-// Subsea 7 S.A. 
+// Subsea 7 S.A.
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.subsea7.com/en/media-centre/media-centre-landing-page-press-releases/subsea-7-s-a--announces-an-internal-reorganisation-and-changes-t.html",
+    function () {
+        return {
+            title: $(".blue_n_plain .title")[0].innerText.trim(),
+            published_s: $(".blue_n_plain .date")[0].innerText.trim(),
+            body: $(".blue_n_plain .text")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
