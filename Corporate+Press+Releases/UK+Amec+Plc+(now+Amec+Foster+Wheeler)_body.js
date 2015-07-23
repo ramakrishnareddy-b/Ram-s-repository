7f628b6a5496727879e0190cbbfbe8ba763996ba
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-23 21:12:19 +0530

    Scripts for UK+Amec+Plc+(now+Amec+Foster+Wheeler)

--- Corporate+Press+Releases/UK+Amec+Plc+(now+Amec+Foster+Wheeler)_body.js
+++ Corporate+Press+Releases/UK+Amec+Plc+(now+Amec+Foster+Wheeler)_body.js
@@ -1,1 +1,15 @@
 // Amec Plc (now Amec Foster Wheeler)
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.amecfw.com/media/news-releases/2015/excellent-physics-employer",
+    function () {
+        var t = $(".editable-content h1")[0].innerText.trim();
+        $(".editable-content h1").remove();
+        return {
+            title: t,
+            published_s: $(".column.left p")[0].innerText.trim(),
+            body: $(".editable-content")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/UK+Amec+Plc+(now+Amec+Foster+Wheeler)_list.js
+++ Corporate+Press+Releases/UK+Amec+Plc+(now+Amec+Foster+Wheeler)_list.js
@@ -1,1 +1,10 @@
-http://www.amecfw.com/media
+//http://www.amecfw.com/media
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.amecfw.com/media"], function () {
+    var urls = [];
+    $(".investor-info li a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
