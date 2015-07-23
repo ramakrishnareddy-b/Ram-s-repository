1f064fd22a4b780df800b754590dc7408ba88e2f
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-23 23:40:06 +0530

    Scripts for USA+Consol+Energy+Inc

--- Corporate+Press+Releases/USA+Consol+Energy+Inc_body.js
+++ Corporate+Press+Releases/USA+Consol+Energy+Inc_body.js
@@ -1,1 +1,13 @@
 // Consol Energy Inc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://phx.corporate-ir.net/phoenix.zhtml?c=66439&p=irol-newsArticle&ID=2068945",
+    function () {
+        return {
+            title: jQuery(".ccbnContent .ccbnTtl")[0].innerText.trim(),
+            published_s: cs.scanForDate(jQuery(".ccbnContent .prfix")[0].innerText.trim(), "MMMM dd, yyyy"),
+            body: jQuery(".ccbnContent .prfix")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Consol+Energy+Inc_list.js
+++ Corporate+Press+Releases/USA+Consol+Energy+Inc_list.js
@@ -1,1 +1,13 @@
-http://phx.corporate-ir.net/phoenix.zhtml?c=66439&p=irol-news&nyo=0
+//http://phx.corporate-ir.net/phoenix.zhtml?c=66439&p=irol-news&nyo=0
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://phx.corporate-ir.net/phoenix.zhtml?c=66439&p=irol-news&nyo=0"], function () {
+    var urls = [];
+    var count = -1;
+    jQuery(".pad5 a.ccbnTblLnk").each(function (idx, el) {
+        if(++count%2 == 0){
+            urls.push(el.href);
+        }
+    });
+    return urls;
+});
