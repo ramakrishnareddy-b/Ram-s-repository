ed51df9230e5ecca8990921b527f252e9f18f088
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-23 23:55:59 +0530

    Scripts for USA+Denbury+Resources+Inc

--- Corporate+Press+Releases/USA+Denbury+Resources+Inc_list.js
+++ Corporate+Press+Releases/USA+Denbury+Resources+Inc_list.js
@@ -1,1 +1,10 @@
-http://www.denbury.com/investor-relations/press-releases/default.aspx
+//http://www.denbury.com/investor-relations/press-releases/default.aspx
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.denbury.com/investor-relations/press-releases/default.aspx"], function () {
+    var urls = [];
+    $(".NewsListContainer .ModuleHeadlineLink").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
