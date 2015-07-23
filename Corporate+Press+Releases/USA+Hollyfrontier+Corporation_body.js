8538badfe5791f6b9bfded3a15ba2399bd88847f
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-24 00:19:42 +0530

    Scripts for USA+Hollyfrontier+Corporation

--- Corporate+Press+Releases/USA+Hollyfrontier+Corporation_body.js
+++ Corporate+Press+Releases/USA+Hollyfrontier+Corporation_body.js
@@ -1,1 +1,13 @@
 // Hollyfrontier Corporation
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://investor.hollyfrontier.com/releasedetail.cfm?ReleaseID=920255",
+    function () {
+        return {
+            title: jQuery("#ndq-article h1")[0].innerText.trim(),
+            published_s: jQuery("#ndq-article .ndq-leftcol")[0].innerText.trim(),
+            body: jQuery("#ndq-article #ndq-releasebody")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Hollyfrontier+Corporation_list.js
+++ Corporate+Press+Releases/USA+Hollyfrontier+Corporation_list.js
@@ -1,1 +1,10 @@
-http://investor.hollyfrontier.com/releases.cfm
+//http://investor.hollyfrontier.com/releases.cfm
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://investor.hollyfrontier.com/releases.cfm"], function () {
+    var urls = [];
+    jQuery(".wsh-dataTable a.ndqicon-html-1").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
