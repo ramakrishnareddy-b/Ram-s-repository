427aa9a61bd3529f8160f8ef1ca7238244961ac5
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-24 22:13:21 +0530

    Scripts for Canada+Meg+Energy+Corporation

--- Corporate+Press+Releases/Canada+Meg+Energy+Corporation_body.js
+++ Corporate+Press+Releases/Canada+Meg+Energy+Corporation_body.js
@@ -1,1 +1,15 @@
 // Meg Energy Corporation
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.contentEvaluate("http://www.megenergy.com/news-room/article/meg-energy-release-second-quarter-financial-results-3",
+    function(){
+        $("#page .body-item .view-links").remove();
+        var b = cs.textUpto($("#page .body-item")[0].innerText.trim(), "For further information, please contact:");
+        return {
+            title: $("#page h1")[0].innerText.trim(),
+            published_s: cs.scanForDate(b, "MMMM dd, yyyy"),
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Canada+Meg+Energy+Corporation_list.js
+++ Corporate+Press+Releases/Canada+Meg+Energy+Corporation_list.js
@@ -1,1 +1,10 @@
-http://www.megenergy.com/news-room/latest-news
+//http://www.megenergy.com/news-room/latest-news
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.megenergy.com/news-room/latest-news"], function () {
+    var urls = [];
+    $(".view-content .news-title a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
