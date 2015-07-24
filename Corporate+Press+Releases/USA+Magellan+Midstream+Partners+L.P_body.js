7e51b574e883507ed4796a65e4d355f557ec8c88
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-24 09:17:58 +0530

    Scripts for USA+Magellan+Midstream+Partners+L.P

--- Corporate+Press+Releases/USA+Magellan+Midstream+Partners+L.P_body.js
+++ Corporate+Press+Releases/USA+Magellan+Midstream+Partners+L.P_body.js
@@ -1,1 +1,13 @@
 // Magellan Midstream Partners L.P
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.magellanlp.com/magellannews.aspx?id=2147484073",
+    function () {
+        return {
+            title: $("#titleLbl")[0].innerText.trim(),
+            published_s: $("#pr-date")[0].innerText.trim(),
+            body: $("#ContentBlock1")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
