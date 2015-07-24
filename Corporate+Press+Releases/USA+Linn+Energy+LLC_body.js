617d64abcc92efd817c1cadd1281c31aa2afcef8
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-24 09:00:44 +0530

    Scripts for USA+Linn+Energy+LLC

--- Corporate+Press+Releases/USA+Linn+Energy+LLC_body.js
+++ Corporate+Press+Releases/USA+Linn+Energy+LLC_body.js
@@ -1,1 +1,13 @@
 // Linn Energy LLC
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://ir.linnenergy.com/releasedetail.cfm?releaseid=920650",
+    function () {
+        return {
+            title: jQuery("#ndq-article h1")[0].innerText.trim(),
+            published_s: jQuery("#ndq-article .ndq-leftcol")[0].innerText.trim().replace(/\//g, '-'),
+            body: jQuery("#ndq-article #ndq-releasebody")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
