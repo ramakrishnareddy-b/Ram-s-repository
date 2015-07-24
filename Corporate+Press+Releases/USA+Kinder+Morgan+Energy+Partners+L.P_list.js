39dd163f32904e7e2f6a46e7c1bad6668af3597d
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-24 08:46:24 +0530

    Scripts for USA+Kinder+Morgan+Energy+Partners+L.P

--- Corporate+Press+Releases/USA+Kinder+Morgan+Energy+Partners+L.P_list.js
+++ Corporate+Press+Releases/USA+Kinder+Morgan+Energy+Partners+L.P_list.js
@@ -1,1 +1,10 @@
-http://news.kindermorgan.com/press-releases/all/all/All
+//http://news.kindermorgan.com/press-releases/all/all/All
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://news.kindermorgan.com/press-releases/all/all/All"], function () {
+    var urls = [];
+    $(".item-list .views-field-title .field-content a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
