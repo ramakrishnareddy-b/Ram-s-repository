d6540c00170e037e22f1270f01294ce6bb5cb422
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-24 22:28:57 +0530

    Scripts for Canada+Pacific+Rubiales+Energy+Corporation

--- Corporate+Press+Releases/Canada+Pacific+Rubiales+Energy+Corporation_list.js
+++ Corporate+Press+Releases/Canada+Pacific+Rubiales+Energy+Corporation_list.js
@@ -1,1 +1,10 @@
-http://pacificrubiales.mediaroom.com/index.php?s=20295
+//http://pacificrubiales.mediaroom.com/index.php?s=20295
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://pacificrubiales.mediaroom.com/index.php?s=20295"], function () {
+    var urls = [];
+    $(".spintable .item .item_name a.itemlink").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
