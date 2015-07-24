226ffb57275e56fe17beaa67156c6026148647f7
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-24 21:13:10 +0530

    Scripts for Canada+Arc+Resources+Ltd

--- Corporate+Press+Releases/Canada+Arc+Resources+Ltd_body.js
+++ Corporate+Press+Releases/Canada+Arc+Resources+Ltd_body.js
@@ -1,1 +1,13 @@
 // Arc Resources Ltd
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.contentEvaluate("http://arcresources.mediaroom.com/2015-07-16-ARC-Resources-Ltd-Confirms-August-17-2015-Dividend-Amount",
+    function(){
+        return {
+            title: $(".wd_news_releases-detail h1.detail_header")[0].innerText.trim(),
+            published_s: $(".wd_news_releases-detail .release_data")[0].innerText.trim(),
+            body: $(".wd_news_releases-detail .news_body")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Canada+Arc+Resources+Ltd_list.js
+++ Corporate+Press+Releases/Canada+Arc+Resources+Ltd_list.js
@@ -1,1 +1,10 @@
-http://arcresources.mediaroom.com
+//http://arcresources.mediaroom.com
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://arcresources.mediaroom.com"], function () {
+    var urls = [];
+    $(".spintable .item a.itemlink").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
