c0e0b03250d5123574c7d2f7d19c703cc4abd173
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-24 23:02:22 +0530

    Scripts for Canada+Sherritt+International+Corporation

--- Corporate+Press+Releases/Canada+Sherritt+International+Corporation_list.js
+++ Corporate+Press+Releases/Canada+Sherritt+International+Corporation_list.js
@@ -1,1 +1,10 @@
-http://www.sherritt.com/newsroom
+//http://www.sherritt.com/newsroom
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.sherritt.com/newsroom"], function () {
+    var urls = [];
+    jQuery(".careertbl a.style2").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
