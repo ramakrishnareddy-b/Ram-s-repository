62e6c596b35cdb20a573f0ef41afdbbfad4a58e3
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-24 20:39:09 +0530

    Scripts for Australia+WorleyParsons+Ltd

--- Corporate+Press+Releases/Australia+WorleyParsons+Ltd_list.js
+++ Corporate+Press+Releases/Australia+WorleyParsons+Ltd_list.js
@@ -1,1 +1,10 @@
-http://www.worleyparsons.com/News/Pages/default.aspx
+//http://www.worleyparsons.com/News/Pages/default.aspx
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.worleyparsons.com/News/Pages/default.aspx"], function () {
+    var urls = [];
+    $(".news-line-item .title a.link").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
