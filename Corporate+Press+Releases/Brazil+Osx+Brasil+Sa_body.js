ea4efc787eac768bb40dc667832584ba5db37c00
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-24 20:55:26 +0530

    Scripts for Brazil+Osx+Brasil+Sa

--- Corporate+Press+Releases/Brazil+Osx+Brasil+Sa_body.js
+++ Corporate+Press+Releases/Brazil+Osx+Brasil+Sa_body.js
@@ -1,1 +1,15 @@
 // Osx Brasil Sa
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.contentEvaluate("http://www.osx.com.br/cgi/cgilua.exe/sys/start.htm?infoid=441&lng=us&sid=115&tpl=view_ultimas_noticias",
+    function(){
+        $(".materia .breadcrumb, .materia .botao_voltar").remove();
+        var t = $(".materia b")[0].innerText.trim();
+        $(".materia b").remove();
+        return {
+            title: t,
+            body: $(".materia")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Brazil+Osx+Brasil+Sa_list.js
+++ Corporate+Press+Releases/Brazil+Osx+Brasil+Sa_list.js
@@ -1,1 +1,10 @@
-http://www.osx.com.br/cgi/cgilua.exe/sys/start.htm?sid=115&lng=us
+//http://www.osx.com.br/cgi/cgilua.exe/sys/start.htm?sid=115&lng=us
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.osx.com.br/cgi/cgilua.exe/sys/start.htm?sid=115&lng=us"], function () {
+    var urls = [];
+    $(".textodamateria a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
