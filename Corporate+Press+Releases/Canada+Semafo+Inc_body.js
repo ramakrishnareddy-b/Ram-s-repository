061cee4385728e5ea4168c916f95997ef1b6f6dd
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-24 22:47:47 +0530

    Scripts for Canada+Semafo+Inc

--- Corporate+Press+Releases/Canada+Semafo+Inc_body.js
+++ Corporate+Press+Releases/Canada+Semafo+Inc_body.js
@@ -1,1 +1,15 @@
 // Semafo Inc
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.contentEvaluate("http://www.semafo.com/news-and-media/news-releases/news-releases-details/2015/Delineation-Drill-Results-at-SEMAFOs-Natougou-Match-Expectations/default.aspx",
+    function(){
+        $(".ModuleDetailsContainer .ModuleContainerInner .ModuleBody img").remove();
+        return {
+            title: $(".ModuleDetailsContainer .ModuleTitle")[0].innerText.trim(),
+            published_s: $(".ModuleDetailsContainer .ModuleContainerInner .ModuleDateContainer .ModuleDate")[0].innerText.trim(),
+            body: $(".ModuleDetailsContainer .ModuleContainerInner .ModuleBody")[0].innerText.trim(),
+            links: $(".ModuleDetailsContainer .ModuleContainerInner .ModuleLinks a.ModuleFileLink").attr("href"),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Canada+Semafo+Inc_list.js
+++ Corporate+Press+Releases/Canada+Semafo+Inc_list.js
@@ -1,1 +1,10 @@
-http://www.semafo.com/English/news-and-media/news-releases/default.aspx
+//http://www.semafo.com/English/news-and-media/news-releases/default.aspx
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.semafo.com/English/news-and-media/news-releases/default.aspx"], function () {
+    var urls = [];
+    $(".news .ModuleItemRow a.newsTitle").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
