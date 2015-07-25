bf620fcbed1aed52a6803beff71eaa199866031d
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-25 20:37:11 +0530

    Scripts for USA+Gulfport+Energy+Corporation

--- Corporate+Press+Releases/USA+Gulfport+Energy+Corporation_body.js
+++ Corporate+Press+Releases/USA+Gulfport+Energy+Corporation_body.js
@@ -1,1 +1,18 @@
 // Gulfport Energy Corporation
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.gulfportenergy.com/news/press-releases/detail/1275/gulfport-energy-corporation-announces-pricing-of-its-common",
+    function () {
+        var t = $(".main-content h1")[0].innerText.trim();
+        var ps = $(".main-content .date")[0].innerText.trim();
+        var l = $(".main-content .related-documents-line a").attr("href");
+        $(".main-content h1, .main-content .related-documents-line, .main-content pre").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $(".main-content article")[0].innerText.trim(),
+            link: l,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Gulfport+Energy+Corporation_list.js
+++ Corporate+Press+Releases/USA+Gulfport+Energy+Corporation_list.js
@@ -1,1 +1,10 @@
-http://www.gulfportenergy.com/news/press-releases
+//http://www.gulfportenergy.com/news/press-releases
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.gulfportenergy.com/news/press-releases"], function () {
+    var urls = [];
+    $(".main-content .media a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
