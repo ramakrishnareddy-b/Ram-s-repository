272d8a929d6fccef1d17677405fd177e122262b4
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-25 23:12:17 +0530

    Scripts for Netherlands+SBM+Offshore

--- Corporate+Press+Releases/Netherlands+SBM+Offshore_body.js
+++ Corporate+Press+Releases/Netherlands+SBM+Offshore_body.js
@@ -1,1 +1,13 @@
 // Netherlands
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.sbmoffshore.com/?news=marica-moors-brasa-following-fpsos-arrival-brazil",
+    function () {
+        return {
+            title: jQuery(".single-press-release-wrap h1.entry-title")[0].innerText.trim(),
+            published_s: jQuery(".single-press-release-wrap time")[0].innerText.trim(),
+            body: jQuery(".single-press-release-wrap .entry-content")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Netherlands+SBM+Offshore_list.js
+++ Corporate+Press+Releases/Netherlands+SBM+Offshore_list.js
@@ -1,2 +1,12 @@
-http://www.sbmoffshore.com/press-room/news/
-http://www.sbmoffshore.com/investor-relations-centre/press-releases/
+//http://www.sbmoffshore.com/press-room/news/
+//http://www.sbmoffshore.com/investor-relations-centre/press-releases/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.sbmoffshore.com/press-room/news/", "http://www.sbmoffshore.com/investor-relations-centre/press-releases/"],
+    function () {
+        var urls = [];
+        jQuery('.single-release .release-title a').each(function (idx, el) {
+            urls.push(el.href);
+        });
+        return urls;
+    });
