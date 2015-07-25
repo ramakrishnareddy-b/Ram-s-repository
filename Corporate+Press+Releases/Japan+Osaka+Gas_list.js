08bf7779c913393512bbc5534ecf95afe2c60263
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-25 19:49:09 +0530

    Scripts for Japan+Osaka+Gas

--- Corporate+Press+Releases/Japan+Osaka+Gas_list.js
+++ Corporate+Press+Releases/Japan+Osaka+Gas_list.js
@@ -1,1 +1,10 @@
-http://www.osakagas.co.jp/en/ir/news/index.html
+//http://www.osakagas.co.jp/en/ir/news/index.html
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.osakagas.co.jp/en/ir/news/index.html"], function () {
+    var urls = [];
+    $("#contents .update-list02 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
