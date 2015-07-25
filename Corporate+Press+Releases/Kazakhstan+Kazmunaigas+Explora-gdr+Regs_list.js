63458a64056c8ec0b827f0e7badc8a389ab246f2
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-25 20:24:07 +0530

    Scripts for Kazakhstan+Kazmunaigas+Explora-gdr+Regs

--- Corporate+Press+Releases/Kazakhstan+Kazmunaigas+Explora-gdr+Regs_list.js
+++ Corporate+Press+Releases/Kazakhstan+Kazmunaigas+Explora-gdr+Regs_list.js
@@ -1,1 +1,12 @@
-http://www.kmg.kz/en/press/company_news/
+//http://www.kmg.kz/en/press/company_news/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.kmg.kz/en/press/company_news/"], function () {
+    var urls = [];
+    $(".news-list .news-item a").each(function (idx, el) {
+        if(urls.indexOf(el.href) == -1) {
+            urls.push(el.href);
+        }
+    });
+    return urls;
+});
