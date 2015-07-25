e80464f24412fa2b9af46804a735c3ffe6e76496
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-25 20:02:44 +0530

    Scripts for Japan+Showa+Shell+Sekiyu+

--- Corporate+Press+Releases/Japan+Showa+Shell+Sekiyu+_body.js
+++ Corporate+Press+Releases/Japan+Showa+Shell+Sekiyu+_body.js
@@ -1,1 +1,18 @@
 // Japan
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.showa-shell.co.jp/english/press_release/pr2015/0723.html",
+    function () {
+        var t = $(".pageheader h1")[0].innerText.trim();
+        var ps = $(".pageheader .pr_en .date")[0].innerText.trim().replace(/\//g,'-');
+        var l = $(".pageheader .pr_en a").attr("href");
+        $(".pageheader .pr_en .date, .pageheader .pr_en a").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $(".pageheader .pr_en")[0].innerText.trim(),
+            link: l,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
