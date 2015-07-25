f054b0b38c9bc648d6169fbdd4f81585a7ea5a4e
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-25 20:10:07 +0530

    Scripts for Japan+TonenGeneral+Group

--- Corporate+Press+Releases/Japan+TonenGeneral+Group_list.js
+++ Corporate+Press+Releases/Japan+TonenGeneral+Group_list.js
@@ -1,1 +1,10 @@
-http://www.tonengeneral.co.jp/english/news/
+//http://www.tonengeneral.co.jp/english/news/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.tonengeneral.co.jp/english/news/"], function () {
+    var urls = [];
+    $(".list-timeline-01 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
