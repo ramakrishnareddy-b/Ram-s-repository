352c36e1e4af2dc391ee036aad561933a3cd4498
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-25 23:02:01 +0530

    Scripts for Netherlands+IV+Group

--- Corporate+Press+Releases/Netherlands+IV+Group_body.js
+++ Corporate+Press+Releases/Netherlands+IV+Group_body.js
@@ -1,1 +1,13 @@
 // Netherlands
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.iv-oil-gas.nl/en-US/Nieuws/De-bouw-van-de-nieuwe-spoorbrug-Muiderberg-vordert",
+    function () {
+        jQuery(".news-overview a, .news-overview img").remove();
+        return {
+            title: jQuery(".cnt-content h1")[0].innerText.trim(),
+            body: jQuery(".news-overview")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Netherlands+IV+Group_list.js
+++ Corporate+Press+Releases/Netherlands+IV+Group_list.js
@@ -1,1 +1,10 @@
-http://www.iv-oil-gas.nl/en-US/Nieuws
+//http://www.iv-oil-gas.nl/en-US/Nieuws
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.iv-oil-gas.nl/en-US/Nieuws"], function () {
+    var urls = [];
+    jQuery('.news-item .news-readmore a').each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
