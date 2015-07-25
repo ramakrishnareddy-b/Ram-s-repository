ce8a915a44318a5461e2f0820892bb7f6bbff647
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-25 19:06:31 +0530

    Scripts for Japan+Itochu

--- Corporate+Press+Releases/Japan+Itochu_body.js
+++ Corporate+Press+Releases/Japan+Itochu_body.js
@@ -1,1 +1,15 @@
 // Japan
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.itochuoil.co.jp/e/news/20141025.html",
+    function () {
+        var ps = $(".inr .inr-pc .tar")[0].innerText.trim().replace(/\./g,'-');
+        $(".inr .inr-pc .tar, .inr .inr-pc img").remove();
+        return {
+            title: $(".inr h2.ttl-1")[0].innerText.trim(),
+            published_s: ps,
+            body: $(".inr .inr-pc")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Japan+Itochu_list.js
+++ Corporate+Press+Releases/Japan+Itochu_list.js
@@ -1,1 +1,10 @@
-http://www.itochuoil.co.jp/e/news/
+//http://www.itochuoil.co.jp/e/news/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.itochuoil.co.jp/e/news/"], function () {
+    var urls = [];
+    $(".inr-pc .news a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
