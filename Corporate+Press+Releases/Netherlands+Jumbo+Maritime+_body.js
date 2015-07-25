06d85efc7e3780f835c3f0b1ef331e7b1bf7a6af
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-25 22:29:33 +0530

    Scripts for Netherlands+Jumbo+Maritime

--- Corporate+Press+Releases/Netherlands+Jumbo+Maritime+_body.js
+++ Corporate+Press+Releases/Netherlands+Jumbo+Maritime+_body.js
@@ -1,1 +1,17 @@
 // Netherlands
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.contentEvaluate("https://www.jumbomaritime.nl/en/news/latest-12-news-items/captain-on-the-bridge/",
+    function(){
+        var t = jQuery("article .content-wrapper h1")[0].innerText.trim();
+        var ps = jQuery(".publication-date")[0].innerText.trim();
+        jQuery("article .content-wrapper h1, article .content-wrapper .publication-date, article .content-wrapper .cmp-share").remove();
+        var b = jQuery("article .content-wrapper")[0].innerText.trim();
+        return {
+            title: t,
+            published_s: ps,
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Netherlands+Jumbo+Maritime+_list.js
+++ Corporate+Press+Releases/Netherlands+Jumbo+Maritime+_list.js
@@ -1,1 +1,10 @@
-https://www.jumbomaritime.nl/en/news/news-archive/
+//https://www.jumbomaritime.nl/en/news/news-archive/
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.linksEvaluate(["https://www.jumbomaritime.nl/en/news/news-archive/"],function(){
+    var urls =[];
+    jQuery('.cmp-project-block a').each(function(idx,el){
+        urls.push(el.href);
+    });
+    return urls;
+});
