986bf3856fb7c1495bf047fbe45a76906fe9e135
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-25 19:22:15 +0530

    Scripts for Japan+JAMSTEC

--- Corporate+Press+Releases/Japan+JAMSTEC_list.js
+++ Corporate+Press+Releases/Japan+JAMSTEC_list.js
@@ -1,1 +1,10 @@
-http://www.jamstec.go.jp/e/about/press_release/
+//http://www.jamstec.go.jp/e/about/press_release/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.jamstec.go.jp/e/about/press_release/"], function () {
+    var urls = [];
+    $("#contents_area .press a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
