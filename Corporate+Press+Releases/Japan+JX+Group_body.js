3572022c936cb7433f3d71cf4539cb0ebd3abcbb
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-25 19:35:30 +0530

    Scripts for Japan+JX+Group

--- Corporate+Press+Releases/Japan+JX+Group_body.js
+++ Corporate+Press+Releases/Japan+JX+Group_body.js
@@ -1,1 +1,16 @@
-// Japan 
+// Japan
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.noe.jx-group.co.jp/english/press/2015/20150421_01.html",
+    function () {
+        var t = $("#mainContents .heading06")[0].innerText.trim();
+        var ps = $("#mainContents .tx02.mt20")[0].innerText.trim();
+        $("#mainContents .heading01, #mainContents .heading03, #mainContents .heading06, #mainContents .tx02").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $("#mainContents")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Japan+JX+Group_list.js
+++ Corporate+Press+Releases/Japan+JX+Group_list.js
@@ -1,1 +1,10 @@
-http://www.noe.jx-group.co.jp/english/press/2015/index.html
+//http://www.noe.jx-group.co.jp/english/press/2015/index.html
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.noe.jx-group.co.jp/english/press/2015/index.html"], function () {
+    var urls = [];
+    $("#mainContents .releaseList01 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
