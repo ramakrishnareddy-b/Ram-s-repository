0e147b66412519362bf3d91242adcc322bd7d031
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-26 10:24:10 +0530

    Scripts for Japan+Fuji+Oil+Co

--- Corporate+Press+Releases/Japan+Fuji+Oil+Co_list.js
+++ Corporate+Press+Releases/Japan+Fuji+Oil+Co_list.js
@@ -1,1 +1,10 @@
-http://www.foc.co.jp/en/newstopics/
+//http://www.foc.co.jp/en/newstopics/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.foc.co.jp/en/newstopics/"], function () {
+    var urls = [];
+    jQuery('.bodyText p a').each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
