235adfe107069deee4059bef8d045d1acb13d7a8
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-26 11:48:01 +0530

    Scripts for Kazakhstan+KazMunayGaz+

--- Corporate+Press+Releases/Kazakhstan+KazMunayGaz+_list.js
+++ Corporate+Press+Releases/Kazakhstan+KazMunayGaz+_list.js
@@ -1,1 +1,10 @@
-http://www.azskmg.kz/cat/press
+//http://www.azskmg.kz/cat/press
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.azskmg.kz/cat/press"], function () {
+    var urls = [];
+    $(".newsl .news-preview a.readmore").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
