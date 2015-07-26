ec9cf2e0da8f615184e381bdb1233ac03495c1a6
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-26 11:07:12 +0530

    Scripts for Netherlands+Keppel+Verolme

--- Corporate+Press+Releases/Netherlands+Keppel+Verolme_body.js
+++ Corporate+Press+Releases/Netherlands+Keppel+Verolme_body.js
@@ -1,1 +1,14 @@
 // Netherlands
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.energyboardroom.com/interviews/harold-linssen-managing-director-keppel-verolme-netherlands",
+    function () {
+        jQuery("article aside, article img").remove();
+        return {
+            title: jQuery("article h1")[0].innerText.trim(),
+            published_s: jQuery("article .date")[0].innerText.trim().replace(/\./g,'-'),
+            body: jQuery("article .pf-content")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Netherlands+Keppel+Verolme_list.js
+++ Corporate+Press+Releases/Netherlands+Keppel+Verolme_list.js
@@ -1,1 +1,13 @@
-http://www.keppelverolme.nl/en/news
+//http://www.keppelverolme.nl/en/news
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.keppelverolme.nl/en/news"], function () {
+    var urls = [];
+    jQuery(".region-content .view-content .lightbox-processed").remove();
+    jQuery(".region-content .view-content a").each(function (idx, el) {
+        if(urls.indexOf(el.href) == -1){
+            urls.push(el.href);
+        }
+    });
+    return urls;
+});
