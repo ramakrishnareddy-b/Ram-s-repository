e40e47b9605713decadfbaa5ac09c96661093e36
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-26 20:44:07 +0530

    Scripts for ExxonMobil+Canada

--- Corporate+Press+Releases/ExxonMobil+Canada_list.js
+++ Corporate+Press+Releases/ExxonMobil+Canada_list.js
@@ -1,1 +1,10 @@
-http://news.exxonmobil.com/
+//http://news.exxonmobil.com/
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.linksEvaluate(["http://news.exxonmobil.com/"],function(){
+    var urls =[];
+    $('.content-item .field-content a').each(function(idx,el){
+        urls.push(el.href);
+    });
+    return urls;
+});
--- Corporate+Press+Releases/France+Maurel+Et+Prom_list.js
+++ Corporate+Press+Releases/France+Maurel+Et+Prom_list.js
@@ -3,7 +3,7 @@ var main = require(require('system').env['NL_HOME'] + 'main.js');
 
 main.linksEvaluate(["http://www.maureletprom.fr/index.php?option=com_content&view=article&id=129&Itemid=116&lang=en"], function () {
     var urls = [];
-    jQuery('.contentpaneopen tbody tr td span a').each(function (idx, el) {
+    $('.contentpaneopen tbody tr td span a').each(function (idx, el) {
         urls.push(el.href);
     });
     return urls;
