d856c9a9bd09d07094c40f4f2cba58a08b00204e
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-26 11:26:52 +0530

    Scripts for Netherlands

--- Corporate+Press+Releases/Netherlands_list.js
+++ Corporate+Press+Releases/Netherlands_list.js
@@ -1,1 +1,10 @@
-http://www.hertel.com/news.html
+//http://www.hertel.com/news.html
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.hertel.com/news.html"], function () {
+    var urls = [];
+    jQuery(".content a.overlay").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
