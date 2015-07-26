6d57a6b2a988679be3593ff567b3fbc873deba9f
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-26 12:07:41 +0530

    Scripts for Total+Gabon

--- Corporate+Press+Releases/Total+Gabon_list.js
+++ Corporate+Press+Releases/Total+Gabon_list.js
@@ -1,1 +1,10 @@
-http://www.total.ga/fr/liste-actualites
+//http://www.total.ga/fr/liste-actualites
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.total.ga/fr/liste-actualites"], function () {
+    var urls = [];
+    jQuery(".list-results a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
