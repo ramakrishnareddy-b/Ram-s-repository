8484e463d3b576f5dab8b966649982ea74b40773
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-26 20:24:24 +0530

    Scripts for Norway+Det+Norske+Oljeselskap+ASA

--- Corporate+Press+Releases/Norway+Det+Norske+Oljeselskap+ASA_body.js
+++ Corporate+Press+Releases/Norway+Det+Norske+Oljeselskap+ASA_body.js
@@ -1,1 +1,19 @@
 // Det Norske Oljeselskap ASA
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.detnor.no/en/second-quarter-results-2015/",
+    function () {
+        var urls = [];
+        jQuery(".type-post .entry-content .wrap-boks a").each(function (idx, el) {
+            urls.push(el.href);
+        });
+        jQuery(".type-post .entry-content .wrap-boks, .type-post .entry-content a").remove();
+        return {
+            title: jQuery(".type-post .entry-title")[0].innerText.trim(),
+            published_s: jQuery(".type-post .date")[0].innerText.trim().replace(/\./g,''),
+            body: jQuery(".type-post .entry-content")[0].innerText.trim(),
+            links: urls,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Norway+Det+Norske+Oljeselskap+ASA_list.js
+++ Corporate+Press+Releases/Norway+Det+Norske+Oljeselskap+ASA_list.js
@@ -1,1 +1,10 @@
-http://www.detnor.no/en/news/
+//http://www.detnor.no/en/news/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.detnor.no/en/news/"], function () {
+    var urls = [];
+    jQuery("#content a.arkiv-block").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
