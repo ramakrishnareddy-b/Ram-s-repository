af4d77cf18786c646d6a762bb4c04aa345e356f0
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-27 20:39:52 +0530

    Scripts for USA+Sabine+Oil+&+Gas+Corporation

--- Corporate+Press+Releases/USA+Sabine+Oil+&+Gas+Corporation_list.js
+++ Corporate+Press+Releases/USA+Sabine+Oil+&+Gas+Corporation_list.js
@@ -1,1 +1,11 @@
-http://investors.sabineoil.com/phoenix.zhtml?c=92251&p=irol-news&nyo=0
+//http://investors.sabineoil.com/phoenix.zhtml?c=92251&p=irol-news&nyo=0
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://investors.sabineoil.com/phoenix.zhtml?c=92251&p=irol-news&nyo=0"], function () {
+    var urls = [];
+    $(".internalpage .ccbnOutline a[target='_blank']").remove();
+    $(".internalpage .ccbnOutline a.ccbnTblLnk").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
