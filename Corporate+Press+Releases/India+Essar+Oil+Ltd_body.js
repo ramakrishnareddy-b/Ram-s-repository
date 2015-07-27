d63f236786d71672a545b7f6e9a2351ac214f809
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-27 20:20:43 +0530

    Scripts for India+Essar+Oil+Ltd

--- Corporate+Press+Releases/India+Essar+Oil+Ltd_body.js
+++ Corporate+Press+Releases/India+Essar+Oil+Ltd_body.js
@@ -1,1 +1,13 @@
 // Essar Oil Ltd
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.essar.com/article.aspx?cont_id=9c0ndFWM6B8=",
+    function () {
+        return {
+            title: $("#printarea .ArticleTitle")[0].innerText.trim(),
+            published_s: $("#printarea .date")[0].innerText.trim(),
+            body: $("#printarea #content")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/India+Essar+Oil+Ltd_list.js
+++ Corporate+Press+Releases/India+Essar+Oil+Ltd_list.js
@@ -1,1 +1,10 @@
-http://www.essar.com/media.aspx?cont_id=+sDyubvY9c8=
+//http://www.essar.com/media.aspx?cont_id=+sDyubvY9c8=
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.essar.com/media.aspx?cont_id=+sDyubvY9c8="], function () {
+    var urls = [];
+    $(".bodytext .Mediabody_linkbold a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
