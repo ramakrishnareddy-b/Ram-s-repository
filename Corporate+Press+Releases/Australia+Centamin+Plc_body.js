f65bdf733792115f7440f19ee6505375fbe18c8d
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-27 20:59:21 +0530

    Scripts for Australia+Centamin+Plc

--- Corporate+Press+Releases/Australia+Centamin+Plc_body.js
+++ Corporate+Press+Releases/Australia+Centamin+Plc_body.js
@@ -1,1 +1,21 @@
 // Centamin Plc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.centamin.com/centamin/media/news/q2-2015-preliminary-production-results",
+    function () {
+        var t = $(".article h1")[0].innerText.trim();
+        var ps = $(".article p:first")[0].innerText.trim();
+        var urls = [];
+        $(".article .icon_pdf a").each(function (idx, el) {
+            urls.push(el.href);
+        })
+        $(".article h1, .article .group, .article p:first, .article .icon_pdf").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: cs.textUpto($(".article")[0].innerText,"Please click"),
+            links: urls,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
