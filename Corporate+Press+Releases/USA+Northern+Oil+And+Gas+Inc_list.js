bce7345b499d633a1106c6d02c93509d8fd7614d
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-28 07:59:08 +0530

    Scripts for USA+Northern+Oil+And+Gas+Inc

--- Corporate+Press+Releases/USA+Northern+Oil+And+Gas+Inc_list.js
+++ Corporate+Press+Releases/USA+Northern+Oil+And+Gas+Inc_list.js
@@ -1,1 +1,11 @@
-http://ir.northernoil.com/releases.cfm?view=all
+//http://ir.northernoil.com/releases.cfm?view=all
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://ir.northernoil.com/releases.cfm?view=all"], function () {
+    var urls = [];
+    jQuery(".wsh-dataTable th, .wsh-dataTable .ndq-filetypes").remove();
+    jQuery(".wsh-dataTable a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
