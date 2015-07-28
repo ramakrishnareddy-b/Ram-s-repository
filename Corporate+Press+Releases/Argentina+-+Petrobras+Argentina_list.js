26519a50c20ddcaac3a50bc5212ec3804f095bb2
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-28 07:00:58 +0530

    Scripts for Argentina+-+Petrobras+Argentina

--- Corporate+Press+Releases/Argentina+-+Petrobras+Argentina_list.js
+++ Corporate+Press+Releases/Argentina+-+Petrobras+Argentina_list.js
@@ -1,1 +1,10 @@
-http://www.petrobras.com/es/paises/argentina/noticias/
+//http://www.petrobras.com/es/paises/argentina/noticias/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.petrobras.com/es/paises/argentina/noticias/"], function () {
+    var urls = [];
+    $("#news-list a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
