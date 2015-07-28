f008dc67be1511cec9d692ed68a1dd1061023648
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-28 20:27:04 +0530

    Scripts for India+Hindustan+Petroleum+Corporation+Ltd

--- Corporate+Press+Releases/India+Hindustan+Petroleum+Corporation+Ltd_body.js
+++ Corporate+Press+Releases/India+Hindustan+Petroleum+Corporation+Ltd_body.js
@@ -1,1 +1,16 @@
 // Hindustan Petroleum Corporation Ltd
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.hindustanpetroleum.com/pressreleasedetails?EnDocID=213",
+    function () {
+        var t = $("#block-system-main h2")[0].innerText.trim();
+        var ps = cs.scanForDate($("#block-system-main p:first")[0].innerText.trim().replace("st","").replace("nd","").replace("th",""), "dd MMMM yyyy");
+        $("#block-system-main h2, #block-system-main p:first, #block-system-main p:last, #block-system-main img").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $("#block-system-main")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/India+Hindustan+Petroleum+Corporation+Ltd_list.js
+++ Corporate+Press+Releases/India+Hindustan+Petroleum+Corporation+Ltd_list.js
@@ -1,1 +1,10 @@
-http://www.hindustanpetroleum.com/pressrelease
+//http://www.hindustanpetroleum.com/pressrelease
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.hindustanpetroleum.com/pressrelease"], function () {
+    var urls = [];
+    $(".field-item ul a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
