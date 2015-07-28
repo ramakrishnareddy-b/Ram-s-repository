6193f6c0161d141dff589475f71d3df021e95cf5
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-28 20:57:25 +0530

    Scripts for Ireland+Kenmare+Resources+Plc

--- Corporate+Press+Releases/Ireland+Kenmare+Resources+Plc_body.js
+++ Corporate+Press+Releases/Ireland+Kenmare+Resources+Plc_body.js
@@ -1,1 +1,16 @@
 // Kenmare Resources Plc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.kenmareresources.com/media/press-releases/2015/news-item-050515.aspx",
+    function () {
+        var t = jQuery(".content-container h1")[0].innerText.trim();
+        var ps = jQuery(".content-container p strong")[0].innerText.trim();
+        jQuery(".content-container h1, .content-container p strong, .content-container .adobeTextContainer, .content-container a").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: jQuery(".content-container")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Ireland+Kenmare+Resources+Plc_list.js
+++ Corporate+Press+Releases/Ireland+Kenmare+Resources+Plc_list.js
@@ -1,1 +1,11 @@
-http://www.kenmareresources.com/media/press-releases/2015.aspx
+//http://www.kenmareresources.com/media/press-releases/2015.aspx
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.kenmareresources.com/media/press-releases/2015.aspx"], function () {
+    var urls = [];
+    jQuery(".content-container .tabmenucontainer").remove();
+    jQuery(".content-container a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
