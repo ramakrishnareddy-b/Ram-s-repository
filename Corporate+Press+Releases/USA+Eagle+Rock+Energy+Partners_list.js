b9267051c10a19f4f73aad1f0dffdf1ced96b6d5
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-28 07:44:29 +0530

    Scripts for USA+Eagle+Rock+Energy+Partners

--- Corporate+Press+Releases/USA+Eagle+Rock+Energy+Partners_list.js
+++ Corporate+Press+Releases/USA+Eagle+Rock+Energy+Partners_list.js
@@ -1,1 +1,10 @@
-http://ir.eaglerockenergy.com/releases.cfm?view=all
+//http://ir.eaglerockenergy.com/releases.cfm?view=all
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://ir.eaglerockenergy.com/releases.cfm?view=all"], function () {
+    var urls = [];
+    jQuery(".wsh-dataTable [data-before='Title'] a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
