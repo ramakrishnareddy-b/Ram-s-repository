c67276b492c165c8820f25555ae04ca28cf64649
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-28 08:10:34 +0530

    Scripts for USA+Targa+Resources+Corp

--- Corporate+Press+Releases/USA+Targa+Resources+Corp_body.js
+++ Corporate+Press+Releases/USA+Targa+Resources+Corp_body.js
@@ -1,1 +1,14 @@
 // Targa Resources Corp
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://ir.targaresources.com/trc/releasedetail.cfm?ReleaseID=923746",
+    function () {
+        var b = $(".pubcontentarea #ndq-content")[0].innerText.trim();
+        return {
+            title: $(".pubcontentarea h2")[0].innerText.trim(),
+            published_s: cs.scanForDate(b, "MMMM dd, yyyy"),
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
