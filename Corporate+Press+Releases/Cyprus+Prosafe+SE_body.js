3ab099aa92150649fcac7da4e8e5146376224a5e
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-28 20:03:31 +0530

    Scripts for Cyprus+Prosafe+SE

--- Corporate+Press+Releases/Cyprus+Prosafe+SE_body.js
+++ Corporate+Press+Releases/Cyprus+Prosafe+SE_body.js
@@ -1,1 +1,12 @@
 // Prosafe SE
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.prosafe.com/news-room/prosafe-appoints-new-chief-financial-officer-article2637-11.html",
+    function () {
+        return {
+            title: $("#top .main_article_top_no_left")[0].innerText.trim(),
+            body: $("#top .main_article_content_no_left")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Cyprus+Prosafe+SE_list.js
+++ Corporate+Press+Releases/Cyprus+Prosafe+SE_list.js
@@ -1,1 +1,10 @@
-http://www.prosafe.com/news-room/
+//http://www.prosafe.com/news-room/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.prosafe.com/news-room/"], function () {
+    var urls = [];
+    $(".full-articlelist h4 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
