86cbb781f08a7e6925884a08561d43fd791c36e9
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-28 19:21:31 +0530

    Scripts for USA+Bristow+Group+Inc

--- Corporate+Press+Releases/USA+Bristow+Group+Inc_body.js
+++ Corporate+Press+Releases/USA+Bristow+Group+Inc_body.js
@@ -1,1 +1,16 @@
 // Bristow Group Inc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://bristowgroup.com/bristow-news/latest-news/2015/bristow-group-inc-announces-redemption-its-300-con/",
+    function () {
+        var t = $(".single-article hgroup h1")[0].innerText.trim();
+        var ps = $(".single-article hgroup h2")[0].innerText.trim();
+        $(".single-article hgroup").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $(".single-article")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Bristow+Group+Inc_list.js
+++ Corporate+Press+Releases/USA+Bristow+Group+Inc_list.js
@@ -1,1 +1,10 @@
-http://bristowgroup.com/bristow-news/latest-news/press-releases/
+//http://bristowgroup.com/bristow-news/latest-news/press-releases/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://bristowgroup.com/bristow-news/latest-news/press-releases/"], function () {
+    var urls = [];
+    $("div.articles h1 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
