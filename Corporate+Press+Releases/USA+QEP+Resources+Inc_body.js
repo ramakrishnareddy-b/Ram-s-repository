93035c1f71f0b935133c4a96df2e8f8d739ad9f4
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-29 08:11:14 +0530

    Scripts for USA+QEP+Resources+Inc

--- Corporate+Press+Releases/USA+QEP+Resources+Inc_body.js
+++ Corporate+Press+Releases/USA+QEP+Resources+Inc_body.js
@@ -1,1 +1,13 @@
 // QEP Resources Inc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://ir.qepres.com/investors/news-releases/news-release-details/2015/QEP-Resources-Inc-Declares-Quarterly-Cash-Dividend-7272015/default.aspx",
+    function () {
+        return {
+            title: $(".ModuleDetailsContainer .ModuleTitleText")[0].innerText.trim(),
+            published_s: $(".ModuleDetailsContainer .ModuleDate")[0].innerText.trim(),
+            body: $(".ModuleDetailsContainer .ModuleBody")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+QEP+Resources+Inc_list.js
+++ Corporate+Press+Releases/USA+QEP+Resources+Inc_list.js
@@ -1,1 +1,10 @@
-http://ir.qepres.com/investors/news-releases/
+//http://ir.qepres.com/investors/news-releases/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://ir.qepres.com/investors/news-releases/"], function () {
+    var urls = [];
+    $(".ModuleContainerInner .ModuleItemRow a.ModuleHeadlineLink").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
