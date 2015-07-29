c1acdff8d82d759a531617c9979852b48f7fc0f4
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-29 07:56:22 +0530

    Scripts for USA+Seacor+Holdings+Inc

--- Corporate+Press+Releases/USA+Seacor+Holdings+Inc_body.js
+++ Corporate+Press+Releases/USA+Seacor+Holdings+Inc_body.js
@@ -1,1 +1,14 @@
 // Seacor Holdings Inc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://ir.stockpr.com/seacorholdings/company-news/detail/1206/seacor-holdings-announces-results-for-its-second-quarter-ended-june-30-2015",
+    function () {
+        return {
+            title: $(".spr-ir-module-container .spr-ir-news-article-title")[0].innerText.trim(),
+            published_s: cs.scanForDate($(".spr-ir-module-container .spr-ir-news-article-date")[0].innerText.trim(), "MMMM dd, yyyy"),
+            body: $(".spr-ir-module-container .spr-ir-news-article-content")[0].innerText.trim(),
+            links: $(".spr-ir-module-container .spr-ir-news-article-pdf-link a").attr("href"),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Seacor+Holdings+Inc_list.js
+++ Corporate+Press+Releases/USA+Seacor+Holdings+Inc_list.js
@@ -1,1 +1,10 @@
-http://ir.stockpr.com/seacorholdings/company-news
+//http://ir.stockpr.com/seacorholdings/company-news
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://ir.stockpr.com/seacorholdings/company-news"], function () {
+    var urls = [];
+    $(".spr-ir-news .rss-item a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
