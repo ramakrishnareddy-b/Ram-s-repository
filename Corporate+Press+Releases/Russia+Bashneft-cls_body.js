0f1663129fb198e9e7378d61ac6f52f4dd25381e
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-29 23:21:44 +0530

    Scripts for Russia+Bashneft-cls

--- Corporate+Press+Releases/Russia+Bashneft-cls_body.js
+++ Corporate+Press+Releases/Russia+Bashneft-cls_body.js
@@ -1,1 +1,14 @@
 // Bashneft-cls
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.bashneft.com/press/releases/8185/",
+    function () {
+        $(".content.wide .news-more-text .return").remove();
+        return {
+            title: $(".content.wide h1")[0].innerText.trim(),
+            published_s: $(".content.wide .news-more-date")[0].innerText.trim(),
+            body: $(".content.wide .news-more-text")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Russia+Bashneft-cls_list.js
+++ Corporate+Press+Releases/Russia+Bashneft-cls_list.js
@@ -1,1 +1,10 @@
-http://www.bashneft.com/press/
+//http://www.bashneft.com/press/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.bashneft.com/press/"], function () {
+    var urls = [];
+    $("#press .item a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
