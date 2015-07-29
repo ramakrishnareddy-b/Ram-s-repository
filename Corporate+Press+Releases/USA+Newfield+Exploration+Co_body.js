ad927317d18f1764bfffdb244aed02e79feef8dc
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-29 22:35:52 +0530

    Scripts for USA+Newfield+Exploration+Co

--- Corporate+Press+Releases/USA+Newfield+Exploration+Co_body.js
+++ Corporate+Press+Releases/USA+Newfield+Exploration+Co_body.js
@@ -1,1 +1,14 @@
 // Newfield Exploration Co
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://phx.corporate-ir.net/phoenix.zhtml?c=63798&p=irol-newsArchiveArticle&ID=2071885",
+    function () {
+        var b = $("#rigthColWideWrapper .xn-content")[0].innerText.trim();
+        return {
+            title: $("#rigthColWideWrapper .ccbnBgTtl")[0].innerText.trim(),
+            published_s: cs.scanForDate(b, "MMMM dd, yyyy"),
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Newfield+Exploration+Co_list.js
+++ Corporate+Press+Releases/USA+Newfield+Exploration+Co_list.js
@@ -1,1 +1,11 @@
-http://phx.corporate-ir.net/phoenix.zhtml?c=63798&p=irol-newsarchive&nyo=0
+//http://phx.corporate-ir.net/phoenix.zhtml?c=63798&p=irol-newsarchive&nyo=0
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://phx.corporate-ir.net/phoenix.zhtml?c=63798&p=irol-newsarchive&nyo=0"], function () {
+    var urls = [];
+    $(".ccbnOutline a[target='_blank']").remove();
+    $(".ccbnOutline a.ccbnTblLnk").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
