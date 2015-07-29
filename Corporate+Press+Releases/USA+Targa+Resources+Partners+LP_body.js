01d393652e60a1a0dcc1b79c16fcc52e9122e550
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-29 08:25:57 +0530

    Scripts for USA+Targa+Resources+Partners+LP

--- Corporate+Press+Releases/USA+Targa+Resources+Partners+LP_body.js
+++ Corporate+Press+Releases/USA+Targa+Resources+Partners+LP_body.js
@@ -1,1 +1,16 @@
 // Targa Resources Partners LP
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://ir.targaresources.com/trc/releasedetail.cfm?ReleaseID=923746",
+    function () {
+        var t = $(".pubcontentarea .pubsubcontentparenttitle h2")[0].innerText.trim();
+        var b = $(".pubcontentarea .StandardCBStyle")[0].innerText.trim();
+        var ps = cs.scanForDate(b, "MMMM dd, yyyy");
+        return {
+            title: t,
+            published_s: ps,
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Targa+Resources+Partners+LP_list.js
+++ Corporate+Press+Releases/USA+Targa+Resources+Partners+LP_list.js
@@ -1,1 +1,11 @@
-http://ir.targaresources.com/trc/news.cfm
+//http://ir.targaresources.com/trc/news.cfm
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://ir.targaresources.com/trc/news.cfm"], function () {
+    var urls = [];
+    $(".dataTable .wsh_file_Type, .dataTable tbody .dataTableBottom").remove();
+    $(".dataTable a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
