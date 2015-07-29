273db780eb32dd3535a69e4de949ec6aeaa0ae36
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-29 22:53:28 +0530

    Scripts for USA+Genesis+Energy+L.P.

--- Corporate+Press+Releases/USA+Genesis+Energy+L.P._body.js
+++ Corporate+Press+Releases/USA+Genesis+Energy+L.P._body.js
@@ -1,1 +1,16 @@
 // Genesis Energy L.P.
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.genesisenergy.com/index.cfm?md=newsroom&tmp=detail&aid=227",
+    function () {
+        var t = $("#mainCol h3")[0].innerText.trim();
+        var ps = cs.scanForDate($("#mainCol .indexDate")[0].innerText.trim(), "MMMM dd, yyyy");
+        $("#mainCol h3, #mainCol .indexDate, #mainCol #phtHdrGeneric").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $("#mainCol")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Genesis+Energy+L.P._list.js
+++ Corporate+Press+Releases/USA+Genesis+Energy+L.P._list.js
@@ -1,1 +1,10 @@
-http://www.genesisenergy.com/index.cfm?md=newsroom&tmp=archives&catid=1
+//http://www.genesisenergy.com/index.cfm?md=newsroom&tmp=archives&catid=1
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.genesisenergy.com/index.cfm?md=newsroom&tmp=archives&catid=1"], function () {
+    var urls = [];
+    $(".clearBoth a.archiveHead").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
