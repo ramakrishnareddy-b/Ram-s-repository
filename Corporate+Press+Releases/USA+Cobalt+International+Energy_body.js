256f7bd73147c7a8337aeb0bf943ab3c5c920f59
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-29 23:10:13 +0530

    Scripts for USA+Cobalt+International+Energy

--- Corporate+Press+Releases/USA+Cobalt+International+Energy_body.js
+++ Corporate+Press+Releases/USA+Cobalt+International+Energy_body.js
@@ -1,1 +1,14 @@
 // Cobalt International Energy
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.cobaltintl.com/newsroom/cobalt-international-energy-inc.-announces-timing-of-second-quarter-2015-fi",
+    function () {
+        var b = $("#content .press_releases")[0].innerText.trim();
+        return {
+            title: $("#content h1")[0].innerText.trim(),
+            published_s: cs.scanForDate(b.replace(/\./g,''), "MMM dd, yyyy"),
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Cobalt+International+Energy_list.js
+++ Corporate+Press+Releases/USA+Cobalt+International+Energy_list.js
@@ -1,1 +1,10 @@
-http://www.cobaltintl.com/newsroom#press_releases
+//http://www.cobaltintl.com/newsroom#press_releases
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.cobaltintl.com/newsroom#press_releases"], function () {
+    var urls = [];
+    $(".entry-listing-collection[data-target-prefix='press_releases_'] .entry-listing-sub-group.is-active .entry-listing-title a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
