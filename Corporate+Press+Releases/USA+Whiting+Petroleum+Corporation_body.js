1dcff3bff9eaf848eb933ef94f6e1f1bdc26b381
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-30 20:07:37 +0530

    Scripts for USA+Whiting+Petroleum+Corporation

--- Corporate+Press+Releases/USA+Whiting+Petroleum+Corporation_body.js
+++ Corporate+Press+Releases/USA+Whiting+Petroleum+Corporation_body.js
@@ -1,1 +1,16 @@
 // Whiting Petroleum Corporation
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.contentEvaluate("http://phx.corporate-ir.net/phoenix.zhtml?c=147759&p=irol-newsArticle&ID=2072331",
+    function(){
+        var t = jQuery("#investor .ccbnTtl")[0].innerText.trim();
+        var b = jQuery("#investor .ccbnTxt")[0].innerText.trim();
+        var ps =  cs.scanForDate(b.replace(/\./g,''), "MMM dd, yyyy");
+        return {
+            title: t,
+            published_s: ps,
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Whiting+Petroleum+Corporation_list.js
+++ Corporate+Press+Releases/USA+Whiting+Petroleum+Corporation_list.js
@@ -1,1 +1,11 @@
-http://www.whiting.com/investor-relations/press-releases/
+//http://www.whiting.com/investor-relations/press-releases/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.whiting.com/investor-relations/press-releases/"], function () {
+    var urls = [];
+    jQuery(".ccbnOutline a[target='_blank']").remove();
+    jQuery(".ccbnOutline a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
