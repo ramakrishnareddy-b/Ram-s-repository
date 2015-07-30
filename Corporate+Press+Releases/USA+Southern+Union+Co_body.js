509f398e5043d2c6727b7638a0e7bb091d1974fb
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-30 20:19:46 +0530

    Scripts for USA+Southern+Union+Co

--- Corporate+Press+Releases/USA+Southern+Union+Co_body.js
+++ Corporate+Press+Releases/USA+Southern+Union+Co_body.js
@@ -1,1 +1,14 @@
 // Southern Union Co
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://ir.energytransfer.com/phoenix.zhtml?c=106094&p=irol-newsArticle&ID=2071140",
+    function () {
+        var b = $("#ccbnContent .ccbnTxt")[0].innerText.trim();
+        return {
+            title: $("#ccbnContent .ccbnTtl")[0].innerText.trim(),
+            published_s: cs.scanForDate(b.replace(/\./g, ''), "MMM dd, yyyy"),
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Southern+Union+Co_list.js
+++ Corporate+Press+Releases/USA+Southern+Union+Co_list.js
@@ -1,1 +1,11 @@
-http://ir.energytransfer.com/phoenix.zhtml?c=106094&p=irol-news&nyo=0
+//http://ir.energytransfer.com/phoenix.zhtml?c=106094&p=irol-news&nyo=0
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://ir.energytransfer.com/phoenix.zhtml?c=106094&p=irol-news&nyo=0"], function () {
+    var urls = [];
+    $(".ccbnOutline a[target='_blank']").remove();
+    $(".ccbnOutline a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
