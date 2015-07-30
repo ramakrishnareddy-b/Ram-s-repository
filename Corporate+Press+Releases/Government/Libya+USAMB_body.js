f6dbed5f56c7720fa843e5097305d170842d7281
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-30 21:27:59 +0530

    Scripts for Libya+USAMB

--- Corporate+Press+Releases/Government/Libya+USAMB_body.js
+++ Corporate+Press+Releases/Government/Libya+USAMB_body.js
@@ -1,1 +1,13 @@
 // Libya
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.state.gov/r/pa/prs/ps/2015/06/243331.htm",
+    function () {
+        return {
+            title: $("#middlecolumn .tier3-headline")[0].innerText.trim(),
+            published_s: $("#middlecolumn #date_long")[0].innerText.trim(),
+            body: $("#middlecolumn #centerblock")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Government/Libya+USAMB_list.js
+++ Corporate+Press+Releases/Government/Libya+USAMB_list.js
@@ -1,1 +1,10 @@
-http://libya.usembassy.gov/news-events/news-from-the-embassy2.html
+//http://libya.usembassy.gov/news-events/news-from-the-embassy2.html
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://libya.usembassy.gov/news-events/news-from-the-embassy2.html"], function () {
+    var urls = [];
+    $(".main_listing h5 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
