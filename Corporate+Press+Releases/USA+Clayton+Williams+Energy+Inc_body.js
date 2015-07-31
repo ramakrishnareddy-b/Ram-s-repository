f9c68582606fa9526f4d995aaa65d286d59e004d
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-31 21:08:34 +0530

    Scripts for USA+Clayton+Williams+Energy+Inc

--- Corporate+Press+Releases/USA+Clayton+Williams+Energy+Inc_body.js
+++ Corporate+Press+Releases/USA+Clayton+Williams+Energy+Inc_body.js
@@ -1,1 +1,12 @@
 // Clayton Williams Energy Inc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.b2i.us/profiles/investor/ResLibraryView.asp?ResLibraryID=78071&GoTopage=1&Category=145&BzID=844&G=539",
+    function () {
+        return {
+            title: jQuery(".b2iNewsStoryTable .b2iNewsStoryHeadline")[0].innerText.trim(),
+            body: jQuery(".b2iNewsStoryTable .b2iNewsItemBodyDiv")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Clayton+Williams+Energy+Inc_list.js
+++ Corporate+Press+Releases/USA+Clayton+Williams+Energy+Inc_list.js
@@ -1,1 +1,10 @@
-http://www.b2i.us/profiles/investor/ResLibrary2.asp?BzID=844&g=539&to=rl&Nav=0&LangID=1&s=0&Category=145
+//http://www.b2i.us/profiles/investor/ResLibrary2.asp?BzID=844&g=539&to=rl&Nav=0&LangID=1&s=0&Category=145
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.b2i.us/profiles/investor/ResLibrary2.asp?BzID=844&g=539&to=rl&Nav=0&LangID=1&s=0&Category=145"], function () {
+    var urls = [];
+    jQuery(".b2iNewsListContent .b2iNewsItemHeadline a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
