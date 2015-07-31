b0572adaa52507263e4656f6e35eb17105fb68f2
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-31 20:35:59 +0530

    Scripts for Canada+Enerflex+Ltd

--- Corporate+Press+Releases/Canada+Enerflex+Ltd_body.js
+++ Corporate+Press+Releases/Canada+Enerflex+Ltd_body.js
@@ -1,1 +1,13 @@
 // Enerflex Ltd
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.enerflex.com/Investors/Press-Releases/news.php?year=2015&xml=http%253A%252F%252Fenerflex.mwnewsroom.com%252Farticle%252Fxml%253Fid%253D1972452",
+    function () {
+        return {
+            title: $("#newsContent .news-headline")[0].innerText.trim(),
+            published_s: $("#newsContent .news-date")[0].innerText.trim(),
+            body: $("#newsContent .mw_release")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Canada+Enerflex+Ltd_list.js
+++ Corporate+Press+Releases/Canada+Enerflex+Ltd_list.js
@@ -1,1 +1,10 @@
-http://www.enerflex.com/Investors/index.php
+//http://www.enerflex.com/Investors/index.php
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.enerflex.com/Investors/index.php"], function () {
+    var urls = [];
+    $(".lpPressReleases .newsTitle a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
