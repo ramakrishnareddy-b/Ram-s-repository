e47d33bfed0f76288079ef84decbd5afdeb441c1
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-31 19:54:37 +0530

    Scripts for USA+Pioneer+Southwest+Energy+Partners+L.P.

--- Corporate+Press+Releases/USA+Pioneer+Southwest+Energy+Partners+L.P._body.js
+++ Corporate+Press+Releases/USA+Pioneer+Southwest+Energy+Partners+L.P._body.js
@@ -1,1 +1,14 @@
 // Pioneer Southwest Energy Partners L.P.
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://investors.pxd.com/phoenix.zhtml?c=90959&p=irol-newsArticle&ID=2069914",
+    function () {
+        var b = $(".ccbnContent .ccbnTxt")[0].innerText.trim();
+        return {
+            title: $(".ccbnContent .ccbnTtl")[0].innerText.trim(),
+            published_s: cs.scanForDate(b.replace(/\./g, ''), "MMM dd, yyyy"),
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Pioneer+Southwest+Energy+Partners+L.P._list.js
+++ Corporate+Press+Releases/USA+Pioneer+Southwest+Energy+Partners+L.P._list.js
@@ -1,1 +1,11 @@
-http://investors.pxd.com/phoenix.zhtml?c=90959&p=irol-news&nyo=0
+//http://investors.pxd.com/phoenix.zhtml?c=90959&p=irol-news&nyo=0
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://investors.pxd.com/phoenix.zhtml?c=90959&p=irol-news&nyo=0"], function () {
+    var urls = [];
+    $(".ccbnOutline a[target='_blank']").remove();
+    $(".ccbnOutline a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
