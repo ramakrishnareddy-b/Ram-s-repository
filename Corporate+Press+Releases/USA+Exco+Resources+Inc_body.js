b8b2a0441908dd46a6ba0684477532180eea475e
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-31 21:54:24 +0530

    Scripts for USA+Exco+Resources+Inc

--- Corporate+Press+Releases/USA+Exco+Resources+Inc_body.js
+++ Corporate+Press+Releases/USA+Exco+Resources+Inc_body.js
@@ -1,1 +1,16 @@
 // Exco Resources Inc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.excoresources.com/single-news-release.htm?regid=2067739",
+    function () {
+        var t = $("#mainRightContent h2")[0].innerText.trim();
+        var ps = $("#mainRightContent h4")[0].innerText.trim();
+        $("#mainRightContent h2, #mainRightContent h4").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $("#mainRightContent")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Exco+Resources+Inc_list.js
+++ Corporate+Press+Releases/USA+Exco+Resources+Inc_list.js
@@ -1,1 +1,14 @@
-http://www.excoresources.com/new-releases.htm
+//http://www.excoresources.com/new-releases.htm
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.excoresources.com/new-releases.htm"], function () {
+    var urls = [];
+    $("#mainRightContent tr:nth-child(1)").remove();
+    $("#mainRightContent tr:nth-child(1)").remove();
+    $("#mainRightContent a").each(function (idx, el) {
+        if(el.href.indexOf(document.location.origin) >= 0) {
+            urls.push(el.href);
+        }
+    });
+    return urls;
+});
