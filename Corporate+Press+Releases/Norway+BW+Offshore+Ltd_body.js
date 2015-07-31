07e6e7b191de7ae56aa6b7246694516a4a46ab9d
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-31 21:23:37 +0530

    Scripts for Norway+BW+Offshore+Ltd

--- Corporate+Press+Releases/Norway+BW+Offshore+Ltd_body.js
+++ Corporate+Press+Releases/Norway+BW+Offshore+Ltd_body.js
@@ -1,1 +1,13 @@
 // BW Offshore Ltd
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.bwoffshore.com/media/press-releases/imported-press-releases1/2015/bw-offshore-contract-extension-for-abo--/",
+    function () {
+        return {
+            title: $(".article-wrapper h2.block-title")[0].innerText.trim(),
+            published_s: $(".article-wrapper .published-date time")[0].innerText.trim().replace(/\./g,'-'),
+            body: $(".article-wrapper .main-text")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Norway+BW+Offshore+Ltd_list.js
+++ Corporate+Press+Releases/Norway+BW+Offshore+Ltd_list.js
@@ -1,1 +1,10 @@
-http://www.bwoffshore.com/media/press-releases/
+//http://www.bwoffshore.com/media/press-releases/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.bwoffshore.com/media/press-releases/"], function () {
+    var urls = [];
+    $(".filter tr[data-year='" + $(".nav-filter .active")[0].innerText + "'] a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
