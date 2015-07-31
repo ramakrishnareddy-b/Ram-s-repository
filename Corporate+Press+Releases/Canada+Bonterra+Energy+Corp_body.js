8db0c927742b8ace5dd42b2e372aedac62c6bcc0
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-31 20:54:53 +0530

    Scripts for Canada+Bonterra+Energy+Corp

--- Corporate+Press+Releases/Canada+Bonterra+Energy+Corp_body.js
+++ Corporate+Press+Releases/Canada+Bonterra+Energy+Corp_body.js
@@ -1,1 +1,15 @@
 // Bonterra Energy Corp
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://bonterraenergy.mwnewsroom.com/press-releases/bonterra-energy-corp-announces-closing-of-31-million-private-placement-tsx-bne-201507081016089001",
+    function () {
+        var b = jQuery(".content .mw_release")[0].innerText.trim();
+        jQuery(".content .mw_release").remove();
+        return {
+            title: jQuery(".content h3")[0].innerText.trim(),
+            published_s: jQuery(".content p strong")[0].innerText.trim(),
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Canada+Bonterra+Energy+Corp_list.js
+++ Corporate+Press+Releases/Canada+Bonterra+Energy+Corp_list.js
@@ -1,1 +1,11 @@
-http://bonterraenergy.mwnewsroom.com/
+//http://bonterraenergy.mwnewsroom.com/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://bonterraenergy.mwnewsroom.com/"], function () {
+    var urls = [];
+    jQuery(".text a[target='_new']").remove();
+    jQuery(".text a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
