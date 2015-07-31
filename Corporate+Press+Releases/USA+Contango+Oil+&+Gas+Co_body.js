dc59f2b3d9976af954fc7139616a7d24f77f70f4
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-31 19:42:47 +0530

    Scripts for USA+Contango+Oil+&+Gas+Co

--- Corporate+Press+Releases/USA+Contango+Oil+&+Gas+Co_body.js
+++ Corporate+Press+Releases/USA+Contango+Oil+&+Gas+Co_body.js
@@ -1,1 +1,17 @@
 // Contango Oil & Gas Co
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://ir.contango.com/releasedetail.cfm?ReleaseID=924466",
+    function () {
+        $(".wsh_prinertOff").remove();
+        var t = $(".wsh_printContainer h1")[0].innerText.trim();
+        var ps = $(".wsh_printContainer .wsh_printDate")[0].innerText.trim();
+        $(".wsh_printContainer .wsh_printDate, .wsh_printContainer h1, .wsh_printContainer #iconMenuPrint").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $(".wsh_printContainer")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Contango+Oil+&+Gas+Co_list.js
+++ Corporate+Press+Releases/USA+Contango+Oil+&+Gas+Co_list.js
@@ -1,1 +1,13 @@
-http://ir.contango.com/releases.cfm
+//http://ir.contango.com/releases.cfm
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://ir.contango.com/releases.cfm"], function () {
+    var urls = [];
+    $(".dataTable .noprint, .dataTable .wsh_file_Type, .dataTable .wsh_briefcase, .dataTable .dataTableBottom").remove();
+    $(".dataTable a").each(function (idx, el) {
+        if(urls.indexOf(el.href) == -1) {
+            urls.push(el.href);
+        }
+    });
+    return urls;
+});
