43c520269204e4a67181ed0cb1243678919c3ef9
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-31 21:32:52 +0530

    Scripts for Thailand+PTT+Exploration+and+Production+Plc

--- Corporate+Press+Releases/Thailand+PTT+Exploration+and+Production+Plc+_body.js
+++ Corporate+Press+Releases/Thailand+PTT+Exploration+and+Production+Plc+_body.js
@@ -1,1 +1,14 @@
-// PTT Exploration and Production Plc 
+// PTT Exploration and Production Plc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.pttep.com/en/News-n-Media/Media%20Corner/Press%20Releases/PTTEP%20announces%20payment%20of%20interim%20dividend%20at%201%20Baht%20per%20share.aspx",
+    function () {
+        $(".box_ct3 .txt_3 .boxContentStyle_6").remove();
+        return {
+            title: $(".box_ct3 .txt_1")[0].innerText.trim(),
+            published_s: $(".box_ct3 .txt_2")[0].innerText.trim(),
+            body: $(".box_ct3 .txt_3")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Thailand+PTT+Exploration+and+Production+Plc+_list.js
+++ Corporate+Press+Releases/Thailand+PTT+Exploration+and+Production+Plc+_list.js
@@ -1,1 +1,10 @@
-http://www.pttep.com/en/News-n-Media/Media%20Corner/Press%20Releases.aspx
+//http://www.pttep.com/en/News-n-Media/Media%20Corner/Press%20Releases.aspx
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.pttep.com/en/News-n-Media/Media%20Corner/Press%20Releases.aspx"], function () {
+    var urls = [];
+    $(".box_ct2 a.one_ct2").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
