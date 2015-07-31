77947ccc3245b05aa6755f00c77fa49b594ae9b8
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-07-31 20:09:43 +0530

    Scripts for USA+Carrizo+Oil+&+Gas+Inc

--- Corporate+Press+Releases/USA+Carrizo+Oil+&+Gas+Inc_body.js
+++ Corporate+Press+Releases/USA+Carrizo+Oil+&+Gas+Inc_body.js
@@ -1,1 +1,13 @@
 // Carrizo Oil & Gas Inc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://ir.carrizo.com/releasedetail.cfm?ReleaseID=923711",
+    function () {
+        return {
+            title: $("#ndq-article h1")[0].innerText.trim(),
+            published_s: $("#ndq-article .ndq-leftcol")[0].innerText.trim(),
+            body: $("#ndq-article #ndq-releasebody")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Carrizo+Oil+&+Gas+Inc_list.js
+++ Corporate+Press+Releases/USA+Carrizo+Oil+&+Gas+Inc_list.js
@@ -1,1 +1,11 @@
-http://ir.carrizo.com/releases.cfm?view=all
+//http://ir.carrizo.com/releases.cfm?view=all
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://ir.carrizo.com/releases.cfm?view=all"], function () {
+    var urls = [];
+    jQuery(".wsh-dataTable .icons, .wsh-dataTable .releaseSummary, .wsh-dataTable .date").remove();
+    jQuery(".wsh-dataTable a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
