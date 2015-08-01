3f1c1b56b8aef63ae780f699cfc650106315b77f
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-01 11:31:57 +0530

    Scripts for Croatia+Ina+Industrija+Nafte+d.d.

--- Corporate+Press+Releases/Croatia+Ina+Industrija+Nafte+d.d._body.js
+++ Corporate+Press+Releases/Croatia+Ina+Industrija+Nafte+d.d._body.js
@@ -1,1 +1,14 @@
 // Ina Industrija Nafte d.d.
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.ina.hr/default.aspx?id=21798",
+    function () {
+        var b = cs.textUpto($(".pageContent .central .pageBody")[0].innerText.trim(), "Click here for announcement in word");
+        return {
+            title: $(".pageContent .central h1")[0].innerText.trim(),
+            published_s: cs.scanForDate(b, "dd MMMM yyyy"),
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Croatia+Ina+Industrija+Nafte+d.d._list.js
+++ Corporate+Press+Releases/Croatia+Ina+Industrija+Nafte+d.d._list.js
@@ -1,1 +1,11 @@
-http://www.ina.hr/default.aspx?id=350
+//http://www.ina.hr/default.aspx?id=350
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.ina.hr/default.aspx?id=350"], function () {
+    var urls = [];
+    $(".pageBody table a[target='_blank']").remove();
+    $(".pageBody table a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
