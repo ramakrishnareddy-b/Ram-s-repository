650ac0aeec3990c4c67fdecc273e3252e39a17cd
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-01 11:48:32 +0530

    Scripts for Korea+SK+Holdings

--- Corporate+Press+Releases/Korea+SK+Holdings_body.js
+++ Corporate+Press+Releases/Korea+SK+Holdings_body.js
@@ -1,1 +1,16 @@
 // SK Holdings
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.sk.com/Channel/News/view/1294",
+    function () {
+        $(".boardview.prev_b").remove();
+        var ps = $(".boardview thead .date")[0].innerText.trim();
+        $(".boardview thead .company, .boardview thead .date").remove();
+        return {
+            title: $(".boardview thead")[0].innerText.trim(),
+            published_s: ps.replace(/\//g,'-'),
+            body: $(".boardview tbody")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Korea+SK+Holdings_list.js
+++ Corporate+Press+Releases/Korea+SK+Holdings_list.js
@@ -1,1 +1,11 @@
-http://www.sk.com/Channel/News/
+//http://www.sk.com/Channel/News/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.sk.com/Channel/News/"], function () {
+    var urls = [];
+    $(".listwrap .pic").remove();
+    $(".listwrap a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
