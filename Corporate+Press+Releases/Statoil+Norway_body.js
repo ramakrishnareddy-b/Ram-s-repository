60f09b89c9dfd78350c1d5f677ba3bd47f2121b0
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-01 13:42:16 +0530

    Scripts for Statoil+Norway

--- Corporate+Press+Releases/Statoil+Norway_body.js
+++ Corporate+Press+Releases/Statoil+Norway_body.js
@@ -1,1 +1,14 @@
 // Statoil
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.statoil.com/en/NewsAndMedia/News/2015/Pages/2Q2015xxxyyy.aspx",
+    function () {
+        var t = $(".newsArticlePage .span6 .articleHeader h1")[0].innerText.trim();
+        $(".newsArticlePage .span6 .articleHeader, .newsArticlePage .span6 .articleFooter, .newsArticlePage .span6 img").remove();
+        return {
+            title: t,
+            body: $(".newsArticlePage .span6")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Statoil+Norway_list.js
+++ Corporate+Press+Releases/Statoil+Norway_list.js
@@ -1,1 +1,11 @@
-http://www.statoil.com/en/newsandmedia/pages/default.aspx
+//http://www.statoil.com/en/newsandmedia/pages/default.aspx
+// changed the url from  http://www.statoil.com/en/newsandmedia/pages/default.aspx to http://www.statoil.com/en/NewsAndMedia/News/Pages/default.aspx
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.statoil.com/en/NewsAndMedia/News/Pages/default.aspx"], function () {
+    var urls = [];
+    $(".searchResult li a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
