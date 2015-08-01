18a50d1d6be054a96a00d581bd224a3edd4f2e7e
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-01 13:29:37 +0530

    Scripts for China+Fangda+Carbon+New+Material+Co+Ltd

--- Corporate+Press+Releases/China+Fangda+Carbon+New+Material+Co+Ltd_body.js
+++ Corporate+Press+Releases/China+Fangda+Carbon+New+Material+Co+Ltd_body.js
@@ -1,1 +1,14 @@
 //  Fangda Carbon New Material Co Ltd
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.fangdacarbon.com/en/product/a/default.shtml",
+    function () {
+        var t = $("td[valign='top'] table[width='95%'] h1")[0].innerText.trim();
+        $("td[valign='top'] table[width='95%'] h1, td[valign='top'] table[width='95%'] table[width='98%']").remove();
+        return {
+            title: t,
+            body: $("td[valign='top'] table[width='95%']")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/China+Fangda+Carbon+New+Material+Co+Ltd_list.js
+++ Corporate+Press+Releases/China+Fangda+Carbon+New+Material+Co+Ltd_list.js
@@ -1,1 +1,12 @@
-http://www.fangdacarbon.com/en/
+//http://www.fangdacarbon.com/en/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.fangdacarbon.com/en/"], function () {
+    var urls = [];
+    $(".default-right a").each(function (idx, el) {
+        if(urls.indexOf(el.href) == -1) {
+            urls.push(el.href);
+        }
+    });
+    return urls;
+});
