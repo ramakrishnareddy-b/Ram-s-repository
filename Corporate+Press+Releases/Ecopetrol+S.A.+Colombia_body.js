0f080c96b65e3ea88e399fbc03aa3fec1e6e649c
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-01 13:54:52 +0530

    Scripts for Ecopetrol+S.A.+Colombia

--- Corporate+Press+Releases/Ecopetrol+S.A.+Colombia_body.js
+++ Corporate+Press+Releases/Ecopetrol+S.A.+Colombia_body.js
@@ -1,1 +1,13 @@
 // Ecopetrol
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.ecopetrol.com.co/wps/portal/web_es/ecopetrol-web/our-company/Press-Room/Press-Release/2015/2015/ecopetrol-announces-the-results-of-its-international-bond-offering/!ut/p/z1/tVZNc9owEP01PgpJ_qY3AwkJDBBCSIMvjCxk49aWHFuEpL--gjIUkmI3HdUXWdq3b7z7dleGIXyCIScvaUJkKjjJ1H4Rusv2BPf7NzM0nHSuTRRMPb8z7gR42rPh1z0AXXgCBENl9u9w0L9FeNi_99WhOQumg2G3071zDv41gDN_5PUsZQ7w4LrvKAyGjzCEIeWykGu4YFQUTJYiW7LKQMedgfiGVbIkgOVFySpioN1SgVKI_PjOMkYqZiATYcdAVHDJuDxhAYRzseGUVUCumcJXm0xWQMQgVUuq4CU_pA1Egq-UJWZlypPdFxY0XcFFmxHsUdMCthN5wMamDSKTmIC4jGHHZzZ22fuMfAw5vKxIMPIO_jWAsEnRcA-p07RGFfN2gt8DzlXtPZr1gL3sTcIPmhhU6QY8snyV_5IpJVjZ2pSqotdSFtUXAxlou922EiGSjLWoyFtVYqA_ea1FJeHTBzBcqER7FxN95cDZb-VtG8Xttk8AiWwK7BWNgY9dDzAfW23KbEap20A4t3UTuroJLc2EGOkm1B0y1q0y9jQT3ny-bAZNI0Y1Sfrt-TkM1OjdzclX1R_aZm99PNPeWYL-aqLWEz6YugktzYSWrZvQ1U3ofJpw0HQPqSIzy1F3pCZxQeRa3bCxOCmzf7-O_2v16v1zKPL5PPettxR8v7-KR9KJnOLlx0Oc58vxWPWwj6zjwUie7n6Z36ws-QlPo-nu/dz/d5/L2dBISEvZ0FBIS9nQSEh/",
+    function () {
+        return {
+            title: $(".contentEditContainer .itemTitle")[0].innerText.trim(),
+            published_s: cs.scanForDate($(".contentEditContainer .itemDate")[0].innerText.trim(), "MMM dd, yyyy"),
+            body: $(".contentEditContainer .body")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Ecopetrol+S.A.+Colombia_list.js
+++ Corporate+Press+Releases/Ecopetrol+S.A.+Colombia_list.js
@@ -1,1 +1,11 @@
-http://www.ecopetrol.com.co/wps/portal/web_es/ecopetrol-web/our-company/Press-Room/Press-Release/!ut/p/z1/hY-xDoIwEIafxYHR9gpBiVvFSIQFNUbsYsDUQgKUlGrj29sQBxMVb7vL9-X-HzOcYdbm90rkupJtXtv9xGbnICU02gBJYL7ygLqUxOvIp9uY4OMAwI-hgNm7H-0Ce3T3Vk3CZZj6L38EYP_-swEZSRD_y2BL0rbwAoGZ4leuuEI3ZbuXWnf9wgEHjDFISClqji6yQb1w4JtVyl7j7APGXXPIYMqKh6GTJ1Si_uk!/dz/d5/L2dBISEvZ0FBIS9nQSEh/
+//http://www.ecopetrol.com.co/wps/portal/web_es/ecopetrol-web/our-company/Press-Room/Press-Release/!ut/p/z1/hY-xDoIwEIafxYHR9gpBiVvFSIQFNUbsYsDUQgKUlGrj29sQBxMVb7vL9-X-HzOcYdbm90rkupJtXtv9xGbnICU02gBJYL7ygLqUxOvIp9uY4OMAwI-hgNm7H-0Ce3T3Vk3CZZj6L38EYP_-swEZSRD_y2BL0rbwAoGZ4leuuEI3ZbuXWnf9wgEHjDFISClqji6yQb1w4JtVyl7j7APGXXPIYMqKh6GTJ1Si_uk!/dz/d5/L2dBISEvZ0FBIS9nQSEh/
+// URL changed to http://www.ecopetrol.com.co/wps/portal/web_es/ecopetrol-web/our-company/Press-Room/Press-Release/2015/!ut/p/z1/rVRNU8IwEP0tHnos2fSDBm9BtAgKKKKSC1PjUlDa1DaA8usNjKPjVxlmmkuym7dvdl82SwS5JyKNVvM40nOVRgtjj0V90ujTMGwPodtvnjnArwLW7DU55ZcBudsB4J_FgQhzzQaUh-dAu-E1M05nyK863ZPmycD_iC8BfIuHoOWaa047Z6FvMJTcEkGETHWmZ2SMUmWoc7WYYGHBp2VBusRC55GNSZZjEVmw3Qo7Vyr5POMCowItcID6W9JMzh_J2PNg2miwyI4ePGl7j3JqM1oPbGTUbUj0UMr6zyJ-Zyn2iSh2kDIZS4Rwzvv0J-C7kK1bpxywVbqzD2CagacPLouJyHGKOea1ZW56ZKZ1VhxbYMF6va7FSsULrEmV1IrYgr-iZqrQ5P4XmIyNjsG_Qp36ZHjgw5QTjryqCetVE7oVE1KomrDqkmnVr0yDignbh7dNZ98EMJ9k_vTyIrgZZirV-Gr-R3XTLEtGo4S5b_bzdXtzgXcmVwaun602N9MkmfR6X44Lzo-O3gH9VX6A/dz/d5/L2dBISEvZ0FBIS9nQSEh/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.ecopetrol.com.co/wps/portal/web_es/ecopetrol-web/our-company/Press-Room/Press-Release/2015/!ut/p/z1/rVRNU8IwEP0tHnos2fSDBm9BtAgKKKKSC1PjUlDa1DaA8usNjKPjVxlmmkuym7dvdl82SwS5JyKNVvM40nOVRgtjj0V90ujTMGwPodtvnjnArwLW7DU55ZcBudsB4J_FgQhzzQaUh-dAu-E1M05nyK863ZPmycD_iC8BfIuHoOWaa047Z6FvMJTcEkGETHWmZ2SMUmWoc7WYYGHBp2VBusRC55GNSZZjEVmw3Qo7Vyr5POMCowItcID6W9JMzh_J2PNg2miwyI4ePGl7j3JqM1oPbGTUbUj0UMr6zyJ-Zyn2iSh2kDIZS4Rwzvv0J-C7kK1bpxywVbqzD2CagacPLouJyHGKOea1ZW56ZKZ1VhxbYMF6va7FSsULrEmV1IrYgr-iZqrQ5P4XmIyNjsG_Qp36ZHjgw5QTjryqCetVE7oVE1KomrDqkmnVr0yDignbh7dNZ98EMJ9k_vTyIrgZZirV-Gr-R3XTLEtGo4S5b_bzdXtzgXcmVwaun602N9MkmfR6X44Lzo-O3gH9VX6A/dz/d5/L2dBISEvZ0FBIS9nQSEh/"], function () {
+    var urls = [];
+    $(".contentIndex .caption a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
