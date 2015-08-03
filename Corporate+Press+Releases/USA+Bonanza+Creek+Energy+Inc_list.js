c42ac9c192a10e24236a6d4956a9f0b5e67d7c52
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-03 21:26:46 +0530

    Scripts for USA+Bonanza+Creek+Energy+Inc

--- Corporate+Press+Releases/USA+Bonanza+Creek+Energy+Inc_list.js
+++ Corporate+Press+Releases/USA+Bonanza+Creek+Energy+Inc_list.js
@@ -1,1 +1,11 @@
-http://ir.bonanzacrk.com/phoenix.zhtml?c=246947&p=irol-news&nyo=0
+//http://ir.bonanzacrk.com/phoenix.zhtml?c=246947&p=irol-news&nyo=0
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://ir.bonanzacrk.com/phoenix.zhtml?c=246947&p=irol-news&nyo=0"], function () {
+    var urls = [];
+    jQuery(".entry-content .ccbnTblLnk[target='_blank']").remove();
+    jQuery(".entry-content .ccbnTblLnk").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
