fd74080234e7e29faddb7f9e743532a22be1727a
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-03 21:16:54 +0530

    Scripts for USA+Hercules+Offshore+Inc

--- Corporate+Press+Releases/USA+Hercules+Offshore+Inc_body.js
+++ Corporate+Press+Releases/USA+Hercules+Offshore+Inc_body.js
@@ -1,1 +1,14 @@
 // Hercules Offshore Inc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://ir.herculesoffshore.com/phoenix.zhtml?c=192573&p=irol-newsArticle&ID=2070332",
+    function () {
+        var b = jQuery(".ccbnContent .xn-content")[0].innerText.trim();
+        return {
+            title: jQuery(".ccbnContent .ccbnTtl")[0].innerText.trim(),
+            published_s: cs.scanForDate(b, "MMMM dd, yyyy"),
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Hercules+Offshore+Inc_list.js
+++ Corporate+Press+Releases/USA+Hercules+Offshore+Inc_list.js
@@ -1,1 +1,11 @@
-http://ir.herculesoffshore.com/phoenix.zhtml?c=192573&p=irol-news&nyo=0
+//http://ir.herculesoffshore.com/phoenix.zhtml?c=192573&p=irol-news&nyo=0
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://ir.herculesoffshore.com/phoenix.zhtml?c=192573&p=irol-news&nyo=0"], function () {
+    var urls = [];
+    jQuery(".ccbnOutline .ccbnTblLnk[target='_blank']").remove();
+    jQuery(".ccbnOutline .ccbnTblLnk").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
