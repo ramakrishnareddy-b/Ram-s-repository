bee5203248a40eea0bed33612bdd9d2bee77f62c
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-03 21:55:46 +0530

    Scripts for USA+Energy+XXI+

--- Corporate+Press+Releases/USA+Energy+XXI+_body.js
+++ Corporate+Press+Releases/USA+Energy+XXI+_body.js
@@ -1,1 +1,16 @@
-// Energy XXI 
+// Energy XXI
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://ir.energyxxi.com/releasedetail.cfm?ReleaseID=920194&CompanyID=EXXI",
+    function () {
+        var t = $("#wsh_middle h1")[0].innerText.trim();
+        var ps = $("#wsh_middle .wsh_printDate")[0].innerText.trim();
+        $("#wsh_middle h1, #wsh_middle .wsh_printDate, #wsh_middle .wsh_prinertOff, #wsh_middle #iconMenuPrint").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $("#wsh_middle")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Energy+XXI+_list.js
+++ Corporate+Press+Releases/USA+Energy+XXI+_list.js
@@ -1,1 +1,11 @@
-http://ir.energyxxi.com/releases.cfm
+//http://ir.energyxxi.com/releases.cfm
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://ir.energyxxi.com/releases.cfm"], function () {
+    var urls = [];
+    $(".dataTable .noprint, .dataTable .wsh_file_Type, .dataTable .dataTableBottom").remove();
+    $(".dataTable a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
