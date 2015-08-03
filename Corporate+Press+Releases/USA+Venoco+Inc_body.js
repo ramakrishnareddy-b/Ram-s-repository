f0b69f7b1d051ab6f73e719ed5f142ca122b560a
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-03 21:41:46 +0530

    Scripts for USA+Venoco+Inc

--- Corporate+Press+Releases/USA+Venoco+Inc_body.js
+++ Corporate+Press+Releases/USA+Venoco+Inc_body.js
@@ -1,1 +1,14 @@
 // Venoco Inc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://investor.venocoinc.com/phoenix.zhtml?c=193733&p=irol-newsArticle&ID=2068632",
+    function () {
+        var b = $(".ccbnContent .ccbnBgTxt")[0].innerText.trim();
+        return {
+            title: $(".ccbnContent .ccbnBgTtl")[0].innerText.trim(),
+            published_s: cs.scanForDate(b, "MMMM dd, yyyy"),
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
