f1a4e3cc0144530459a603b4c6d619dac58e00da
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-03 21:08:25 +0530

    Scripts for USA+Goodrich+Petroleum+Corp

--- Corporate+Press+Releases/USA+Goodrich+Petroleum+Corp_body.js
+++ Corporate+Press+Releases/USA+Goodrich+Petroleum+Corp_body.js
@@ -1,1 +1,13 @@
 // Goodrich Petroleum Corp
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://goodrichpetroleum.investorroom.com/2015-07-27-Goodrich-Petroleum-Corporation-Announces-Sale-Of-Proved-Reserves-And-Portion-Of-Leasehold-In-The-Eagle-Ford-Shale",
+    function () {
+        return {
+            title: $(".wd_news_releases-detail .detail_header")[0].innerText.trim(),
+            published_s: $(".wd_news_releases-detail .release_data")[0].innerText.trim(),
+            body: $(".wd_news_releases-detail .news_body")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Goodrich+Petroleum+Corp_list.js
+++ Corporate+Press+Releases/USA+Goodrich+Petroleum+Corp_list.js
@@ -1,1 +1,10 @@
-http://goodrichpetroleum.investorroom.com/news-releases
+//http://goodrichpetroleum.investorroom.com/news-releases
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://goodrichpetroleum.investorroom.com/news-releases"], function () {
+    var urls = [];
+    $(".spintable .item_name a.itemlink").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
