7c0f1f05a40701edc1971c8bd10c2aab12782dd3
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-03 22:11:48 +0530

    Scripts for Norway+Ganger+Rolf+ASA

--- Corporate+Press+Releases/Norway+Ganger+Rolf+ASA_body.js
+++ Corporate+Press+Releases/Norway+Ganger+Rolf+ASA_body.js
@@ -1,1 +1,24 @@
 // Ganger Rolf ASA
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.ganger-rolf.com/press-release?SelectedRelease=http://publish.ne.cision.com/Release/GetDetail/2348AEECABE97F4F",
+    function () {
+        var t = $(".threeColumnCenter h1")[0].innerText.trim();
+        var urls = [];
+        $(".threeColumnCenter .reprotFilesDetails a").each(function (idx, el) {
+            urls.push(el.href);
+        });
+        $(".threeColumnCenter h1, .threeColumnCenter .reprotFilesDetails").remove();
+        var b = $(".threeColumnCenter")[0].innerText.trim();
+        $(".threeColumnCenter p").remove();
+        var ps = $(".threeColumnCenter")[0].innerText.trim();
+        b.replace(ps, "");
+        return {
+            title: t,
+            published_s: ps.replace(/\//g,'-'),
+            body: b,
+            links: urls,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Norway+Ganger+Rolf+ASA_list.js
+++ Corporate+Press+Releases/Norway+Ganger+Rolf+ASA_list.js
@@ -1,1 +1,10 @@
-http://www.ganger-rolf.com/press-release
+//http://www.ganger-rolf.com/press-release
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.ganger-rolf.com/press-release"], function () {
+    var urls = [];
+    $(".eventList a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
