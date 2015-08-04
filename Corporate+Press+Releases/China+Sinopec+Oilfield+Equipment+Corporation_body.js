e198a396bd6cf665f3d428c1956b17b7cd2c8a94
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-04 23:17:21 +0530

    Scripts for China+Sinopec+Oilfield+Equipment+Corporation

--- Corporate+Press+Releases/China+Sinopec+Oilfield+Equipment+Corporation_body.js
+++ Corporate+Press+Releases/China+Sinopec+Oilfield+Equipment+Corporation_body.js
@@ -1,1 +1,14 @@
 // Sinopec Oilfield Equipment Corporation
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.kingdream.com/yw/news_infro.asp?ArtID=3146",
+    function () {
+        var t = $("table[style='margin-top:10px; margin-bottom:10px;'] .p4")[0].innerText.trim();
+        $("table[style='margin-top:10px; margin-bottom:10px;'] .p4").remove();
+        return {
+            title: t,
+            body: $("table[style='margin-top:10px; margin-bottom:10px;']")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/China+Sinopec+Oilfield+Equipment+Corporation_list.js
+++ Corporate+Press+Releases/China+Sinopec+Oilfield+Equipment+Corporation_list.js
@@ -1,1 +1,10 @@
-http://www.kingdream.com/yw/news.asp
+//http://www.kingdream.com/yw/news.asp
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.kingdream.com/yw/news.asp"], function () {
+    var urls = [];
+    $("td[height='400'] a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
