2e7e9ffe1302b387df659718889ffd229d6c93c9
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-04 22:36:35 +0530

    Scripts for Canada+Transglobe+Energy+Corporation

--- Corporate+Press+Releases/Canada+Transglobe+Energy+Corporation_body.js
+++ Corporate+Press+Releases/Canada+Transglobe+Energy+Corporation_body.js
@@ -1,1 +1,14 @@
 // Transglobe Energy Corporation
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.trans-globe.com/news/release?id=1960803",
+    function () {
+        var b = $(".news_article .mw_release")[0].innerText.trim();
+        return {
+            title: $(".news_article h3")[0].innerText.trim(),
+            published_s: cs.scanForDate(b, "MMMM dd, yyyy"),
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Canada+Transglobe+Energy+Corporation_list.js
+++ Corporate+Press+Releases/Canada+Transglobe+Energy+Corporation_list.js
@@ -1,1 +1,10 @@
-http://www.trans-globe.com/news/
+//http://www.trans-globe.com/news/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.trans-globe.com/news/"], function () {
+    var urls = [];
+    $("div.main h3 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
