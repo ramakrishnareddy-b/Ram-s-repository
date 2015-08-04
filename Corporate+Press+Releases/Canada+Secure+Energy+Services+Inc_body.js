c56281b6f642ccc2e9995c746981b5aa2de30297
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-04 22:45:43 +0530

    Scripts for Canada+Secure+Energy+Services+Inc

--- Corporate+Press+Releases/Canada+Secure+Energy+Services+Inc_body.js
+++ Corporate+Press+Releases/Canada+Secure+Energy+Services+Inc_body.js
@@ -1,1 +1,14 @@
 // Secure Energy Services Inc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://secure-energy.mediaroom.com/2015-07-29-SECURE-Energy-Services-Announces-Two-New-Locations-and-Results-for-the-Second-Quarter-Ended-June-30-2015",
+    function () {
+        var b = $(".wd_newsfeed_releases-detail .wd_body")[0].innerText.trim();
+        return {
+            title: $(".wd_newsfeed_releases-detail .wd_title")[0].innerText.trim(),
+            published_s: cs.scanForDate(b, "MMMM dd, yyyy"),
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Canada+Secure+Energy+Services+Inc_list.js
+++ Corporate+Press+Releases/Canada+Secure+Energy+Services+Inc_list.js
@@ -1,1 +1,10 @@
-http://secure-energy.mediaroom.com/
+//http://secure-energy.mediaroom.com/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://secure-energy.mediaroom.com/"], function () {
+    var urls = [];
+    $(".wd_item_list .wd_title a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
