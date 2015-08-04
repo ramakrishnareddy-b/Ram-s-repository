fa2f81e17318924a83e8ad343535764895221a0d
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-04 21:48:35 +0530

    Scripts for Canada+Whitecap+Resources+Inc

--- Corporate+Press+Releases/Canada+Whitecap+Resources+Inc_body.js
+++ Corporate+Press+Releases/Canada+Whitecap+Resources+Inc_body.js
@@ -1,1 +1,18 @@
 // Whitecap Resources Inc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.wcap.ca/news-category/18-2015/154-whitecap-resources-inc-confirms-monthly-dividend-for-july-of-0-0625-per-share.html",
+    function () {
+        var t = jQuery(".item-page .title")[0].innerText.trim();
+        jQuery(".item-page .title").remove();
+        var b = jQuery(".item-page")[0].innerText.trim();
+        var ps = cs.scanForDate(b, "MMMM dd, yyyy");
+        b.replace(ps, "");
+        return {
+            title: t,
+            published_s: ps,
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Canada+Whitecap+Resources+Inc_list.js
+++ Corporate+Press+Releases/Canada+Whitecap+Resources+Inc_list.js
@@ -1,1 +1,11 @@
-http://www.wcap.ca/news.html
+//http://www.wcap.ca/news.html
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.wcap.ca/news.html"], function () {
+    var urls = [];
+    jQuery(".slides-container .slides li[style='top: 0px; left: 0px; position: absolute; display: none; width: 920px; height: 1170px;']").remove();
+    jQuery(".slides-container .lnd_latestnews.piet a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
