4427485704b0615ec53797262fe4e44a1638da4a
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-04 23:02:52 +0530

    Scripts for USA+Dorchester+Minerals+L.P.

--- Corporate+Press+Releases/USA+Dorchester+Minerals+L.P._body.js
+++ Corporate+Press+Releases/USA+Dorchester+Minerals+L.P._body.js
@@ -1,1 +1,16 @@
 // Dorchester Minerals L.P.
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.dmlp.net/releasedetail.cfm?ReleaseID=913769",
+    function () {
+        var t = $("#wsh_middle h1")[0].innerText.trim();
+        var ps = $("#wsh_middle .wsh_printDate")[0].innerText.trim();
+        $("#wsh_middle h1, #wsh_middle #wsh_printBar, #wsh_middle .wsh_prinertOff").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $("#wsh_middle")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Dorchester+Minerals+L.P._list.js
+++ Corporate+Press+Releases/USA+Dorchester+Minerals+L.P._list.js
@@ -1,1 +1,11 @@
-http://www.dmlp.net/releases.cfm
+//http://www.dmlp.net/releases.cfm
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.dmlp.net/releases.cfm"], function () {
+    var urls = [];
+    $(".dataTable .noprint, .dataTable .wsh_file_Type, .dataTable .dataTableBottom").remove();
+    $(".dataTable .wsh_more a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
