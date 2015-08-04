546174aea99b48275b7304c3f5d3967b5b2d6574
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-04 22:22:33 +0530

    Scripts for Russia+RusPetro+Plc

--- Corporate+Press+Releases/Russia+RusPetro+Plc_body.js
+++ Corporate+Press+Releases/Russia+RusPetro+Plc_body.js
@@ -1,1 +1,17 @@
 // RusPetro Plc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.ruspetro.com/media/company-news/2015/06/09/agm-presentation-2015/index.html",
+    function () {
+        var urls = [];
+        $("#mainContent ul a").each(function (idx, el) {
+            urls.push(el.href);
+        });
+        return {
+            title: $("#mainContent .news-header h1")[0].innerText.trim(),
+            published_s: $("#mainContent p")[0].innerText.trim(),
+            links: urls,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Russia+RusPetro+Plc_list.js
+++ Corporate+Press+Releases/Russia+RusPetro+Plc_list.js
@@ -1,1 +1,11 @@
-http://www.ruspetro.com/media/index.html
+//http://www.ruspetro.com/media/index.html
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.ruspetro.com/media/index.html"], function () {
+    var urls = [];
+    $("#presslist li[style='display: none;']").remove();
+    $("#presslist .newspagetitle a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
