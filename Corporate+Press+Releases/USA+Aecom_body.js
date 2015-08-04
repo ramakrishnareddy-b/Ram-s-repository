9183100a8dedd4581e3b8398862f7951a3d20ddc
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-04 22:14:45 +0530

    Scripts for USA+Aecom

--- Corporate+Press+Releases/USA+Aecom_body.js
+++ Corporate+Press+Releases/USA+Aecom_body.js
@@ -1,1 +1,16 @@
 // Aecom
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://aecom.com/News/_featuredStories/Janet+C.+Wolfenbarger+to+Join+AECOM+Board+of+Directors",
+    function () {
+        var t = $("#siabcontentbody #titleDiv h1")[0].innerText.trim();
+        var ps = $("#siabcontentbody #titleDiv h5")[0].innerText.trim();
+        $("#siabcontentbody #titleDiv").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $("#siabcontentbody")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Aecom_list.js
+++ Corporate+Press+Releases/USA+Aecom_list.js
@@ -1,1 +1,10 @@
-http://aecom.com/News/Press+Releases?_ga=1.160183483.747549448.1437992856
+//http://aecom.com/News/Press+Releases?_ga=1.160183483.747549448.1437992856
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://aecom.com/News/Press+Releases?_ga=1.160183483.747549448.1437992856"], function () {
+    var urls = [];
+    $(".linkList_2 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
