a095fe95ac3f36e7278c1a2e31ccf1792b849fc7
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-05 23:35:58 +0530

    Scripts for Canada+Trinidad+Drilling+Ltd

--- Corporate+Press+Releases/Canada+Trinidad+Drilling+Ltd_body.js
+++ Corporate+Press+Releases/Canada+Trinidad+Drilling+Ltd_body.js
@@ -1,1 +1,14 @@
 // Trinidad Drilling Ltd
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://trinidaddrilling.mediaroom.com/index.php?s=22496&item=137162",
+    function () {
+        var b = $(".wd_news_releases-detail .news_body")[0].innerText.trim();
+        return {
+            title: $(".wd_news_releases-detail h1.detail_header")[0].innerText.trim(),
+            published_s: cs.scanForDate(b, "MMMM dd, yyyy"),
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Canada+Trinidad+Drilling+Ltd_list.js
+++ Corporate+Press+Releases/Canada+Trinidad+Drilling+Ltd_list.js
@@ -1,1 +1,10 @@
-http://trinidaddrilling.mediaroom.com/index.php?s=22496
+//http://trinidaddrilling.mediaroom.com/index.php?s=22496
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://trinidaddrilling.mediaroom.com/index.php?s=22496"], function () {
+    var urls = [];
+    $(".spintable a.itemlink").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
