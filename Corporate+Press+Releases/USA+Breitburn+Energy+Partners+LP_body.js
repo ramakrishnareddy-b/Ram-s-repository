286d4b76de74d2ed11003b59ba430febf6e25e09
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-05 22:47:01 +0530

    Scripts for USA+Breitburn+Energy+Partners+LP

--- Corporate+Press+Releases/USA+Breitburn+Energy+Partners+LP_body.js
+++ Corporate+Press+Releases/USA+Breitburn+Energy+Partners+LP_body.js
@@ -1,1 +1,13 @@
 // Breitburn Energy Partners LP
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://ir.breitburn.com/releasedetail.cfm?ReleaseID=925091",
+    function () {
+        return {
+            title: $("#ndq-article h1")[0].innerText.trim(),
+            published_s: $("#ndq-article .ndq-leftcol")[0].innerText.trim(),
+            body: $("#ndq-article #ndq-releasebody")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Breitburn+Energy+Partners+LP_list.js
+++ Corporate+Press+Releases/USA+Breitburn+Energy+Partners+LP_list.js
@@ -1,1 +1,10 @@
-http://ir.breitburn.com/releases.cfm
+//http://ir.breitburn.com/releases.cfm
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://ir.breitburn.com/releases.cfm"], function () {
+    var urls = [];
+    $(".wsh-dataTable a.newsTitle").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
