8eaf4c88580a2bb93c81ec7e1ea5d00f708f12c6
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-05 21:45:36 +0530

    Scripts for USA+Magnum+Hunter+Resources+Corporation

--- Corporate+Press+Releases/USA+Magnum+Hunter+Resources+Corporation_body.js
+++ Corporate+Press+Releases/USA+Magnum+Hunter+Resources+Corporation_body.js
@@ -1,1 +1,13 @@
 // Magnum Hunter Resources Corporation
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.b2i.us/profiles/investor/ResLibraryView.asp?ResLibraryID=78260&GoTopage=1&Category=1848&BzID=1969&G=202",
+    function () {
+        return {
+            title: $(".b2iNewsStoryTable .b2iNewsStoryHeadline")[0].innerText.trim(),
+            published_s: $(".b2iNewsStoryTable .b2iNewsStoryDate")[0].innerText.trim(),
+            body: $(".b2iNewsStoryTable .b2iNewsStoryBody")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Magnum+Hunter+Resources+Corporation_list.js
+++ Corporate+Press+Releases/USA+Magnum+Hunter+Resources+Corporation_list.js
@@ -1,1 +1,10 @@
-http://www.magnumhunterresources.com/pressreleases.html
+//http://www.magnumhunterresources.com/pressreleases.html
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.magnumhunterresources.com/pressreleases.html"], function () {
+    var urls = [];
+    $(".b2iNewsListContent .b2iNewsItemHeadline a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
