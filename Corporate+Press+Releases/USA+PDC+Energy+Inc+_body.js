b487fa432e376c1853e9105a3374bc0fe38e77b5
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-05 23:28:19 +0530

    Scripts for USA+PDC+Energy+Inc+

--- Corporate+Press+Releases/USA+PDC+Energy+Inc+_body.js
+++ Corporate+Press+Releases/USA+PDC+Energy+Inc+_body.js
@@ -1,1 +1,16 @@
-// PDC Energy Inc 
+// PDC Energy Inc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://investor.pdce.com/releasedetail.cfm?ReleaseID=921572",
+    function () {
+        var t = $("#wsh_middle h1")[0].innerText.trim();
+        var ps = $("#wsh_middle .wsh_printDate")[0].innerText.trim();
+        $("#wsh_middle h1, #wsh_middle .wsh_printDate, #wsh_middle .wsh_prinertOff").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $("#wsh_middle")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+PDC+Energy+Inc+_list.js
+++ Corporate+Press+Releases/USA+PDC+Energy+Inc+_list.js
@@ -1,1 +1,10 @@
-http://investor.pdce.com/releases.cfm
+//http://investor.pdce.com/releases.cfm
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://investor.pdce.com/releases.cfm"], function () {
+    var urls = [];
+    $(".dataTable .wsh_more a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
