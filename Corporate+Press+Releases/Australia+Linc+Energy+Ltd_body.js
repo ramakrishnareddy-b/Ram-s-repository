540f3b4d16d5a0c41c6093372b553ef72bc72f03
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-05 08:23:56 +0530

    Scripts for Australia+Linc+Energy+Ltd

--- Corporate+Press+Releases/Australia+Linc+Energy+Ltd_body.js
+++ Corporate+Press+Releases/Australia+Linc+Energy+Ltd_body.js
@@ -1,1 +1,19 @@
 // Linc Energy Ltd
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://lincenergy.listedcompany.com/news.html/id/481261",
+    function () {
+        $("#centre-col .si_marginBottom .si_floatRight").remove();
+        var urls = [];
+        jQuery('#centre-col ol li a').each(function (idx, el) {
+            urls.push(el.href);
+        });
+        return {
+            title: $("#centre-col h2")[0].innerText.trim(),
+            published_s: $("#centre-col .si_marginBottom")[0].innerText.trim(),
+            body: $("#centre-col .ir_table")[0].innerText.trim(),
+            links: urls,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Australia+Linc+Energy+Ltd_list.js
+++ Corporate+Press+Releases/Australia+Linc+Energy+Ltd_list.js
@@ -1,1 +1,10 @@
-http://lincenergy.listedcompany.com/newsroom.html
+//http://lincenergy.listedcompany.com/newsroom.html
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://lincenergy.listedcompany.com/newsroom.html"], function () {
+    var urls = [];
+    $(".ir_news .ir_newsTitle a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
