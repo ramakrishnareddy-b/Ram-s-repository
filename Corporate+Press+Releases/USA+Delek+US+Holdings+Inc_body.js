51e5bd1b7a1ede1494a7c677f7a1fb1057202a30
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-05 22:57:01 +0530

    Scripts for USA+Delek+US+Holdings+Inc

--- Corporate+Press+Releases/USA+Delek+US+Holdings+Inc_body.js
+++ Corporate+Press+Releases/USA+Delek+US+Holdings+Inc_body.js
@@ -1,1 +1,14 @@
 // Delek US Holdings Inc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://phx.corporate-ir.net/phoenix.zhtml?c=196835&p=irol-newsArticle&ID=2075172",
+    function () {
+        var b = $(".node-inner .ccbnTxt")[0].innerText.trim();
+        return {
+            title: $(".node-inner .ccbnTtl")[0].innerText.trim(),
+            published_s: cs.scanForDate(b, "MMMM dd, yyyy"),
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Delek+US+Holdings+Inc_list.js
+++ Corporate+Press+Releases/USA+Delek+US+Holdings+Inc_list.js
@@ -1,1 +1,10 @@
-http://phx.corporate-ir.net/phoenix.zhtml?c=196835&p=irol-news
+//http://phx.corporate-ir.net/phoenix.zhtml?c=196835&p=irol-news
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://phx.corporate-ir.net/phoenix.zhtml?c=196835&p=irol-news"], function () {
+    var urls = [];
+    $(".ccbnOutline a.ccbnTblLnk").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
