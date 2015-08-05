b22469242b1640b76f0ea55875d7aee13b5d0361
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-05 23:43:55 +0530

    Scripts for USA+Halcón+Resources+Corporation+

--- Corporate+Press+Releases/USA+Halcón+Resources+Corporation+_list.js
+++ Corporate+Press+Releases/USA+Halcón+Resources+Corporation+_list.js
@@ -1,1 +1,10 @@
-http://investors.halconresources.com/releases.cfm
+//http://investors.halconresources.com/releases.cfm
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://investors.halconresources.com/releases.cfm"], function () {
+    var urls = [];
+    jQuery(".dataTable .wsh_more a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
