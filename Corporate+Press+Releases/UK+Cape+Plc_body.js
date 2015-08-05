3fbac7eec744a1fcd2ba032d767e4fc244269df3
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-05 22:22:36 +0530

    Scripts for UK+Cape+Plc

--- Corporate+Press+Releases/UK+Cape+Plc_body.js
+++ Corporate+Press+Releases/UK+Cape+Plc_body.js
@@ -1,1 +1,16 @@
 // Cape Plc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.capeplc.com/news-and-media/news-announcements/2015/08/meet-cape-at-offshore-europe.aspx",
+    function () {
+        var t = $(".grid_66 h1").text().trim();
+        var ps = $(".grid_66 p:first")[0].innerText.trim();
+        $(".grid_66 h1, .grid_66 p:first").remove();
+        return {
+            title: t,
+            published_s: ps,
+            body: $(".grid_66")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/UK+Cape+Plc_list.js
+++ Corporate+Press+Releases/UK+Cape+Plc_list.js
@@ -1,1 +1,10 @@
-http://www.capeplc.com/news-and-media/news-announcements.aspx
+//http://www.capeplc.com/news-and-media/news-announcements.aspx
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.capeplc.com/news-and-media/news-announcements.aspx"], function () {
+    var urls = [];
+    $(".news_announce .announce a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
