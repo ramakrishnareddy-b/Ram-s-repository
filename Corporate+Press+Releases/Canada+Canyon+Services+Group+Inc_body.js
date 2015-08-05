d6ced598a244848f4c3ce60dcb3dc47393ea7446
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-05 09:05:08 +0530

    Scripts for Canada+Canyon+Services+Group+Inc

--- Corporate+Press+Releases/Canada+Canyon+Services+Group+Inc_body.js
+++ Corporate+Press+Releases/Canada+Canyon+Services+Group+Inc_body.js
@@ -1,1 +1,13 @@
 // Canyon Services Group Inc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.canyontech.ca/newsroom/articles/article-122602/",
+    function () {
+        return {
+            title: $(".news_article .entry-title")[0].innerText.trim(),
+            published_s: $(".news_article .date-label")[0].innerText.trim().replace(/\//g, '-'),
+            body: $(".news_article .entry-content")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Canada+Canyon+Services+Group+Inc_list.js
+++ Corporate+Press+Releases/Canada+Canyon+Services+Group+Inc_list.js
@@ -1,1 +1,10 @@
-http://www.canyontech.ca/newsroom/news_category/releases/
+//http://www.canyontech.ca/newsroom/news_category/releases/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.canyontech.ca/newsroom/news_category/releases/"], function () {
+    var urls = [];
+    $(".news-grid a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
