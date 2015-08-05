cf3a5e4cc8ba66821c42c03fe499fee783d316bf
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-05 22:36:00 +0530

    Scripts for China+Xinjiang+International+Industry+Co+Ltd

--- Corporate+Press+Releases/China+Xinjiang+International+Industry+Co+Ltd_body.js
+++ Corporate+Press+Releases/China+Xinjiang+International+Industry+Co+Ltd_body.js
@@ -1,1 +1,16 @@
 // Xinjiang International Industry Co Ltd
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.xjgjsy.com/Html/xwzxnr.asp?ID=261&SortID=10",
+    function () {
+        var urls = [];
+        $(".xuxian1 p a").each(function (idx, el) {
+            urls.push(el.href);
+        });
+        return {
+            published_s: cs.scanForDate($(".xuxian1 font[color='999999']")[0].innerText.trim(), "yyyy-MM-dd"),
+            links: urls,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/China+Xinjiang+International+Industry+Co+Ltd_list.js
+++ Corporate+Press+Releases/China+Xinjiang+International+Industry+Co+Ltd_list.js
@@ -1,1 +1,10 @@
-http://www.xjgjsy.com/Html/xwzx.asp?SortID=10&SortPath=0,10,
+//http://www.xjgjsy.com/Html/xwzx.asp?SortID=10&SortPath=0,10,
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.xjgjsy.com/Html/xwzx.asp?SortID=10&SortPath=0,10,"], function () {
+    var urls = [];
+    $("#table1 a[target='_blank']").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
