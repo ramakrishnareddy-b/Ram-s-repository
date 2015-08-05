42acf029aee91988e1d8ec14d17b795e36c32ee1
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-05 23:14:36 +0530

    Scripts for USA+Basic+Energy+Services+Inc

--- Corporate+Press+Releases/USA+Basic+Energy+Services+Inc_body.js
+++ Corporate+Press+Releases/USA+Basic+Energy+Services+Inc_body.js
@@ -1,1 +1,14 @@
 // Basic Energy Services Inc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://ir.basicenergyservices.com/phoenix.zhtml?c=130445&p=irol-newsArticle&ID=2073359",
+    function () {
+        var b = jQuery(".item-page .ccbnTxt")[0].innerText.trim();
+        return {
+            title: jQuery(".item-page .ccbnTtl")[0].innerText.trim(),
+            published_s: cs.scanForDate(b, "MMMM dd, yyyy"),
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Basic+Energy+Services+Inc_list.js
+++ Corporate+Press+Releases/USA+Basic+Energy+Services+Inc_list.js
@@ -1,1 +1,11 @@
-http://ir.basicenergyservices.com/phoenix.zhtml?c=130445&p=irol-news&nyo=0
+//http://ir.basicenergyservices.com/phoenix.zhtml?c=130445&p=irol-news&nyo=0
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://ir.basicenergyservices.com/phoenix.zhtml?c=130445&p=irol-news&nyo=0"], function () {
+    var urls = [];
+    jQuery("tr.ccbnOutline a.ccbnTblLnk[target='_blank']").remove();
+    jQuery("tr.ccbnOutline a.ccbnTblLnk").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
