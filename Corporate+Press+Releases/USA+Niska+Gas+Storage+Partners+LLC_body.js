b79f56759e087bf6dae519e0ae54ec89bbf14dab
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-05 22:13:51 +0530

    Scripts for USA+Niska+Gas+Storage+Partners+LLC

--- Corporate+Press+Releases/USA+Niska+Gas+Storage+Partners+LLC_body.js
+++ Corporate+Press+Releases/USA+Niska+Gas+Storage+Partners+LLC_body.js
@@ -1,1 +1,14 @@
 // Niska Gas Storage Partners LLC
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://investor.niskapartners.com/phoenix.zhtml?c=235313&p=irol-newsArticle&ID=2059007",
+    function () {
+        var b = $("#thomson .ccbnTxt, .ccbn-tab-content-active .ccbnTxt")[0].innerText.trim();
+        return {
+            title: $("#thomson .ccbnTtl, .ccbn-tab-content-active h3")[0].innerText.trim(),
+            published_s: cs.scanForDate(b, "MMMM dd, yyyy"),
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Niska+Gas+Storage+Partners+LLC_list.js
+++ Corporate+Press+Releases/USA+Niska+Gas+Storage+Partners+LLC_list.js
@@ -1,1 +1,11 @@
-http://investor.niskapartners.com/phoenix.zhtml?c=235313&p=irol-news&nyo=0
+//http://investor.niskapartners.com/phoenix.zhtml?c=235313&p=irol-news&nyo=0
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://investor.niskapartners.com/phoenix.zhtml?c=235313&p=irol-news&nyo=0"], function () {
+    var urls = [];
+    $(".ccbnOutline .ccbnTblLnk[target='_blank']").remove();
+    $(".ccbnOutline .ccbnTblLnk").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
