ec1b5e5097b18f48cbf107fce99d68787cf5f91b
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-05 23:05:36 +0530

    Scripts for USA+Parker+Drilling+Co

--- Corporate+Press+Releases/USA+Parker+Drilling+Co_body.js
+++ Corporate+Press+Releases/USA+Parker+Drilling+Co_body.js
@@ -1,1 +1,16 @@
 // Parker Drilling Co
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://parkerdrilling.investorroom.com/index-php-s-19-item-94",
+    function () {
+        var t = $(".wd_newsfeed_releases-detail .wd_title")[0].innerText.trim();
+        $(".wd_newsfeed_releases-detail .wd_title").remove();
+        var b = $(".wd_newsfeed_releases-detail")[0].innerText.trim();
+        return {
+            title: t,
+            published_s: cs.scanForDate(b, "MMMM dd, yyyy"),
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Parker+Drilling+Co_list.js
+++ Corporate+Press+Releases/USA+Parker+Drilling+Co_list.js
@@ -1,1 +1,10 @@
-http://www.parkerdrilling.com/newsroom.aspx
+//http://www.parkerdrilling.com/newsroom.aspx
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.parkerdrilling.com/newsroom.aspx"], function () {
+    var urls = [];
+    $("#newsreleases a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
