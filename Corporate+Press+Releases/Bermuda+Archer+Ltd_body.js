02069c830ca59ffc818f4bf8571121151552961b
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-05 08:33:49 +0530

    Scripts for Bermuda+Archer+Ltd

--- Corporate+Press+Releases/Bermuda+Archer+Ltd_body.js
+++ Corporate+Press+Releases/Bermuda+Archer+Ltd_body.js
@@ -1,1 +1,14 @@
 // Archer Ltd
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://archerwell.com/archer-limited-second-quarter-2015-results/",
+    function () {
+        var b = $('.content__body')[0].innerText.trim();
+        return {
+            title: $("h1.page__header__title")[0].innerText.trim(),
+            published_s: cs.scanForDate(b, "MMMM dd, yyyy"),
+            body: b,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/Bermuda+Archer+Ltd_list.js
+++ Corporate+Press+Releases/Bermuda+Archer+Ltd_list.js
@@ -1,1 +1,10 @@
-http://archerwell.com/press/
+//http://archerwell.com/press/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://archerwell.com/press/"], function () {
+    var urls = [];
+    $(".archive-list a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
