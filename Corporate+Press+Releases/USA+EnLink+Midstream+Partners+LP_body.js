694c22f0b83dbb2e68ac67ff80a648dfc73e8013
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-06 22:09:47 +0530

    Scripts for USA+EnLink+Midstream+Partners+LP

--- Corporate+Press+Releases/USA+EnLink+Midstream+Partners+LP_body.js
+++ Corporate+Press+Releases/USA+EnLink+Midstream+Partners+LP_body.js
@@ -1,1 +1,18 @@
 // EnLink Midstream Partners LP
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://enlk.enlink.com/investor/investor-news/investor-news-details/2015/EnLink-Midstream-Reports-Second-Quarter-2015-Results/default.aspx",
+    function () {
+        var urls = [];
+        $(".ModuleContainer .ModuleLinks a").each(function (idx, el) {
+            urls.push(el.href);
+        });
+        return {
+            title: $(".ModuleContainer h1")[0].innerText.trim(),
+            published_s: $(".ModuleContainer .ModuleDate")[0].innerText.trim().replace(/\//g,'-'),
+            body: $(".ModuleContainer .ModuleBody")[0].innerText.trim(),
+            links: urls,
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+EnLink+Midstream+Partners+LP_list.js
+++ Corporate+Press+Releases/USA+EnLink+Midstream+Partners+LP_list.js
@@ -1,1 +1,10 @@
-http://enlk.enlink.com/investor/investor-news/default.aspx?
+//http://enlk.enlink.com/investor/investor-news/default.aspx?
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://enlk.enlink.com/investor/investor-news/default.aspx?"], function () {
+    var urls = [];
+    $(".ModuleContainerInner a.ModuleHeadlineLink").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
