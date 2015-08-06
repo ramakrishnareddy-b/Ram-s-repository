f3c40c5bc23a7a18a8869035373957204cd5ecec
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-06 22:43:10 +0530

    Scripts for USA+Energy+Investments+Global+Ltd

--- Corporate+Press+Releases/USA+Energy+Investments+Global+Ltd+_body.js
+++ Corporate+Press+Releases/USA+Energy+Investments+Global+Ltd+_body.js
@@ -1,1 +1,13 @@
 // Energy Investments Global Ltd 
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.eigpartners.com/news/press-releases/eig-hires-rob-johnson-us-head-oil-gas-direct-lending-1",
+    function () {
+        return {
+            title: $("section.col-md-9 .page-title")[0].innerText.trim(),
+            published_s: $("section.col-md-9 .datetime")[0].innerText.trim(),
+            body: $("section.col-md-9 .field-name-body")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Energy+Investments+Global+Ltd+_list.js
+++ Corporate+Press+Releases/USA+Energy+Investments+Global+Ltd+_list.js
@@ -1,1 +1,10 @@
-http://www.eigpartners.com/news/press-releases
+//http://www.eigpartners.com/news/press-releases
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.eigpartners.com/news/press-releases"], function () {
+    var urls = [];
+    $(".view-content h4 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
