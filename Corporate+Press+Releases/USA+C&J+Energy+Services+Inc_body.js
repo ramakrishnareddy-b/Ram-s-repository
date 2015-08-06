9731145ca17faf0979479337a67f8b0dd7fce21a
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-06 22:25:40 +0530

    Scripts for USA+C&J+Energy+Services+Inc

--- Corporate+Press+Releases/USA+C&J+Energy+Services+Inc_body.js
+++ Corporate+Press+Releases/USA+C&J+Energy+Services+Inc_body.js
@@ -1,1 +1,13 @@
 // C&J Energy Services Inc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.cjenergy.com/news-events/press-releases/cj-energy-services-belfield-north-dakota-facility-earns-osha-vpp-star-certi",
+    function () {
+        return {
+            title: $(".article-large h2")[0].innerText.trim(),
+            published_s: $(".article-large .article-meta p")[0].innerText.trim(),
+            body: $(".article-large .article-content")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+C&J+Energy+Services+Inc_list.js
+++ Corporate+Press+Releases/USA+C&J+Energy+Services+Inc_list.js
@@ -1,1 +1,10 @@
-http://www.cjenergy.com/news-events/press-releases
+//http://www.cjenergy.com/news-events/press-releases
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.cjenergy.com/news-events/press-releases"], function () {
+    var urls = [];
+    $(".days h3 a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
