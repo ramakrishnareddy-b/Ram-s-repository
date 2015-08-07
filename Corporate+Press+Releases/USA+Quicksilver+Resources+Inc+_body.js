122e2027e0d2123cf9c274cda8038663399476e3
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-07 21:56:33 +0530

    Scripts for USA+Quicksilver+Resources+Inc+

--- Corporate+Press+Releases/USA+Quicksilver+Resources+Inc+_body.js
+++ Corporate+Press+Releases/USA+Quicksilver+Resources+Inc+_body.js
@@ -1,1 +1,13 @@
-// Quicksilver Resources Inc 
+// Quicksilver Resources Inc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://investors.qrinc.com/releasedetail.cfm?releaseid=920378",
+    function () {
+        return {
+            title: $("#ndq-article h1")[0].innerText.trim(),
+            published_s: $("#ndq-article .ndq-leftcol")[0].innerText.trim(),
+            body: $("#ndq-article #ndq-releasebody")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Quicksilver+Resources+Inc+_list.js
+++ Corporate+Press+Releases/USA+Quicksilver+Resources+Inc+_list.js
@@ -1,1 +1,10 @@
-http://investors.qrinc.com/releases.cfm
+//http://investors.qrinc.com/releases.cfm
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://investors.qrinc.com/releases.cfm"], function () {
+    var urls = [];
+    $(".wsh-dataTable .ndq-filetypes .ndqicon-html-1").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
