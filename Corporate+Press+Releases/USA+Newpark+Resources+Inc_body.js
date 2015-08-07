e9fc34cd4573e672e516ca233691b7d3283975b4
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-07 21:34:33 +0530

    Scripts for USA+Newpark+Resources+Inc

--- Corporate+Press+Releases/USA+Newpark+Resources+Inc_body.js
+++ Corporate+Press+Releases/USA+Newpark+Resources+Inc_body.js
@@ -1,1 +1,13 @@
 // Newpark Resources Inc
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.contentEvaluate("http://www.newpark.com/interests/investors/information-and-reports/news-releases?showNewsItem=2073273&filter[year]=2015&filter[text]=",
+    function () {
+        return {
+            title: jQuery(".newsItem h3")[0].innerText.trim(),
+            published_s: jQuery(".newsItem .newsDate")[0].innerText.trim(),
+            body: jQuery(".newsItem .wysiwygNewsItem")[0].innerText.trim(),
+            url: document.location.href
+        }; //returns object with title,published_s,body and url properties.
+    }
+);
--- Corporate+Press+Releases/USA+Newpark+Resources+Inc_list.js
+++ Corporate+Press+Releases/USA+Newpark+Resources+Inc_list.js
@@ -1,1 +1,11 @@
-http://www.newpark.com/interests/investors/information-and-reports/news-releases
+//http://www.newpark.com/interests/investors/information-and-reports/news-releases
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.newpark.com/interests/investors/information-and-reports/news-releases"], function () {
+    var urls = [];
+    jQuery(".newsTables .newsTable[style='display:none;']").remove();
+    jQuery(".newsTables .newsTable[style='display:block;'] .cellTitle a").each(function (idx, el) {
+        urls.push(el.href);
+    });
+    return urls;
+});
