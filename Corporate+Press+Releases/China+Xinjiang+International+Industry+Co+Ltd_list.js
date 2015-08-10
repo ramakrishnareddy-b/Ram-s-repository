83838d29ffcb1ba8a1fd255d459a554849612e61
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-10 07:00:36 +0530

    Change script of the PDF list with title, date and content.

--- Corporate+Press+Releases/China+Xinjiang+International+Industry+Co+Ltd_list.js
+++ Corporate+Press+Releases/China+Xinjiang+International+Industry+Co+Ltd_list.js
@@ -4,7 +4,9 @@ var main = require(require('system').env['NL_HOME'] + 'main.js');
 main.linksEvaluate(["http://www.xjgjsy.com/Html/xwzx.asp?SortID=10&SortPath=0,10,"], function () {
     var urls = [];
     $("#table1 a[target='_blank']").each(function (idx, el) {
-        urls.push(el.href);
+        var published_dt=cs.scanForDate($(el).parent().parent().parent().text(), "yyyy-MM-dd");
+        var obj={link:el.href,title:$(el).text(),published_s:published_dt,content:'pdf'};
+        urls.push(obj);
     });
     return urls;
 });
