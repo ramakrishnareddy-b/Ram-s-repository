6a2023911cd3fd34b9809f129ca19d338d8ce1bf
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-10 22:27:48 +0530

    Scripts for Australia+CUE+Energy+Resources

--- Corporate+Press+Releases/Australia+CUE+Energy+Resources+_list.js
+++ Corporate+Press+Releases/Australia+CUE+Energy+Resources+_list.js
@@ -1,1 +1,12 @@
-http://www.cuenrg.com.au/irm/content/asx-announcements.aspx?RID=8
+//http://www.cuenrg.com.au/irm/content/asx-announcements.aspx?RID=8
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.cuenrg.com.au/irm/content/asx-announcements.aspx?RID=8"], function () {
+    var urls = [];
+    $(".newsitems .article a").each(function (idx, el) {
+        var published_dt = $(el).next().parent()[0].innerText.trim();
+        var obj = {link: el.href, title: $(el).text().trim(), published_s: published_dt, content: 'pdf'};
+        urls.push(obj);
+    });
+    return urls;
+});
