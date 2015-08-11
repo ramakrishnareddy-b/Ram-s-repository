6f6720fb490d4480caaa89e54149d485d903e95c
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-11 23:16:36 +0530

    Scripts for Australia+AWE+Ltd

--- Corporate+Press+Releases/Australia+AWE+Ltd_list.js
+++ Corporate+Press+Releases/Australia+AWE+Ltd_list.js
@@ -1,1 +1,12 @@
-http://www.awexplore.com/irm/content/investor_asx.html
+//http://www.awexplore.com/irm/content/investor_asx.html
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.awexplore.com/irm/content/investor_asx.html"], function () {
+    var urls = [];
+    $(".newsitems .article").each(function (idx, el) {
+        var anc = $(el).find("a")[0];
+        var obj={link:anc.href,title:anc.innerText.trim(),published_s:$(el).find("articleDate").text(),content:'pdf'};
+        urls.push(obj);
+    });
+    return urls;
+});
