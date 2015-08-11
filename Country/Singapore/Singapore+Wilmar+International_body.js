887f80fdfd918542892ff772bdfd21b74c1b19a4
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-11 23:36:24 +0530

    Scripts for Singapore+Wilmar+International

--- Country/Singapore/Singapore+Wilmar+International_body.js
+++ Country/Singapore/Singapore+Wilmar+International_body.js
@@ -1,1 +1,2 @@
-// Singapore 
+// Singapore
+// PDF files.
--- Country/Singapore/Singapore+Wilmar+International_list.js
+++ Country/Singapore/Singapore+Wilmar+International_list.js
@@ -1,13 +1,14 @@
 //http://ir-media.wilmar-international.com/phoenix.zhtml?c=164878&p=irol-newsMedia&nyo=0
-
 var main=require(require('system').env['NL_HOME']+'main.js');
 
 main.linksEvaluate(["http://ir-media.wilmar-international.com/phoenix.zhtml?c=164878&p=irol-newsMedia&nyo=0"],function(){
-        var urls =[];
-        //if mutliple links with different selectors, use comma to put them in one jQuery selector.
-        jQuery('a.ccbnTblLnk').each(function(idx,el){
-            urls.push(el.href);
-        });
+    var urls =[];
+    $(".ccbnOutline .ccbnBgTblOdd, .ccbnOutline .ccbnBgTblEven").each(function (idx, el) {
+        var published_dt = $(el).find("td[nowrap='nowrap']").text().trim();
+        var anc = $(el).find(".ccbnTblLnk")[0];
+        var obj={link:anc.href,title:anc.innerText.trim(),published_s:published_dt,content:'pdf'};
+        urls.push(obj);
+    });
 
-        return urls;
+    return urls;
 });
