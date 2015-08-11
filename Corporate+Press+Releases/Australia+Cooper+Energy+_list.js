b0631e9be890b70bbd1fc59381df6b506bc443cf
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-11 23:26:04 +0530

    Scripts for Australia+Cooper+Energy+

--- Corporate+Press+Releases/Australia+Cooper+Energy+_list.js
+++ Corporate+Press+Releases/Australia+Cooper+Energy+_list.js
@@ -1,1 +1,13 @@
-http://www.cooperenergy.com.au/investor-information/asx-announcements
+//http://www.cooperenergy.com.au/investor-information/asx-announcements
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.cooperenergy.com.au/investor-information/asx-announcements"], function () {
+    var urls = [];
+    $(".sub-page-list a").each(function (idx, el) {
+        var published_dt = $(el).find(".date").text().trim();
+        $(el).find(".date").remove();
+        var obj={link:el.href,title:$(el).text().trim(),published_s:published_dt,content:'pdf'};
+        urls.push(obj);
+    });
+    return urls;
+});
