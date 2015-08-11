844e1f817061633463724ec4ca282fabd8fa5802
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-11 23:06:18 +0530

    Scripts for Australia+Oilex+Ltd

--- Corporate+Press+Releases/Australia+Oilex+Ltd_list.js
+++ Corporate+Press+Releases/Australia+Oilex+Ltd_list.js
@@ -1,1 +1,11 @@
-https://www.oilex.com.au/investor-information/announcements/all
+//https://www.oilex.com.au/investor-information/announcements/all
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["https://www.oilex.com.au/investor-information/announcements/all"], function () {
+    var urls = [];
+    $(".article-full-list a").each(function (idx, el) {
+        var obj={link:el.href,title:$(el).find("h2").text().trim(),published_s:$(el).find("time").text(),content:'pdf'};
+        urls.push(obj);
+    });
+    return urls;
+});
