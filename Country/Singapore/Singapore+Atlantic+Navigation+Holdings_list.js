1439c1a2cfdbdb6a497f4ca4ff7043c16fbce0b6
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-12 00:10:34 +0530

    Scripts for Singapore+Atlantic+Navigation+Holdings

--- Country/Singapore/Singapore+Atlantic+Navigation+Holdings_list.js
+++ Country/Singapore/Singapore+Atlantic+Navigation+Holdings_list.js
@@ -1,1 +1,17 @@
-http://www.atlanticnavigation.com/#!ir-news-all/cvuk
+//http://www.atlanticnavigation.com/#!ir-news-all/cvuk
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.linksEvaluate(["http://www.atlanticnavigation.com/#!ir-news-all/cvuk"],function(){
+    var urls =[];
+    $(".s12inlineContent .flex_vbox[style='position:relative;display:block;-webkit-box-orient:vertical;-webkit-flex-direction:column;-ms-flex-direction:column;flex-direction:column;box-sizing:border-box;']").each(function (idx, el) {
+        var published_dt = $(el).find(".color_3").text();
+        $(el).find("a").each(function(idx1, el1) {
+            if(published_dt != null) {
+                var obj={link:el1.href,title:$(el1).text().trim(),published_s:published_dt,content:'pdf'};
+                urls.push(obj);
+            }
+        });
+    });
+
+    return urls;
+});
