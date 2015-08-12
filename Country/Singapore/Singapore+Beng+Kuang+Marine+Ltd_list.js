dff23ae510c257f46d736cb579e0bb161fdbfa5e
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-12 22:18:32 +0530

    Scripts for Singapore+Beng+Kuang+Marine+Ltd

--- Country/Singapore/Singapore+Beng+Kuang+Marine+Ltd_list.js
+++ Country/Singapore/Singapore+Beng+Kuang+Marine+Ltd_list.js
@@ -1,1 +1,17 @@
-http://ir.chartnexus.com/bengkuangmarine/announcement.php
+//http://ir.chartnexus.com/bengkuangmarine/announcement.php
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.linksEvaluate(["http://ir.chartnexus.com/bengkuangmarine/announcement.php"],function(){
+    var urls =[];
+    $(".dpcontent .eventseven_m[style='display:none;'], .dpcontent .eventsodd_m[style='display:none;']").remove();
+    $(".dpcontent .eventseven_m, .dpcontent .eventsodd_m").each(function (idx, el) {
+        var published_dt = $(el).find(".annfull table").text().trim().replace(/\|/g, '');
+        var anc = $(el).find("a")[0];
+        if(anc != null) {
+            var obj={link:anc.href,title:$(el).find(".annfull p")[0].innerText.trim(),published_s:published_dt,content:'pdf'};
+            urls.push(obj);
+        }
+    });
+
+    return urls;
+});
