afd1061acc85a4b5b94428baf064dd6abc77d44e
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-15 11:29:25 +0530

    Scripts for Australia+Buru+Energy+Ltd

--- Corporate+Press+Releases/Australia+Buru+Energy+_list.js
+++ Corporate+Press+Releases/Australia+Buru+Energy+_list.js
@@ -1,1 +1,12 @@
-http://www.buruenergy.com/news/media-releases/
+//http://www.buruenergy.com/news/media-releases/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.buruenergy.com/news/media-releases/"], function () {
+    var urls = [];
+    jQuery(".prettyListItems a").each(function (idx, el) {
+        // date is not available for this website.
+        var obj = {link: el.href, title: jQuery(el).text().trim(), published_s: "", content: 'pdf'};
+        urls.push(obj);
+    });
+    return urls;
+});
