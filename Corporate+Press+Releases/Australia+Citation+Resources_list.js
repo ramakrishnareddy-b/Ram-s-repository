0d2c71ebb274f2568d19678fab9c09036c205578
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-15 12:06:45 +0530

    Scripts for Australia+Citation+Resources

--- Corporate+Press+Releases/Australia+Citation+Resources_list.js
+++ Corporate+Press+Releases/Australia+Citation+Resources_list.js
@@ -1,1 +1,12 @@
-http://citationresources.com.au/display/index/asx-announcements
+//http://citationresources.com.au/display/index/asx-announcements
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://citationresources.com.au/display/index/asx-announcements"], function () {
+    var urls = [];
+    $(".article-list tbody tr").each(function (idx, el) {
+        var anc = $(el).find("a")[0];
+        var obj={link:anc.href,title:anc.innerText.trim(),published_s:$(el).find(".date")[0].innerText.trim(),content:'pdf'};
+        urls.push(obj);
+    });
+    return urls;
+});
