58fecfe5ae43cf9f47d31f90d4a68db525459e04
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-16 13:14:55 +0530

    Scripts for Brazil+Petrorio

--- Corporate+Press+Releases/Australia+Woodside+Energy_list.js
+++ Corporate+Press+Releases/Australia+Woodside+Energy_list.js
@@ -1,1 +1,12 @@
-http://www.woodside.com.au/Investors-Media/announcements/Pages/Announcements-CurrentYear.aspx#.VUCVyL6zBFI
+//http://www.woodside.com.au/Investors-Media/announcements/Pages/Announcements-CurrentYear.aspx#.VUCVyL6zBFI
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.linksEvaluate(["http://www.woodside.com.au/Investors-Media/announcements/Pages/Announcements-CurrentYear.aspx#.VUCVyL6zBFI"], function () {
+    var urls = [];
+    $(".Announcements_CurrentYear .month li").each(function (idx, el) {
+        var anc = $(el).find("a");
+        var obj={link:anc[0].href,title:anc.find(".title")[0].innerText,published_s:anc.find(".date")[0].innerText.trim().replace(/\./g,'-'),content:'pdf'};
+        urls.push(obj);
+    });
+    return urls;
+});
