0f995231d3315e0bccfc37bfe5fc3e3a54724392
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-16 13:21:38 +0530

    Scripts for Brazil+Petrorio

--- Corporate+Press+Releases/Australia+Tap+Oil+Ltd_list.js
+++ Corporate+Press+Releases/Australia+Tap+Oil+Ltd_list.js
@@ -1,1 +1,12 @@
-http://www.tapoil.com.au/irm/archive/asx-announcements.aspx?RID=288
+//http://www.tapoil.com.au/irm/archive/asx-announcements.aspx?RID=288
+var main=require(require('system').env['NL_HOME']+'main.js');
+
+main.linksEvaluate(["http://www.tapoil.com.au/irm/archive/asx-announcements.aspx?RID=288"], function () {
+    var urls = [];
+    $(".newsitems .article").each(function (idx, el) {
+        var anc = $(el).find("a")[0];
+        var obj={link:anc.href,title:anc.innerText.trim(),published_s:$(el).find(".articleDate").text(),content:'pdf'};
+        urls.push(obj);
+    });
+    return urls;
+});
