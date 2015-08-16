6461fc633b8750f52c52ffd428e729072f03cb2b
Ramakrishnareddy. Bandi <ramakrishnareddy.b@happiestminds.com>
2015-08-16 14:13:39 +0530

    Scripts for Glencore+International+Switzerland

--- Corporate+Press+Releases/Glencore+International+Switzerland_body.js
+++ Corporate+Press+Releases/Glencore+International+Switzerland_body.js
@@ -1,1 +1,2 @@
 // Glencore
+// PDF files
--- Corporate+Press+Releases/Glencore+International+Switzerland_list.js
+++ Corporate+Press+Releases/Glencore+International+Switzerland_list.js
@@ -1,1 +1,15 @@
-http://www.glencore.com/media/
+//http://www.glencore.com/media/news/
+var main = require(require('system').env['NL_HOME'] + 'main.js');
+
+main.linksEvaluate(["http://www.glencore.com/media/news/"], function () {
+    var urls = [];
+    $(".typography ul > li").each(function (idx, el) {
+        var anc=$(el).find("a")[0];
+        var link_s = anc.href;
+        var title =  anc.innerText.trim();
+        $(el).find("a").remove();
+        var obj = {link: link_s, title: title, published_s: $(el).text().trim().replace(/\//g, '-'), content: 'pdf'};
+        urls.push(obj);
+    });
+    return urls;
+});
